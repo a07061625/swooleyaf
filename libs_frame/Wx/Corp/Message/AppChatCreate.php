53c2344465edb5974d8b5116801fb8a87e9b907d
jiangwei <jiangwei07061625@163.com>
2019-01-24 23:32:57 +0800

    wx corp add message

---
+++ libs_frame/Wx/Corp/Message/AppChatCreate.php
@@ -0,0 +1,136 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 姜伟
+ * Date: 2018/12/22 0022
+ * Time: 11:05
+ */
+namespace Wx\Corp\Message;
+
+use Constant\ErrorCode;
+use Exception\Wx\WxException;
+use Tool\Tool;
+use Wx\WxBaseCorp;
+use Wx\WxTraitCorp;
+use Wx\WxUtilBase;
+
+/**
+ * 创建群聊会话
+ * @package Wx\Corp\Message
+ */
+class AppChatCreate extends WxBaseCorp {
+    use WxTraitCorp;
+
+    /**
+     * 群聊名
+     * @var string
+     */
+    private $name = '';
+    /**
+     * 群主id
+     * @var string
+     */
+    private $owner = '';
+    /**
+     * 群成员列表
+     * @var array
+     */
+    private $userlist = [];
+    /**
+     * 群id
+     * @var string
+     */
+    private $chatid = '';
+
+    public function __construct(string $corpId,string $agentTag){
+        parent::__construct();
+        $this->serviceUrl = 'https://qyapi.weixin.qq.com/cgi-bin/appchat/create?access_token=';
+        $this->_corpId = $corpId;
+        $this->_agentTag = $agentTag;
+        $this->reqData['chatid'] = Tool::createNonceStr('8', 'numlower') . Tool::getNowTime();
+    }
+
+    private function __clone(){
+    }
+
+    /**
+     * @param string $name
+     * @throws \Exception\Wx\WxException
+     */
+    public function setName(string $name){
+        if (strlen($name) > 0) {
+            $this->reqData['name'] = mb_substr($name, 0, 25);
+        } else {
+            throw new WxException('群聊名不合法', ErrorCode::WX_PARAM_ERROR);
+        }
+    }
+
+    /**
+     * @param string $owner
+     * @throws \Exception\Wx\WxException
+     */
+    public function setOwner(string $owner){
+        if (ctype_alnum($owner)) {
+            $this->reqData['owner'] = $owner;
+        } else {
+            throw new WxException('群主id不合法', ErrorCode::WX_PARAM_ERROR);
+        }
+    }
+
+    /**
+     * @param array $userList
+     * @throws \Exception\Wx\WxException
+     */
+    public function setUserList(array $userList){
+        $users = [];
+        foreach ($userList as $eUserId) {
+            if(ctype_alnum($eUserId)){
+                $userId = strtolower($eUserId);
+                $users[$userId] = 1;
+            }
+        }
+        $userNum = count($users);
+        if ($userNum < 2) {
+            throw new WxException('群成员不能少于2个', ErrorCode::WX_PARAM_ERROR);
+        } else if($userNum > 500){
+            throw new WxException('群成员不能超过500个', ErrorCode::WX_PARAM_ERROR);
+        }
+
+        $this->reqData['userlist'] = array_keys($users);
+    }
+
+    /**
+     * @param string $chatId
+     * @throws \Exception\Wx\WxException
+     */
+    public function setChatId(string $chatId){
+        if (ctype_alnum($chatId) && (strlen($chatId) <= 32)) {
+            $this->reqData['chatid'] = $chatId;
+        } else {
+            throw new WxException('群id不合法', ErrorCode::WX_PARAM_ERROR);
+        }
+    }
+
+    public function getDetail() : array {
+        if(!isset($this->reqData['name'])){
+            throw new WxException('群聊名不能为空', ErrorCode::WX_PARAM_ERROR);
+        }
+
+        $resArr = [
+            'code' => 0,
+        ];
+
+        $this->curlConfigs[CURLOPT_URL] = $this->serviceUrl . $this->getAccessToken(WxBaseCorp::ACCESS_TOKEN_TYPE_CORP, $this->_corpId, $this->_agentTag);
+        $this->curlConfigs[CURLOPT_POSTFIELDS] = Tool::jsonEncode($this->reqData, JSON_UNESCAPED_UNICODE);
+        $sendRes = WxUtilBase::sendPostReq($this->curlConfigs);
+        $sendData = Tool::jsonDecode($sendRes);
+        if($sendData['errcode'] == 0){
+            $resArr['data'] = $sendData;
+        } else {
+            $resArr['code'] = ErrorCode::WX_POST_ERROR;
+            $resArr['message'] = $sendData['errmsg'];
+        }
+
+        return $resArr;
+    }
+}
---
+++ libs_frame/Wx/Corp/Message/AppChatSend.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 姜伟
+ * Date: 2018/12/22 0022
+ * Time: 11:05
+ */
+namespace Wx\Corp\Message;
+
+use Constant\ErrorCode;
+use Exception\Wx\WxException;
+use Tool\Tool;
+use Wx\WxBaseCorp;
+use Wx\WxTraitCorp;
+use Wx\WxUtilBase;
+
+/**
+ * 发送群聊会话消息
+ * @package Wx\Corp\Message
+ */
+class AppChatSend extends WxBaseCorp {
+    use WxTraitCorp;
+
+    /**
+     * 群id
+     * @var string
+     */
+    private $chatid = '';
+    /**
+     * 消息类型
+     * @var string
+     */
+    private $msgtype = '';
+    /**
+     * 保密消息标识,默认0 0:否 1:是
+     * @var int
+     */
+    private $safe = 0;
+
+    public function __construct(string $corpId,string $agentTag){
+        parent::__construct();
+        $this->serviceUrl = 'https://qyapi.weixin.qq.com/cgi-bin/appchat/send?access_token=';
+        $this->_corpId = $corpId;
+        $this->_agentTag = $agentTag;
+    }
+
+    private function __clone(){
+    }
+
+    /**
+     * @param string $chatId
+     * @throws \Exception\Wx\WxException
+     */
+    public function setChatId(string $chatId){
+        if (ctype_alnum($chatId) && (strlen($chatId) <= 32)) {
+            $this->reqData['chatid'] = $chatId;
+        } else {
+            throw new WxException('群id不合法', ErrorCode::WX_PARAM_ERROR);
+        }
+    }
+
+    /**
+     * @param string $type
+     * @param array $data
+     * @throws \Exception\Wx\WxException
+     */
+    public function setMsgData(string $type,array $data){
+        if(!isset(self::$totalMessageType[$type])){
+            throw new WxException('消息类型不支持', ErrorCode::WX_PARAM_ERROR);
+        } else if($type == WxBaseCorp::MESSAGE_TYPE_MINI_NOTICE){
+            throw new WxException('消息类型不支持', ErrorCode::WX_PARAM_ERROR);
+        } else if(empty($data)){
+            throw new WxException('消息数据不能为空', ErrorCode::WX_PARAM_ERROR);
+        }
+
+        $this->reqData['msgtype'] = $type;
+        $this->reqData[$type] = $data;
+    }
+
+    /**
+     * @param int $safe
+     * @throws \Exception\Wx\WxException
+     */
+    public function setSafe(int $safe){
+        if (in_array($safe, [0, 1])) {
+            $this->reqData['safe'] = $safe;
+        } else {
+            throw new WxException('保密消息标识不合法', ErrorCode::WX_PARAM_ERROR);
+        }
+    }
+
+    public function getDetail() : array {
+        if(!isset($this->reqData['chatid'])){
+            throw new WxException('群id不能为空', ErrorCode::WX_PARAM_ERROR);
+        }
+        if(!isset($this->reqData['msgtype'])){
+            throw new WxException('消息类型不能为空', ErrorCode::WX_PARAM_ERROR);
+        }
+
+        $resArr = [
+            'code' => 0,
+        ];
+
+        $this->curlConfigs[CURLOPT_URL] = $this->serviceUrl . $this->getAccessToken(WxBaseCorp::ACCESS_TOKEN_TYPE_CORP, $this->_corpId, $this->_agentTag);
+        $this->curlConfigs[CURLOPT_POSTFIELDS] = Tool::jsonEncode($this->reqData, JSON_UNESCAPED_UNICODE);
+        $sendRes = WxUtilBase::sendPostReq($this->curlConfigs);
+        $sendData = Tool::jsonDecode($sendRes);
+        if($sendData['errcode'] == 0){
+            $resArr['data'] = $sendData;
+        } else {
+            $resArr['code'] = ErrorCode::WX_POST_ERROR;
+            $resArr['message'] = $sendData['errmsg'];
+        }
+
+        return $resArr;
+    }
+}
