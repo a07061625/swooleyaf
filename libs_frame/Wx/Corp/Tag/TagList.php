9e3cb44def9cb8e0d5d1a73fbf0e8bc00f81651d
jiangwei <jiangwei07061625@163.com>
2019-01-25 17:57:41 +0800

    wx corp add tag

---
+++ libs_frame/Wx/Corp/Tag/TagList.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 姜伟
+ * Date: 2018/12/22 0022
+ * Time: 11:05
+ */
+namespace Wx\Corp\Tag;
+
+use Constant\ErrorCode;
+use Tool\Tool;
+use Wx\WxBaseCorp;
+use Wx\WxTraitCorp;
+use Wx\WxUtilBase;
+
+/**
+ * 获取标签列表
+ * @package Wx\Corp\Tag
+ */
+class TagList extends WxBaseCorp {
+    use WxTraitCorp;
+
+    public function __construct(string $corpId,string $agentTag){
+        parent::__construct();
+        $this->serviceUrl = 'https://qyapi.weixin.qq.com/cgi-bin/tag/list';
+        $this->_corpId = $corpId;
+        $this->_agentTag = $agentTag;
+    }
+
+    private function __clone(){
+    }
+
+    public function getDetail() : array {
+        $resArr = [
+            'code' => 0,
+        ];
+
+        $this->reqData['access_token'] = $this->getAccessToken($this->_tokenType, $this->_corpId, $this->_agentTag);
+        $this->curlConfigs[CURLOPT_URL] = $this->serviceUrl . '?' . http_build_query($this->reqData);
+        $sendRes = WxUtilBase::sendGetReq($this->curlConfigs);
+        $sendData = Tool::jsonDecode($sendRes);
+        if($sendData['errcode'] == 0){
+            $resArr['data'] = $sendData;
+        } else {
+            $resArr['code'] = ErrorCode::WX_GET_ERROR;
+            $resArr['message'] = $sendData['errmsg'];
+        }
+
+        return $resArr;
+    }
+}
---
+++ libs_frame/Wx/Corp/Tag/TagUpdate.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 姜伟
+ * Date: 2018/12/22 0022
+ * Time: 11:05
+ */
+namespace Wx\Corp\Tag;
+
+use Constant\ErrorCode;
+use Exception\Wx\WxException;
+use Tool\Tool;
+use Wx\WxBaseCorp;
+use Wx\WxTraitCorp;
+use Wx\WxUtilBase;
+
+/**
+ * 更新标签名字
+ * @package Wx\Corp\Tag
+ */
+class TagUpdate extends WxBaseCorp {
+    use WxTraitCorp;
+
+    /**
+     * 标签id
+     * @var int
+     */
+    private $tagid = 0;
+    /**
+     * 名称
+     * @var string
+     */
+    private $tagname = '';
+
+    public function __construct(string $corpId,string $agentTag){
+        parent::__construct();
+        $this->serviceUrl = 'https://qyapi.weixin.qq.com/cgi-bin/tag/update?access_token=';
+        $this->_corpId = $corpId;
+        $this->_agentTag = $agentTag;
+    }
+
+    private function __clone(){
+    }
+
+    /**
+     * @param int $tagId
+     * @throws \Exception\Wx\WxException
+     */
+    public function setTagId(int $tagId){
+        if ($tagId > 0) {
+            $this->reqData['tagid'] = $tagId;
+        } else {
+            throw new WxException('标签id不合法', ErrorCode::WX_PARAM_ERROR);
+        }
+    }
+
+    /**
+     * @param string $tagName
+     * @throws \Exception\Wx\WxException
+     */
+    public function setTagName(string $tagName){
+        if (strlen($tagName) > 0) {
+            $this->reqData['tagname'] = mb_substr($tagName, 0, 16);
+        } else {
+            throw new WxException('名称不合法', ErrorCode::WX_PARAM_ERROR);
+        }
+    }
+
+    public function getDetail() : array {
+        if(!isset($this->reqData['tagid'])){
+            throw new WxException('标签id不能为空', ErrorCode::WX_PARAM_ERROR);
+        }
+        if(!isset($this->reqData['tagname'])){
+            throw new WxException('名称不能为空', ErrorCode::WX_PARAM_ERROR);
+        }
+
+        $resArr = [
+            'code' => 0,
+        ];
+
+        $this->curlConfigs[CURLOPT_URL] = $this->serviceUrl . $this->getAccessToken($this->_tokenType, $this->_corpId, $this->_agentTag);
+        $this->curlConfigs[CURLOPT_POSTFIELDS] = Tool::jsonEncode($this->reqData, JSON_UNESCAPED_UNICODE);
+        $sendRes = WxUtilBase::sendPostReq($this->curlConfigs);
+        $sendData = Tool::jsonDecode($sendRes);
+        if($sendData['errcode'] == 0){
+            $resArr['data'] = $sendData;
+        } else {
+            $resArr['code'] = ErrorCode::WX_POST_ERROR;
+            $resArr['message'] = $sendData['errmsg'];
+        }
+
+        return $resArr;
+    }
+}
