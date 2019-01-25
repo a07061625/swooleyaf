6d657b44e4bcb4de6ffdb0e285d9183fa4c9763f
jiangwei <jiangwei07061625@163.com>
2019-01-25 10:31:07 +0800

    wx corp add crm

---
+++ libs_frame/Wx/Corp/Crm/ExternalContactGet.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 姜伟
+ * Date: 2018/12/22 0022
+ * Time: 11:05
+ */
+namespace Wx\Corp\Crm;
+
+use Constant\ErrorCode;
+use Exception\Wx\WxException;
+use Tool\Tool;
+use Wx\WxBaseCorp;
+use Wx\WxTraitCorp;
+use Wx\WxUtilBase;
+
+/**
+ * 获取外部联系人详情
+ * @package Wx\Corp\Crm
+ */
+class ExternalContactGet extends WxBaseCorp {
+    use WxTraitCorp;
+
+    /**
+     * 外部联系人用户ID
+     * @var string
+     */
+    private $external_userid = '';
+
+    public function __construct(string $corpId,string $agentTag){
+        parent::__construct();
+        $this->serviceUrl = 'https://qyapi.weixin.qq.com/cgi-bin/crm/get_external_contact';
+        $this->_corpId = $corpId;
+        $this->_agentTag = $agentTag;
+    }
+
+    private function __clone(){
+    }
+
+    /**
+     * @param string $externalUserId
+     * @throws \Exception\Wx\WxException
+     */
+    public function setExternalUserId(string $externalUserId){
+        if (ctype_alnum($externalUserId)) {
+            $this->reqData['external_userid'] = $externalUserId;
+        } else {
+            throw new WxException('外部联系人用户ID不合法', ErrorCode::WX_PARAM_ERROR);
+        }
+    }
+
+    public function getDetail() : array {
+        if(!isset($this->reqData['external_userid'])){
+            throw new WxException('外部联系人用户ID不能为空', ErrorCode::WX_PARAM_ERROR);
+        }
+
+        $resArr = [
+            'code' => 0,
+        ];
+
+        $this->reqData['access_token'] = $this->getAccessToken($this->_tokenType, $this->_corpId, $this->_agentTag);
+        $this->curlConfigs[CURLOPT_URL] = $this->serviceUrl . '?' . http_build_query($this->reqData);
+        $sendRes = WxUtilBase::sendGetReq($this->curlConfigs);
+        $sendData = Tool::jsonDecode($sendRes);
+        if($sendData['errcode'] == 0){
+            $resArr['data'] = $sendData;
+        } else {
+            $resArr['code'] = ErrorCode::WX_GET_ERROR;
+            $resArr['message'] = $sendData['errmsg'];
+        }
+
+        return $resArr;
+    }
+}
---
+++ libs_frame/Wx/Corp/Crm/ExternalContactTransfer.php
@@ -0,0 +1,114 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 姜伟
+ * Date: 2018/12/22 0022
+ * Time: 11:05
+ */
+namespace Wx\Corp\Crm;
+
+use Constant\ErrorCode;
+use Exception\Wx\WxException;
+use Tool\Tool;
+use Wx\WxBaseCorp;
+use Wx\WxTraitCorp;
+use Wx\WxUtilBase;
+
+/**
+ * 离职成员的外部联系人再分配
+ * @package Wx\Corp\Crm
+ */
+class ExternalContactTransfer extends WxBaseCorp {
+    use WxTraitCorp;
+
+    /**
+     * 外部联系人用户ID
+     * @var string
+     */
+    private $external_userid = '';
+    /**
+     * 离职成员用户ID
+     * @var string
+     */
+    private $handover_userid = '';
+    /**
+     * 接替成员用户ID
+     * @var string
+     */
+    private $takeover_userid = '';
+
+    public function __construct(string $corpId,string $agentTag){
+        parent::__construct();
+        $this->serviceUrl = 'https://qyapi.weixin.qq.com/cgi-bin/crm/transfer_external_contact?access_token=';
+        $this->_corpId = $corpId;
+        $this->_agentTag = $agentTag;
+    }
+
+    private function __clone(){
+    }
+
+    /**
+     * @param string $externalUserId
+     * @throws \Exception\Wx\WxException
+     */
+    public function setExternalUserId(string $externalUserId){
+        if (ctype_alnum($externalUserId)) {
+            $this->reqData['external_userid'] = $externalUserId;
+        } else {
+            throw new WxException('外部联系人用户ID不合法', ErrorCode::WX_PARAM_ERROR);
+        }
+    }
+
+    /**
+     * @param string $handoverUserId
+     * @throws \Exception\Wx\WxException
+     */
+    public function setHandoverUserId(string $handoverUserId){
+        if (ctype_alnum($handoverUserId)) {
+            $this->reqData['handover_userid'] = $handoverUserId;
+        } else {
+            throw new WxException('离职成员用户ID不合法', ErrorCode::WX_PARAM_ERROR);
+        }
+    }
+
+    /**
+     * @param string $takeoverUserId
+     * @throws \Exception\Wx\WxException
+     */
+    public function setTakeoverUserId(string $takeoverUserId){
+        if (ctype_alnum($takeoverUserId)) {
+            $this->reqData['takeover_userid'] = $takeoverUserId;
+        } else {
+            throw new WxException('接替成员用户ID不合法', ErrorCode::WX_PARAM_ERROR);
+        }
+    }
+
+    public function getDetail() : array {
+        if(!isset($this->reqData['external_userid'])){
+            throw new WxException('外部联系人用户ID不能为空', ErrorCode::WX_PARAM_ERROR);
+        }
+        if(!isset($this->reqData['handover_userid'])){
+            throw new WxException('离职成员用户ID不能为空', ErrorCode::WX_PARAM_ERROR);
+        }
+        if(!isset($this->reqData['takeover_userid'])){
+            throw new WxException('接替成员用户ID不能为空', ErrorCode::WX_PARAM_ERROR);
+        }
+
+        $resArr = [
+            'code' => 0,
+        ];
+
+        $this->curlConfigs[CURLOPT_URL] = $this->serviceUrl . $this->getAccessToken($this->_tokenType, $this->_corpId, $this->_agentTag);
+        $this->curlConfigs[CURLOPT_POSTFIELDS] = Tool::jsonEncode($this->reqData, JSON_UNESCAPED_UNICODE);
+        $sendRes = WxUtilBase::sendPostReq($this->curlConfigs);
+        $sendData = Tool::jsonDecode($sendRes);
+        if($sendData['errcode'] == 0){
+            $resArr['data'] = $sendData;
+        } else {
+            $resArr['code'] = ErrorCode::WX_POST_ERROR;
+            $resArr['message'] = $sendData['errmsg'];
+        }
+
+        return $resArr;
+    }
+}
