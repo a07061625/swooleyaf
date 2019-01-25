7451869573ce3264fccbef0131cf39595db47c15
jiangwei <jiangwei07061625@163.com>
2019-01-25 21:33:29 +0800

    create code image update

--- sy_services/application/controllers/Image.php
+++ sy_services/application/controllers/Image.php
@@ -117,25 +117,25 @@ class ImageController extends CommonController {
         $imageWidth = (int)\Request\SyRequest::getParams('image_width', 130);
         $imageHeight = (int)\Request\SyRequest::getParams('image_height', 45);
         $image = imagecreate($imageWidth, $imageHeight);
-        imagecolorallocate($image, mt_rand(50, 200), mt_rand(0, 155), mt_rand(0, 155)); //第一次对imagecolorallocate()的调用会给基于调色板的图像填充背景色
+        imagecolorallocate($image, random_int(50, 200), random_int(0, 155), random_int(0, 155)); //第一次对imagecolorallocate()的调用会给基于调色板的图像填充背景色
         $fontColor = imageColorAllocate($image, 255, 255, 255); //字体颜色
         $code = '';
         //产生随机字符
         for ($i = 0; $i < 4; $i++) {
-            $randAsciiNumArray = [mt_rand(48, 57), mt_rand(65, 90)];
-            $randAsciiNum = $randAsciiNumArray[mt_rand(0, 1)];
+            $randAsciiNumArray = [random_int(48, 57), random_int(65, 90)];
+            $randAsciiNum = $randAsciiNumArray[random_int(0, 1)];
             $randStr = chr($randAsciiNum);
-            imagettftext($image, 30, mt_rand(0, 20) - mt_rand(0, 25), 5 + $i * 30, mt_rand(30, 35), $fontColor, $fontPath, $randStr);
+            imagettftext($image, 30, random_int(0, 20) - random_int(0, 25), 5 + $i * 30, random_int(30, 35), $fontColor, $fontPath, $randStr);
             $code .= $randStr;
         }
         //干扰线
         for ($i = 0; $i < 8; $i++) {
-            $lineColor = imagecolorallocate($image, mt_rand(0, 255), mt_rand(0, 255), mt_rand(0, 255));
-            imageline($image, mt_rand(0, $imageWidth), 0, mt_rand(0, $imageWidth), $imageHeight, $lineColor);
+            $lineColor = imagecolorallocate($image, random_int(0, 255), random_int(0, 255), random_int(0, 255));
+            imageline($image, random_int(0, $imageWidth), 0, random_int(0, $imageWidth), $imageHeight, $lineColor);
         }
         //干扰点
         for ($i = 0; $i < 250; $i++) {
-            imagesetpixel($image, mt_rand(0, $imageWidth), mt_rand(0, $imageHeight), $fontColor);
+            imagesetpixel($image, random_int(0, $imageWidth), random_int(0, $imageHeight), $fontColor);
         }
 
         ob_start();
