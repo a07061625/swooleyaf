5a933d9de6f3832b20f13db37b9cf1499bee9086
jiangwei <jiangwei07061625@163.com>
2019-01-28 16:56:29 +0800

    dingding add sns

---
+++ libs_frame/DingDing/Corp/Sns/UserInfoGet.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 姜伟
+ * Date: 19-1-28
+ * Time: 下午4:45
+ */
+namespace DingDing\Corp\Sns;
+
+use Constant\ErrorCode;
+use DingDing\TalkBaseCorp;
+use DingDing\TalkUtilBase;
+use DingDing\TalkUtilCorp;
+use DingDing\TalkUtilProvider;
+use Exception\DingDing\TalkException;
+use Tool\Tool;
+
+/**
+ * 获取授权用户的个人信息
+ * @package DingDing\Corp\Sns
+ */
+class UserInfoGet extends TalkBaseCorp {
+    /**
+     * 用户openid
+     * @var string
+     */
+    private $openid = '';
+    /**
+     * 持久授权码
+     * @var string
+     */
+    private $persistent_code = '';
+
+    public function __construct(string $corpId){
+        parent::__construct();
+        $this->_corpId = $corpId;
+    }
+
+    private function __clone(){
+    }
+
+    /**
+     * @param string $openid
+     * @throws \Exception\DingDing\TalkException
+     */
+    public function setOpenid(string $openid){
+        if(ctype_alnum($openid)){
+            $this->openid = $openid;
+        } else {
+            throw new TalkException('用户openid不合法', ErrorCode::DING_TALK_PARAM_ERROR);
+        }
+    }
+
+    /**
+     * @param string $persistentCode
+     * @throws \Exception\DingDing\TalkException
+     */
+    public function setPersistentCode(string $persistentCode){
+        if(ctype_alnum($persistentCode)){
+            $this->persistent_code = $persistentCode;
+        } else {
+            throw new TalkException('持久授权码不合法', ErrorCode::DING_TALK_PARAM_ERROR);
+        }
+    }
+
+    public function getDetail() : array {
+        if(strlen($this->openid) == 0){
+            throw new TalkException('用户openid不能为空', ErrorCode::DING_TALK_PARAM_ERROR);
+        }
+        if(strlen($this->persistent_code) == 0){
+            throw new TalkException('持久授权码不能为空', ErrorCode::DING_TALK_PARAM_ERROR);
+        }
+
+        $resArr = [
+            'code' => 0,
+        ];
+
+        if (strlen($this->_corpId) > 0) {
+            $this->curlConfigs[CURLOPT_URL] = $this->serviceDomain . '/sns/getuserinfo?' . http_build_query([
+                'sns_token' => TalkUtilCorp::getUserSnsToken($this->_corpId, $this->openid, $this->persistent_code),
+            ]);
+        } else {
+            $this->curlConfigs[CURLOPT_URL] = $this->serviceDomain . '/sns/getuserinfo?' . http_build_query([
+                'sns_token' => TalkUtilProvider::getUserSnsToken($this->openid, $this->persistent_code),
+            ]);
+        }
+        $sendRes = TalkUtilBase::sendGetReq($this->curlConfigs);
+        $sendData = Tool::jsonDecode($sendRes);
+        if($sendData['errcode'] == 0){
+            $resArr['data'] = $sendData;
+        } else {
+            $resArr['code'] = ErrorCode::DING_TALK_GET_ERROR;
+            $resArr['message'] = $sendData['errmsg'];
+        }
+
+        return $resArr;
+    }
+}
--- libs_frame/DingDing/TalkUtilCorp.php
+++ libs_frame/DingDing/TalkUtilCorp.php
@@ -11,6 +11,8 @@ use Constant\Project;
 use DesignPatterns\Factories\CacheSimpleFactory;
 use DesignPatterns\Singletons\DingTalkConfigSingleton;
 use DingDing\Corp\Common\AccessToken;
+use DingDing\Corp\Sns\SnsTokenGet;
+use DingDing\Corp\Sns\TokenGet;
 use DingDing\Corp\Sso\SsoToken;
 use Tool\Tool;
 use Traits\SimpleTrait;
@@ -72,5 +74,63 @@ final class TalkUtilCorp extends TalkUtilBase {
         CacheSimpleFactory::getRedisInstance()->expire($redisKey, 8000);
 
         return $ssoTokenDetail['access_token'];
+    }
+
+    /**
+     * 获取sns token
+     * @param string $corpId
+     * @return string
+     */
+    public static function getSnsToken(string $corpId) : string {
+        $nowTime = Tool::getNowTime();
+        $redisKey = Project::REDIS_PREFIX_DINGTALK_CORP . $corpId;
+        $redisData = CacheSimpleFactory::getRedisInstance()->hGetAll($redisKey);
+        if (isset($redisData['snsat_key']) && ($redisData['snsat_key'] == $redisKey) && ($redisData['snsat_expire'] >= $nowTime)) {
+            return $redisData['snsat_content'];
+        }
+
+        $snsTokenObj = new TokenGet($corpId);
+        $snsTokenDetail = $snsTokenObj->getDetail();
+        unset($snsTokenObj);
+
+        CacheSimpleFactory::getRedisInstance()->hMset($redisKey, [
+            'snsat_content' => $snsTokenDetail['access_token'],
+            'snsat_expire' => (int)($nowTime + 7180),
+            'snsat_key' => $redisKey,
+        ]);
+        CacheSimpleFactory::getRedisInstance()->expire($redisKey, 8000);
+
+        return $snsTokenDetail['access_token'];
+    }
+
+    /**
+     * 获取用户sns token
+     * @param string $corpId
+     * @param string $openid 用户openid
+     * @param string $persistentCode 持久授权码
+     * @return string
+     */
+    public static function getUserSnsToken(string $corpId,string $openid,string $persistentCode) : string {
+        $nowTime = Tool::getNowTime();
+        $redisKey = Project::REDIS_PREFIX_DINGTALK_CORP . $corpId . '_' . $openid;
+        $redisData = CacheSimpleFactory::getRedisInstance()->hGetAll($redisKey);
+        if (isset($redisData['sns_key']) && ($redisData['sns_key'] == $redisKey) && ($redisData['sns_expire'] >= $nowTime)) {
+            return $redisData['sns_content'];
+        }
+
+        $snsTokenObj = new SnsTokenGet($corpId);
+        $snsTokenObj->setOpenid($openid);
+        $snsTokenObj->setPersistentCode($persistentCode);
+        $snsTokenDetail = $snsTokenObj->getDetail();
+        unset($snsTokenObj);
+
+        CacheSimpleFactory::getRedisInstance()->hMset($redisKey, [
+            'sns_content' => $snsTokenDetail['sns_token'],
+            'sns_expire' => (int)($nowTime + $snsTokenDetail['expires_in'] - 10),
+            'sns_key' => $redisKey,
+        ]);
+        CacheSimpleFactory::getRedisInstance()->expire($redisKey, 8000);
+
+        return $snsTokenDetail['sns_token'];
     }
 }
