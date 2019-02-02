7b8f7cec35c61090493a37629d636362e18b10a3
jiangwei <jiangwei07061625@163.com>
2019-02-02 15:46:05 +0800

    dingding add attendance and workrecord

---
+++ libs_frame/DingDing/Corp/Attendance/LeaveStatus.php
@@ -0,0 +1,151 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 姜伟
+ * Date: 19-2-2
+ * Time: 下午1:03
+ */
+namespace DingDing\Corp\Attendance;
+
+use Constant\ErrorCode;
+use DingDing\TalkBaseCorp;
+use DingDing\TalkTraitCorp;
+use DingDing\TalkUtilBase;
+use Exception\DingDing\TalkException;
+use Tool\Tool;
+
+/**
+ * 查询请假状态
+ * @package DingDing\Corp\Attendance
+ */
+class LeaveStatus extends TalkBaseCorp {
+    use TalkTraitCorp;
+
+    /**
+     * 用户列表
+     * @var string
+     */
+    private $userid_list = '';
+    /**
+     * 开始时间
+     * @var int
+     */
+    private $start_time = 0;
+    /**
+     * 结束时间
+     * @var int
+     */
+    private $end_time = 0;
+    /**
+     * 偏移量
+     * @var int
+     */
+    private $offset = 0;
+    /**
+     * 分页大小
+     * @var int
+     */
+    private $size = 0;
+
+    public function __construct(string $corpId,string $agentTag){
+        parent::__construct();
+        $this->_corpId = $corpId;
+        $this->_agentTag = $agentTag;
+        $this->reqData['offset'] = 0;
+        $this->reqData['size'] = 10;
+    }
+
+    private function __clone(){
+    }
+
+    /**
+     * @param array $userList
+     * @throws \Exception\DingDing\TalkException
+     */
+    public function setUserList(array $userList){
+        $users = [];
+        foreach ($userList as $eUserId) {
+            if(ctype_alnum($eUserId)){
+                $users[$eUserId] = 1;
+            }
+        }
+
+        $userNum = count($users);
+        if($userNum == 0){
+            throw new TalkException('用户列表不能为空', ErrorCode::DING_TALK_PARAM_ERROR);
+        } else if($userNum > 100){
+            throw new TalkException('用户不能超过100个', ErrorCode::DING_TALK_PARAM_ERROR);
+        }
+        $this->reqData['userid_list'] = implode(',', array_keys($users));
+    }
+
+    /**
+     * @param int $startTime
+     * @param int $endTime
+     * @throws \Exception\DingDing\TalkException
+     */
+    public function setStartTimeAndEndTime(int $startTime,int $endTime){
+        if ($startTime < 946656000) {
+            throw new TalkException('开始时间不合法', ErrorCode::DING_TALK_PARAM_ERROR);
+        } else if($endTime < $startTime){
+            throw new TalkException('结束时间不能小于开始时间', ErrorCode::DING_TALK_PARAM_ERROR);
+        } else if(($endTime - $startTime) > 15552000){
+            throw new TalkException('结束时间不能超过开始时间180天', ErrorCode::DING_TALK_PARAM_ERROR);
+        }
+
+        $this->reqData['start_time'] = $startTime;
+        $this->reqData['end_time'] = $endTime;
+    }
+
+    /**
+     * @param int $offset
+     * @throws \Exception\DingDing\TalkException
+     */
+    public function setOffset(int $offset){
+        if($offset >= 0){
+            $this->reqData['offset'] = $offset;
+        } else {
+            throw new TalkException('偏移量不合法', ErrorCode::DING_TALK_PARAM_ERROR);
+        }
+    }
+
+    /**
+     * @param int $size
+     * @throws \Exception\DingDing\TalkException
+     */
+    public function setSize(int $size){
+        if($size > 0){
+            $this->reqData['size'] = $size > 20 ? 20 : $size;
+        } else {
+            throw new TalkException('分页大小不合法', ErrorCode::DING_TALK_PARAM_ERROR);
+        }
+    }
+
+    public function getDetail() : array {
+        if(!isset($this->reqData['userid_list'])){
+            throw new TalkException('用户列表不能为空', ErrorCode::DING_TALK_PARAM_ERROR);
+        }
+        if(!isset($this->reqData['start_time'])){
+            throw new TalkException('开始时间不能为空', ErrorCode::DING_TALK_PARAM_ERROR);
+        }
+
+        $resArr = [
+            'code' => 0,
+        ];
+
+        $this->curlConfigs[CURLOPT_URL] = $this->serviceDomain . '/topapi/attendance/getleavestatus?' . http_build_query([
+            'access_token' => $this->getAccessToken(TalkBaseCorp::ACCESS_TOKEN_TYPE_CORP, $this->_corpId, $this->_agentTag),
+        ]);
+        $this->curlConfigs[CURLOPT_POSTFIELDS] = Tool::jsonEncode($this->reqData, JSON_UNESCAPED_UNICODE);
+        $sendRes = TalkUtilBase::sendPostReq($this->curlConfigs);
+        $sendData = Tool::jsonDecode($sendRes);
+        if($sendData['errcode'] == 0){
+            $resArr['data'] = $sendData;
+        } else {
+            $resArr['code'] = ErrorCode::DING_TALK_POST_ERROR;
+            $resArr['message'] = $sendData['errmsg'];
+        }
+
+        return $resArr;
+    }
+}
---
+++ libs_frame/DingDing/Corp/WorkRecord/WorkRecordAdd.php
@@ -0,0 +1,155 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 姜伟
+ * Date: 19-2-2
+ * Time: 下午2:23
+ */
+namespace DingDing\Corp\WorkRecord;
+
+use Constant\ErrorCode;
+use DingDing\TalkBaseCorp;
+use DingDing\TalkTraitCorp;
+use DingDing\TalkUtilBase;
+use Exception\DingDing\TalkException;
+use Tool\Tool;
+
+/**
+ * 发起待办
+ * @package DingDing\Corp\WorkRecord
+ */
+class WorkRecordAdd extends TalkBaseCorp {
+    use TalkTraitCorp;
+
+    /**
+     * 用户ID
+     * @var string
+     */
+    private $userid = '';
+    /**
+     * 待办时间
+     * @var int
+     */
+    private $create_time = 0;
+    /**
+     * 标题
+     * @var string
+     */
+    private $title = '';
+    /**
+     * 跳转链接
+     * @var string
+     */
+    private $url = '';
+    /**
+     * 表单列表
+     * @var array
+     */
+    private $formItemList = [];
+
+    public function __construct(string $corpId,string $agentTag){
+        parent::__construct();
+        $this->_corpId = $corpId;
+        $this->_agentTag = $agentTag;
+        $this->reqData['formItemList'] = [];
+    }
+
+    private function __clone(){
+    }
+
+    /**
+     * @param string $userId
+     * @throws \Exception\DingDing\TalkException
+     */
+    public function setUserId(string $userId){
+        if(ctype_alnum($userId)){
+            $this->reqData['userid'] = $userId;
+        } else {
+            throw new TalkException('用户ID不合法', ErrorCode::DING_TALK_PARAM_ERROR);
+        }
+    }
+
+    /**
+     * @param int $createTime
+     * @throws \Exception\DingDing\TalkException
+     */
+    public function setCreateTime(int $createTime){
+        if($createTime > Tool::getNowTime()){
+            $this->reqData['create_time'] = 1000 * $createTime;
+        } else {
+            throw new TalkException('待办时间不合法', ErrorCode::DING_TALK_PARAM_ERROR);
+        }
+    }
+
+    /**
+     * @param string $title
+     * @throws \Exception\DingDing\TalkException
+     */
+    public function setTitle(string $title){
+        if(strlen($title) > 0){
+            $this->reqData['title'] = $title;
+        } else {
+            throw new TalkException('标题不合法', ErrorCode::DING_TALK_PARAM_ERROR);
+        }
+    }
+
+    /**
+     * @param string $url
+     * @throws \Exception\DingDing\TalkException
+     */
+    public function setUrl(string $url){
+        if (preg_match('/^(http|https)\:\/\/\S+$/', $url) > 0) {
+            $this->reqData['url'] = $url;
+        } else {
+            throw new TalkException('跳转链接不合法', ErrorCode::DING_TALK_PARAM_ERROR);
+        }
+    }
+
+    /**
+     * @param array $formItem
+     * @throws \Exception\DingDing\TalkException
+     */
+    public function addFormItem(array $formItem){
+        if(empty($formItem)){
+            throw new TalkException('表单不合法', ErrorCode::DING_TALK_PARAM_ERROR);
+        }
+        $this->reqData['formItemList'][] = $formItem;
+    }
+
+    public function getDetail() : array {
+        if(!isset($this->reqData['userid'])){
+            throw new TalkException('用户ID不能为空', ErrorCode::DING_TALK_PARAM_ERROR);
+        }
+        if(!isset($this->reqData['create_time'])){
+            throw new TalkException('待办时间不能为空', ErrorCode::DING_TALK_PARAM_ERROR);
+        }
+        if(!isset($this->reqData['title'])){
+            throw new TalkException('标题不能为空', ErrorCode::DING_TALK_PARAM_ERROR);
+        }
+        if(!isset($this->reqData['url'])){
+            throw new TalkException('跳转链接不能为空', ErrorCode::DING_TALK_PARAM_ERROR);
+        }
+        if(empty($this->reqData['formItemList'])){
+            throw new TalkException('表单列表不能为空', ErrorCode::DING_TALK_PARAM_ERROR);
+        }
+
+        $resArr = [
+            'code' => 0,
+        ];
+
+        $this->curlConfigs[CURLOPT_URL] = $this->serviceDomain . '/topapi/workrecord/add?' . http_build_query([
+            'access_token' => $this->getAccessToken($this->_tokenType, $this->_corpId, $this->_agentTag),
+        ]);
+        $this->curlConfigs[CURLOPT_POSTFIELDS] = Tool::jsonEncode($this->reqData, JSON_UNESCAPED_UNICODE);
+        $sendRes = TalkUtilBase::sendPostReq($this->curlConfigs);
+        $sendData = Tool::jsonDecode($sendRes);
+        if($sendData['errcode'] == 0){
+            $resArr['data'] = $sendData;
+        } else {
+            $resArr['code'] = ErrorCode::DING_TALK_POST_ERROR;
+            $resArr['message'] = $sendData['errmsg'];
+        }
+
+        return $resArr;
+    }
+}
