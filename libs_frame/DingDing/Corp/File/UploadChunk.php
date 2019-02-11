ebfa60de506c797a900088cc31c3bbb8a4107871
jiangwei <jiangwei07061625@163.com>
2019-02-11 18:16:43 +0800

    dingding add file

---
+++ libs_frame/DingDing/Corp/File/UploadChunk.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 姜伟
+ * Date: 19-2-11
+ * Time: 下午5:12
+ */
+namespace DingDing\Corp\File;
+
+use Constant\ErrorCode;
+use DesignPatterns\Singletons\DingTalkConfigSingleton;
+use DingDing\TalkBaseCorp;
+use DingDing\TalkTraitCorp;
+use Exception\DingDing\TalkException;
+
+/**
+ * 上传文件块
+ * @package DingDing\Corp\File
+ */
+class UploadChunk extends TalkBaseCorp {
+    use TalkTraitCorp;
+
+    /**
+     * 应用ID
+     * @var string
+     */
+    private $agent_id = '';
+    /**
+     * 上传事务id
+     * @var string
+     */
+    private $upload_id = '';
+    /**
+     * 文件块号
+     * @var int
+     */
+    private $chunk_sequence = 0;
+
+    public function __construct(string $corpId,string $agentTag){
+        parent::__construct();
+        $this->_corpId = $corpId;
+        $this->_agentTag = $agentTag;
+        $agentInfo = DingTalkConfigSingleton::getInstance()->getCorpConfig($corpId)->getAgentInfo($agentTag);
+        $this->agent_id = $agentInfo['id'];
+    }
+
+    private function __clone(){
+    }
+
+    /**
+     * @param string $filePath
+     * @throws \Exception\DingDing\TalkException
+     */
+    public function setFilePath(string $filePath){
+        if(file_exists($filePath) && is_readable($filePath)){
+            $this->reqData['file'] = new \CURLFile($filePath);
+        } else {
+            throw new TalkException('文件不合法', ErrorCode::DING_TALK_PARAM_ERROR);
+        }
+    }
+
+    /**
+     * @param string $uploadId
+     * @throws \Exception\DingDing\TalkException
+     */
+    public function setUploadId(string $uploadId){
+        if(strlen($uploadId) > 0){
+            $this->upload_id = $uploadId;
+        } else {
+            throw new TalkException('上传事务id不合法', ErrorCode::DING_TALK_PARAM_ERROR);
+        }
+    }
+
+    /**
+     * @param int $chunkSequence
+     * @throws \Exception\DingDing\TalkException
+     */
+    public function setChunkSequence(int $chunkSequence){
+        if($chunkSequence > 0){
+            $this->chunk_sequence = $chunkSequence;
+        } else {
+            throw new TalkException('文件块号不合法', ErrorCode::DING_TALK_PARAM_ERROR);
+        }
+    }
+
+    public function getDetail() : array {
+        if(!isset($this->reqData['file'])){
+            throw new TalkException('文件不能为空', ErrorCode::DING_TALK_PARAM_ERROR);
+        }
+        if(strlen($this->upload_id) == 0){
+            throw new TalkException('上传事务id不能为空', ErrorCode::DING_TALK_PARAM_ERROR);
+        }
+        if($this->chunk_sequence <= 0){
+            throw new TalkException('文件块号不能为空', ErrorCode::DING_TALK_PARAM_ERROR);
+        }
+
+        $this->curlConfigs[CURLOPT_URL] = $this->serviceDomain . '/file/upload/chunk?' . http_build_query([
+            'access_token' => $this->getAccessToken($this->_tokenType, $this->_corpId, $this->_agentTag),
+            'agent_id' => $this->agent_id,
+            'upload_id' => $this->upload_id,
+            'chunk_sequence' => $this->chunk_sequence,
+        ]);
+        $this->curlConfigs[CURLOPT_POSTFIELDS] = $this->reqData;
+        return $this->sendRequest('POST');
+    }
+}
---
+++ libs_frame/DingDing/Corp/File/UploadTransaction.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 姜伟
+ * Date: 19-2-11
+ * Time: 下午5:12
+ */
+namespace DingDing\Corp\File;
+
+use Constant\ErrorCode;
+use DesignPatterns\Singletons\DingTalkConfigSingleton;
+use DingDing\TalkBaseCorp;
+use DingDing\TalkTraitCorp;
+use Exception\DingDing\TalkException;
+
+/**
+ * 分块上传事务
+ * @package DingDing\Corp\File
+ */
+class UploadTransaction extends TalkBaseCorp {
+    use TalkTraitCorp;
+
+    /**
+     * 应用ID
+     * @var string
+     */
+    private $agent_id = '';
+    /**
+     * 文件大小
+     * @var int
+     */
+    private $file_size = 0;
+    /**
+     * 文件总块数
+     * @var int
+     */
+    private $chunk_numbers = 0;
+    /**
+     * 上传事务id,没有设置是开启事务,设置了是提交事务
+     * @var string
+     */
+    private $upload_id = '';
+
+    public function __construct(string $corpId,string $agentTag){
+        parent::__construct();
+        $this->_corpId = $corpId;
+        $this->_agentTag = $agentTag;
+        $agentInfo = DingTalkConfigSingleton::getInstance()->getCorpConfig($corpId)->getAgentInfo($agentTag);
+        $this->reqData['agent_id'] = $agentInfo['id'];
+    }
+
+    private function __clone(){
+    }
+
+    /**
+     * @param int $fileSize
+     * @throws \Exception\DingDing\TalkException
+     */
+    public function setFileSize(int $fileSize){
+        if($fileSize >= 102400){
+            $this->reqData['file_size'] = $fileSize;
+        } else {
+            throw new TalkException('文件大小不合法', ErrorCode::DING_TALK_PARAM_ERROR);
+        }
+    }
+
+    /**
+     * @param int $chunkNumbers
+     * @throws \Exception\DingDing\TalkException
+     */
+    public function setChunkNumbers(int $chunkNumbers){
+        if(($chunkNumbers > 0) && ($chunkNumbers <= 10000)){
+            $this->reqData['chunk_numbers'] = $chunkNumbers;
+        } else {
+            throw new TalkException('文件总块数不合法', ErrorCode::DING_TALK_PARAM_ERROR);
+        }
+    }
+
+    /**
+     * @param string $uploadId
+     * @throws \Exception\DingDing\TalkException
+     */
+    public function setUploadId(string $uploadId){
+        if(strlen($uploadId) > 0){
+            $this->upload_id = $uploadId;
+        } else {
+            throw new TalkException('上传事务id不合法', ErrorCode::DING_TALK_PARAM_ERROR);
+        }
+    }
+
+    public function getDetail() : array {
+        if(!isset($this->reqData['file_size'])){
+            throw new TalkException('文件大小不能为空', ErrorCode::DING_TALK_PARAM_ERROR);
+        }
+        if(!isset($this->reqData['chunk_numbers'])){
+            throw new TalkException('文件总块数不能为空', ErrorCode::DING_TALK_PARAM_ERROR);
+        }
+
+        $this->reqData['access_token'] = $this->getAccessToken($this->_tokenType, $this->_corpId, $this->_agentTag);
+        $this->curlConfigs[CURLOPT_URL] = $this->serviceDomain . '/file/upload/transaction?' . http_build_query($this->reqData);
+        return $this->sendRequest('GET');
+    }
+}
