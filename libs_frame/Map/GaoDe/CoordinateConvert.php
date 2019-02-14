696572ef67fc1961e7da1efbd1f1f7a6f68978aa
jiangwei <jiangwei07061625@163.com>
2019-02-14 15:08:14 +0800

    map add gaode

---
+++ libs_frame/Map/GaoDe/CoordinateConvert.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 姜伟
+ * Date: 19-2-14
+ * Time: 下午1:42
+ */
+namespace Map\GaoDe;
+
+use Constant\ErrorCode;
+use Exception\Map\GaoDeMapException;
+use Map\MapBaseGaoDe;
+
+/**
+ * 坐标转换
+ * @package Map\GaoDe
+ */
+class CoordinateConvert extends MapBaseGaoDe {
+    /**
+     * 经纬度坐标
+     * @var string
+     */
+    private $locations = '';
+    /**
+     * 原坐标系
+     * @var string
+     */
+    private $coordsys = '';
+
+    public function __construct(){
+        parent::__construct();
+        $this->serviceUri = '/assistant/coordinate/convert';
+        $this->reqData['coordsys'] = 'autonavi';
+    }
+
+    /**
+     * @param array $locationList
+     * @throws \Exception\Map\GaoDeMapException
+     */
+    public function setLocations(array $locationList){
+        $locationNum = count($locationList);
+        if($locationNum == 0){
+            throw new GaoDeMapException('经纬度坐标不合法', ErrorCode::MAP_GAODE_PARAM_ERROR);
+        } else if($locationNum > 40){
+            throw new GaoDeMapException('经纬度坐标不能超过40个', ErrorCode::MAP_GAODE_PARAM_ERROR);
+        }
+        $this->reqData['locations'] = implode('|', $locationList);
+    }
+
+    /**
+     * @param string $coordSys
+     * @throws \Exception\Map\GaoDeMapException
+     */
+    public function setCoordSys(string $coordSys){
+        if(in_array($coordSys, ['gps', 'mapbar', 'baidu', 'autonavi'])){
+            $this->reqData['coordsys'] = $coordSys;
+        } else {
+            throw new GaoDeMapException('原坐标系不合法', ErrorCode::MAP_GAODE_PARAM_ERROR);
+        }
+    }
+
+    public function getDetail() : array {
+        if(!isset($this->reqData['locations'])){
+            throw new GaoDeMapException('经纬度坐标不能为空', ErrorCode::MAP_BAIDU_PARAM_ERROR);
+        }
+
+        return $this->getContent();
+    }
+}
