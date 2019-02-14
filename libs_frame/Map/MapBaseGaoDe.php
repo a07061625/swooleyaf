5109fc67e6bdf9b1a4e6771cf5eb2075386267ba
jiangwei <jiangwei07061625@163.com>
2019-02-14 14:44:59 +0800

    map add gaode

---
+++ libs_frame/Map/MapBaseGaoDe.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 姜伟
+ * Date: 19-2-14
+ * Time: 下午12:47
+ */
+namespace Map;
+
+use DesignPatterns\Singletons\MapSingleton;
+
+abstract class MapBaseGaoDe extends MapBase {
+    /**
+     * 应用key
+     * @var string
+     */
+    private $key = '';
+    /**
+     * 服务域名
+     * @var string
+     */
+    private $serviceDomain = '';
+    /**
+     * 服务uri
+     * @var string
+     */
+    protected $serviceUri = '';
+
+    public function __construct(){
+        parent::__construct();
+        $this->serviceDomain = 'https://restapi.amap.com/v3';
+        $this->key = MapSingleton::getInstance()->getGaoDeConfig()->getKey();
+    }
+
+    public function __clone(){
+    }
+
+    protected function getServiceUrl() {
+        return $this->serviceDomain . $this->serviceUri;
+    }
+
+    protected function getContent() : array {
+        $this->reqData['key'] = $this->key;
+        $this->reqData['output'] = 'JSON';
+        MapUtilGaoDe::createSign($this->reqData);
+        $this->curlConfigs[CURLOPT_URL] = $this->getServiceUrl() . '?' . http_build_query($this->reqData);
+        return $this->curlConfigs;
+    }
+}
