668219c660f89c8d244f6ca4aabcadc0d6ba1cef
jiangwei <jiangwei07061625@163.com>
2019-02-19 00:27:27 +0800

    update reflect

--- libs_frame/Reflection/BaseReflect.php
+++ libs_frame/Reflection/BaseReflect.php
@@ -21,42 +21,41 @@ class BaseReflect {
     use SimpleTrait;
 
     /**
-     * 获取方法自定义注解
-     * 自定义注解格式如下：
-     *   @name   -    xxx
+     * 获取控制器过滤器注解
+     * 过滤器注解格式如下：
+     *   name   -    xxx
      *   注解名 分隔符 注解值
      * @param string $className 类全名
      * @param string $methodName 方法名称
+     * @param string $controllerType
      * @return array
      * @throws \Exception\Reflection\ReflectException
      */
-    public static function getMethodDefinedAnnotations(string $className,string $methodName) : array {
+    public static function getControllerFilterAnnotations(string $className,string $methodName,string &$controllerType) : array {
         $annotations = [];
 
         try{
             $class = new \ReflectionClass($className);
-            $doc = $class->getMethod($methodName)->getDocComment();
-            $docs = preg_filter('/\s+/', '', explode(PHP_EOL, $doc));
-            foreach ($docs as $eDoc) {
-                preg_match('/(\@[a-zA-Z0-9]+)\-(\{\S+\})/', $eDoc, $saveDoc);
-                if (!empty($saveDoc)) {
-                    if(empty($annotations[$saveDoc[1]])){
-                        $annotations[$saveDoc[1]] = [];
-                    }
-
-                    $annotations[$saveDoc[1]][] = $saveDoc[2];
-                }
-            }
-
             $instance = $class->newInstanceWithoutConstructor();
             if ($instance instanceof BaseController) {
-                if(!isset($annotations[Validator::ANNOTATION_NAME])){
-                    $annotations[Validator::ANNOTATION_NAME] = [];
+                $doc = $class->getMethod($methodName)->getDocComment();
+                $docs = preg_filter('/\s+/', '', explode(PHP_EOL, $doc));
+                foreach ($docs as $eDoc) {
+                    preg_match('/(\@[a-zA-Z0-9]+)\-(\{\S+\})/', $eDoc, $saveDoc);
+                    if (isset($saveDoc[1]) && ($saveDoc[1] == Validator::ANNOTATION_NAME)) {
+                        $annotations[] = $saveDoc[2];
+                    }
                 }
 
-                if((SY_SERVER_TYPE == Server::SERVER_TYPE_API_GATE) && $instance->signStatus){
-                    $annotations[Validator::ANNOTATION_NAME][] = '{"field":"' . Validator::ANNOTATION_TAG_SIGN . '","explain":"签名值","type":"string","rules":{"sign":3600,"required":1}}';
+                if (SY_SERVER_TYPE == Server::SERVER_TYPE_API_GATE) {
+                    $controllerType = $instance->signStatus ? 'a1' : 'a2';
+                } else if (SY_SERVER_TYPE == Server::SERVER_TYPE_API_MODULE) {
+                    $controllerType = 'b1';
+                } else {
+                    $controllerType = 'c1';
                 }
+            } else {
+                $controllerType = '';
             }
         } catch (ReflectException $e) {
             throw $e;
@@ -76,38 +75,42 @@ class BaseReflect {
      */
     public static function getValidatorAnnotations(string $className,string $methodName) : array {
         $resArr = [];
-        $annotations = self::getMethodDefinedAnnotations($className, $methodName);
-        if (isset($annotations[Validator::ANNOTATION_NAME]) && !empty($annotations[Validator::ANNOTATION_NAME])) {
-            $ignoreSign = SY_SERVER_TYPE == Server::SERVER_TYPE_API_GATE ? false : true;
-            foreach ($annotations[Validator::ANNOTATION_NAME] as $eAnnotation) {
-                $data = Tool::jsonDecode($eAnnotation);
-                if(!is_array($data)){
-                    throw new ReflectException('数据校验格式不正确', ErrorCode::REFLECT_ANNOTATION_DATA_ERROR);
-                } else if(!is_string($data['field'])){
-                    throw new ReflectException('字段名称必须为字符串', ErrorCode::REFLECT_ANNOTATION_DATA_ERROR);
-                } else if(!is_string($data['explain'])){
-                    throw new ReflectException('字段解释必须为字符串', ErrorCode::REFLECT_ANNOTATION_DATA_ERROR);
-                } else if(!is_string($data['type'])){
-                    throw new ReflectException('校验器类型必须为字符串', ErrorCode::REFLECT_ANNOTATION_DATA_ERROR);
-                } else if(!is_array($data['rules'])){
-                    throw new ReflectException('校验规则必须为数组', ErrorCode::REFLECT_ANNOTATION_DATA_ERROR);
-                }
+        $controllerType = '';
+        $annotations = self::getControllerFilterAnnotations($className, $methodName, $controllerType);
+        if (strlen($controllerType) > 0) {
+            if (!empty($annotations)) {
+                foreach ($annotations as $eAnnotation) {
+                    $data = Tool::jsonDecode($eAnnotation);
+                    if(!is_array($data)){
+                        throw new ReflectException('数据校验格式不正确', ErrorCode::REFLECT_ANNOTATION_DATA_ERROR);
+                    } else if(!is_string($data['field'])){
+                        throw new ReflectException('字段名称必须为字符串', ErrorCode::REFLECT_ANNOTATION_DATA_ERROR);
+                    } else if(!is_string($data['explain'])){
+                        throw new ReflectException('字段解释必须为字符串', ErrorCode::REFLECT_ANNOTATION_DATA_ERROR);
+                    } else if(!is_string($data['type'])){
+                        throw new ReflectException('校验器类型必须为字符串', ErrorCode::REFLECT_ANNOTATION_DATA_ERROR);
+                    } else if(!is_array($data['rules'])){
+                        throw new ReflectException('校验规则必须为数组', ErrorCode::REFLECT_ANNOTATION_DATA_ERROR);
+                    }
 
-                if($data['field'] != Validator::ANNOTATION_TAG_IGNORE_SIGN){
-                    $result = new ValidatorResult();
-                    $result->setExplain($data['explain']);
-                    $result->setField($data['field']);
-                    $result->setType($data['type']);
-                    $result->setRules($data['rules']);
-                    $resArr[$data['field']] = $result;
-                } else if(!$ignoreSign){
-                    $ignoreSign = true;
+                    if ($data['field'] != Validator::ANNOTATION_TAG_IGNORE_SIGN) {
+                        if ($data['field'] != Validator::ANNOTATION_TAG_SIGN) {
+                            $result = new ValidatorResult();
+                            $result->setExplain($data['explain']);
+                            $result->setField($data['field']);
+                            $result->setType($data['type']);
+                            $result->setRules($data['rules']);
+                            $resArr[$data['field']] = $result;
+                        }
+                    } else if ($controllerType == 'a1') {
+                        $controllerType = 'a2';
+                    }
                 }
             }
-            if($ignoreSign){
-                unset($resArr[Validator::ANNOTATION_TAG_SIGN]);
-            }
-            if (in_array(SY_SERVER_TYPE, [Server::SERVER_TYPE_API_GATE, Server::SERVER_TYPE_FRONT_GATE])) {
+
+            if ($controllerType == 'b1') {
+                unset($resArr[Validator::ANNOTATION_TAG_SY_TOKEN]);
+            } else {
                 $tokenResult = new ValidatorResult();
                 $tokenResult->setExplain('令牌');
                 $tokenResult->setField(Validator::ANNOTATION_TAG_SY_TOKEN);
@@ -116,8 +119,18 @@ class BaseReflect {
                     'sytoken' => 1,
                 ]);
                 $resArr[Validator::ANNOTATION_TAG_SY_TOKEN] = $tokenResult;
-            } else {
-                unset($resArr[Validator::ANNOTATION_TAG_SY_TOKEN]);
+
+                if ($controllerType == 'a1') {
+                    $signResult = new ValidatorResult();
+                    $signResult->setExplain('签名值');
+                    $signResult->setField(Validator::ANNOTATION_TAG_SIGN);
+                    $signResult->setType('string');
+                    $signResult->setRules([
+                        'sign' => 3600,
+                        'required' => 1,
+                    ]);
+                    $resArr[Validator::ANNOTATION_TAG_SIGN] = $signResult;
+                }
             }
         }
 
