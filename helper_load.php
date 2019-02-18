48ff2c06b60f3e0981485b4935dcadf3dd26d59d
jiangwei <jiangwei07061625@163.com>
2019-02-18 17:46:20 +0800

    frame add constant SY_TOKEN

--- helper_load.php
+++ helper_load.php
@@ -5,8 +5,8 @@ define('SY_PROJECT', 'z01');
 define('SY_DATABASE', true);
 define('SY_LC_WX_ACCOUNT', false);
 define('SY_LC_WXOPEN_AUTHORIZER', false);
-define('SY_TIMER', true);
 define('SY_PROJECT_LIBS_ROOT', __DIR__ . '/libs_project/');
+define('SY_TOKEN', '245dcbf2');
 
 $frameLibsDir = \Yaconf::get('project.' . SY_ENV . SY_PROJECT . '.dir.libs.frame');
 if(substr($frameLibsDir, -1) == '/'){
