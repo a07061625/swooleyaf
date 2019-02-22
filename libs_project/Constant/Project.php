d43e62eac19858a27287de1cd9e4bf518ad595b7
jiangwei <jiangwei07061625@163.com>
2019-02-22 19:30:22 +0800

    server preprocess trait add demo

--- libs_project/Constant/Project.php
+++ libs_project/Constant/Project.php
@@ -142,6 +142,7 @@ final class Project extends ProjectBase {
     const WXMINI_EXPIRE_TOKEN = 7000; //小程序token超时时间,单位为秒
     const WXMINI_DEFAULT_CLIENT_IP = '127.0.0.1'; //默认客户端IP
 
+    //服务预处理常量,标识长度为5位,第一位固定为/,后四位代表不同预处理操作,其中后四位全为数字的为框架内部预留标识
     const PRE_PROCESS_TAG_HTTP_PROJECT_TEST = '/a000'; //HTTP服务项目标识-测试
     const PRE_PROCESS_TAG_RPC_PROJECT_TEST = '/a000'; //RPC服务项目标识-测试
 }
