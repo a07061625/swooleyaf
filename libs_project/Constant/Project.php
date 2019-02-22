6c47874f7b0a555c0af09b6c5091d1352f566384
jiangwei <jiangwei07061625@163.com>
2019-02-22 19:29:47 +0800

    server preprocess trait add demo

--- libs_project/Constant/Project.php
+++ libs_project/Constant/Project.php
@@ -48,6 +48,7 @@ final class Project extends ProjectBase {
     const MESSAGE_QUEUE_TOPIC_REQ_HEALTH_CHECK = 'a001'; //主题-请求健康检查
     const MESSAGE_QUEUE_TOPIC_TEST = SY_ENV . SY_PROJECT . '0000'; //主题-测试
 
+    //服务预处理常量,标识长度为5位,第一位固定为/,后四位代表不同预处理操作,其中后四位全为数字的为框架内部预留标识
     const PRE_PROCESS_TAG_HTTP_PROJECT_TEST = '/a000'; //HTTP服务项目标识-测试
     const PRE_PROCESS_TAG_RPC_PROJECT_TEST = '/a000'; //RPC服务项目标识-测试
 }
