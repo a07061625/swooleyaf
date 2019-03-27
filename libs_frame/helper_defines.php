f933d148eb018c41187da8022082f872b932f272
jiangwei <jiangwei07061625@163.com>
2019-03-27 18:17:18 +0800

    frame version update to 6.3.2;fixed bug for rpc request fail unknown reason

--- libs_frame/helper_defines.php
+++ libs_frame/helper_defines.php
@@ -8,7 +8,7 @@
 ini_set('display_errors', 'On');
 error_reporting(E_ALL);
 date_default_timezone_set('PRC');
-define('SY_VERSION', '6.3.1');
+define('SY_VERSION', '6.3.2');
 $syLogPath = ini_get('seaslog.default_basepath');
 if(substr($syLogPath, -1) == '/'){
     define('SY_LOG_PATH', $syLogPath . 'sy' . SY_PROJECT);
