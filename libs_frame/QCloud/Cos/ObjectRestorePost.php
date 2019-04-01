82b30737bef9685f6ad1f5ad78627f8026a6ff4d
jiangwei <jiangwei07061625@163.com>
2019-04-02 07:47:05 +0800

    qcloud add cos

---
+++ libs_frame/QCloud/Cos/ObjectRestorePost.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * User: 姜伟
+ * Date: 2019/3/30 0030
+ * Time: 18:24
+ */
+namespace QCloud\Cos;
+
+use Constant\ErrorCode;
+use Exception\QCloud\CosException;
+use QCloud\CloudBaseCos;
+use Tool\Tool;
+
+/**
+ * 恢复archive类型的对象
+ * @package QCloud\Cos
+ */
+class ObjectRestorePost extends CloudBaseCos {
+    /**
+     * 对象名称
+     * @var string
+     */
+    private $objectKey = '';
+
+    public function __construct(){
+        parent::__construct();
+        $this->setReqHost();
+        $this->setReqMethod(self::REQ_METHOD_POST);
+    }
+
+    private function __clone(){
+    }
+
+    /**
+     * @param string $objectKey
+     * @throws \Exception\QCloud\CosException
+     */
+    public function setObjectKey(string $objectKey){
+        if(strlen($objectKey) > 0){
+            $this->reqUri = '/' . $objectKey . '?restore';
+            $this->objectKey = $objectKey;
+            $this->signParams['restore'] = '';
+        } else {
+            throw new CosException('对象名称不合法', ErrorCode::QCLOUD_COS_PARAM_ERROR);
+        }
+    }
+
+    /**
+     * @param array $restoreConfig
+     * @throws \Exception\QCloud\CosException
+     */
+    public function setRestoreConfig(array $restoreConfig){
+        if(empty($restoreConfig)){
+            throw new CosException('恢复配置不合法', ErrorCode::QCLOUD_COS_PARAM_ERROR);
+        }
+
+        $this->reqData = $restoreConfig;
+    }
+
+    public function getDetail() : array {
+        if(strlen($this->objectKey) == 0){
+            throw new CosException('对象名称不能为空', ErrorCode::QCLOUD_COS_PARAM_ERROR);
+        }
+        if(empty($this->reqData)){
+            throw new CosException('恢复配置不能为空', ErrorCode::QCLOUD_COS_PARAM_ERROR);
+        }
+        $this->curlConfigs[CURLOPT_POSTFIELDS] = Tool::arrayToXml($this->reqData);
+
+        return $this->getContent();
+    }
+}
