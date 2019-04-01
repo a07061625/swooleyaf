30eead23b5eb5e25e1e578365b2b4c9e03786a0f
jiangwei <jiangwei07061625@163.com>
2019-04-01 17:59:10 +0800

    qcloud add cos

---
+++ libs_frame/QCloud/Cos/MultipartUploadAbort.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * User: 姜伟
+ * Date: 2019/3/30 0030
+ * Time: 18:24
+ */
+namespace QCloud\Cos;
+
+use Constant\ErrorCode;
+use Exception\QCloud\CosException;
+use QCloud\CloudBaseCos;
+
+/**
+ * 舍弃并删除一个上传分块
+ * @package QCloud\Cos
+ */
+class MultipartUploadAbort extends CloudBaseCos {
+    /**
+     * 对象名称
+     * @var string
+     */
+    private $objectKey = '';
+    /**
+     * 上传ID
+     * @var string
+     */
+    private $uploadId = '';
+
+    public function __construct(){
+        parent::__construct();
+        $this->setReqHost();
+        $this->setReqMethod(self::REQ_METHOD_DELETE);
+    }
+
+    private function __clone(){
+    }
+
+    /**
+     * @param string $objectKey
+     * @throws \Exception\QCloud\CosException
+     */
+    public function setObjectKey(string $objectKey){
+        if(strlen($objectKey) > 0){
+            $this->reqUri = '/' . $objectKey;
+            $this->objectKey = $objectKey;
+        } else {
+            throw new CosException('对象名称不合法', ErrorCode::QCLOUD_COS_PARAM_ERROR);
+        }
+    }
+
+    /**
+     * @param string $uploadId
+     * @throws \Exception\QCloud\CosException
+     */
+    public function setUploadId(string $uploadId){
+        if(strlen($uploadId) > 0){
+            $this->signParams['uploadid'] = $uploadId;
+            $this->reqData['uploadId'] = $uploadId;
+        } else {
+            throw new CosException('上传ID不合法', ErrorCode::QCLOUD_COS_PARAM_ERROR);
+        }
+    }
+
+    public function getDetail() : array {
+        if(strlen($this->objectKey) == 0){
+            throw new CosException('对象名称不能为空', ErrorCode::QCLOUD_COS_PARAM_ERROR);
+        }
+        if(!isset($this->reqData['uploadId'])){
+            throw new CosException('上传ID不能为空', ErrorCode::QCLOUD_COS_PARAM_ERROR);
+        }
+        $this->setReqQuery($this->reqData);
+        return $this->getContent();
+    }
+}
---
+++ libs_frame/QCloud/Cos/ObjectAclGet.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * User: 姜伟
+ * Date: 2019/3/30 0030
+ * Time: 18:24
+ */
+namespace QCloud\Cos;
+
+use Constant\ErrorCode;
+use Exception\QCloud\CosException;
+use QCloud\CloudBaseCos;
+
+/**
+ * 获取对象的访问权限
+ * @package QCloud\Cos
+ */
+class ObjectAclGet extends CloudBaseCos {
+    /**
+     * 对象名称
+     * @var string
+     */
+    private $objectKey = '';
+
+    public function __construct(){
+        parent::__construct();
+        $this->setReqHost();
+        $this->setReqMethod(self::REQ_METHOD_GET);
+    }
+
+    private function __clone(){
+    }
+
+    /**
+     * @param string $objectKey
+     * @throws \Exception\QCloud\CosException
+     */
+    public function setObjectKey(string $objectKey){
+        if(strlen($objectKey) > 0){
+            $this->reqUri = '/' . $objectKey . '?acl';
+            $this->objectKey = $objectKey;
+            $this->signParams['acl'] = '';
+        } else {
+            throw new CosException('对象名称不合法', ErrorCode::QCLOUD_COS_PARAM_ERROR);
+        }
+    }
+
+    public function getDetail() : array {
+        if(strlen($this->objectKey) == 0){
+            throw new CosException('对象名称不能为空', ErrorCode::QCLOUD_COS_PARAM_ERROR);
+        }
+
+        return $this->getContent();
+    }
+}
