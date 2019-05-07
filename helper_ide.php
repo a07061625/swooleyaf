023a90ac5808f5a36d8fed11ca8e347f3f0235fd
jiangwei <jiangwei07061625@163.com>
2019-05-07 17:16:19 +0800

    add code sniffer

--- helper_ide.php
+++ helper_ide.php
@@ -1,10 +1,12 @@
 <?php
 require_once __DIR__ . '/helper_load.php';
 
-class IDETool {
+class IDETool
+{
     use \Traits\SimpleTrait;
 
-    public static function generator(){
+    public static function generator()
+    {
         $option = strtolower(\Tool\Tool::getClientOption(1, true, ''));
         switch ($option) {
             case 'rdkafka':
@@ -33,7 +35,8 @@ class IDETool {
         }
     }
 
-    private static function help(){
+    private static function help()
+    {
         echo '显示帮助: php helper_ide.php -h' . PHP_EOL;
         echo '生成扩展帮助文件: php helper_ide.php xxx' . PHP_EOL;
         echo '    xxx:PHP扩展名称,可以通过命令[php -m]获取' . PHP_EOL;
--- helper_sytask.php
+++ helper_sytask.php
@@ -1,13 +1,13 @@
 <?php
 require_once __DIR__ . '/helper_load.php';
 
-$projects = include(__DIR__ . '/helper_projects.php');
+$projects = include __DIR__ . '/helper_projects.php';
 $timeArr = explode('-', date('H-i'));
 $minute = (int)$timeArr[1];
 $hour = (int)$timeArr[0];
 
 //定时检测数据库连接
-if(in_array($minute, [0, 30])){
+if (in_array($minute, [0, 30], true)) {
     $dbCheck = new \Helper\DbCheck();
     $dbCheck->check($projects);
 }
