7f2e1923a55cbed9de7be3b13029700f92d096d7
jiangwei <jiangwei07061625@163.com>
2019-05-23 18:00:24 +0800

    alipay config move common part to libs_frame

--- libs_project/Traits/AliPayConfigTrait.php
+++ libs_project/Traits/AliPayConfigTrait.php
@@ -15,18 +15,6 @@ use Tool\Tool;
 trait AliPayConfigTrait
 {
     /**
-     * 支付配置列表
-     * @var array
-     */
-    private $payConfigs = [];
-
-    /**
-     * 支付配置清理时间戳
-     * @var int
-     */
-    private $payClearTime = 0;
-
-    /**
      * 更新支付配置
      * @param string $appId
      * @return \AliPay\PayConfig
@@ -57,57 +45,5 @@ trait AliPayConfigTrait
         $this->payConfigs[$appId] = $payConfig;
 
         return $payConfig;
-    }
-
-    /**
-     * 获取支付配置
-     * @param string $appId
-     * @return \AliPay\PayConfig|null
-     */
-    public function getPayConfig(string $appId)
-    {
-        $nowTime = Tool::getNowTime();
-        $payConfig = $this->getLocalPayConfig($appId);
-        if (is_null($payConfig)) {
-            $payConfig = $this->refreshPayConfig($appId);
-        } elseif ($payConfig->getExpireTime() < $nowTime) {
-            $payConfig = $this->refreshPayConfig($appId);
-        }
-
-        return $payConfig->isValid() ? $payConfig : null;
-    }
-
-    /**
-     * 移除支付配置
-     * @param string $appId
-     */
-    public function removePayConfig(string $appId)
-    {
-        unset($this->payConfigs[$appId]);
-    }
-
-    /**
-     * 获取本地支付配置
-     * @param string $appId
-     * @return \AliPay\PayConfig|null
-     */
-    private function getLocalPayConfig(string $appId)
-    {
-        $nowTime = Tool::getNowTime();
-        if ($this->payClearTime < $nowTime) {
-            $delIds = [];
-            foreach ($this->payConfigs as $eAppId => $payConfig) {
-                if ($payConfig->getExpireTime() < $nowTime) {
-                    $delIds[] = $eAppId;
-                }
-            }
-            foreach ($delIds as $eAppId) {
-                unset($this->payConfigs[$eAppId]);
-            }
-
-            $this->payClearTime = $nowTime + Project::TIME_EXPIRE_LOCAL_ALIPAY_CLEAR;
-        }
-
-        return Tool::getArrayVal($this->payConfigs, $appId, null);
     }
 }
