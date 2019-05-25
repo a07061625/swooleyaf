c846ca5029986e639c102f583176017d16140193
jiangwei <jiangwei07061625@163.com>
2019-05-25 17:48:43 +0800

    add aspect test

---
+++ libs_project/SyAspect/Test.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 姜伟
+ * Date: 2019/5/25 0025
+ * Time: 17:30
+ */
+namespace SyAspect;
+
+use Log\Log;
+use Traits\SimpleTrait;
+
+class Test extends BaseAspect
+{
+    use SimpleTrait;
+
+    public static function handleBefore()
+    {
+        Log::log('before aspect');
+    }
+
+    public static function handleAfter()
+    {
+        Log::log('after aspect');
+    }
+}
