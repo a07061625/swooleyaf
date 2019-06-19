b0f17bbbfc5c554e19483f55111af10ee13a373c
jiangwei <jiangwei07061625@163.com>
2019-06-19 11:07:36 +0800

    frame add logistics for kuaidi100

--- libs_frame/DesignPatterns/Singletons/IMConfigSingleton.php
+++ libs_frame/DesignPatterns/Singletons/IMConfigSingleton.php
@@ -23,16 +23,6 @@ class IMConfigSingleton
 
     private function __construct()
     {
-        $configs = Tool::getConfig('im.' . SY_ENV . SY_PROJECT);
-
-        //设置大鱼配置
-        $tencentConfig = new TencentConfig();
-        $tencentConfig->setAppId((string)Tool::getArrayVal($configs, 'tencent.app.id', '', true));
-        $tencentConfig->setPrivateKey((string)Tool::getArrayVal($configs, 'tencent.private.key', '', true));
-        $tencentConfig->setCommandSign((string)Tool::getArrayVal($configs, 'tencent.command.sign', '', true));
-        $tencentConfig->setAccountAdmin((string)Tool::getArrayVal($configs, 'tencent.account.admin', '', true));
-        $tencentConfig->setAccountType((string)Tool::getArrayVal($configs, 'tencent.account.type', '', true));
-        $this->tencentConfig = $tencentConfig;
     }
 
     /**
@@ -52,6 +42,17 @@ class IMConfigSingleton
      */
     public function getTencentConfig()
     {
+        if (is_null($this->tencentConfig)) {
+            $configs = Tool::getConfig('im.' . SY_ENV . SY_PROJECT);
+            $tencentConfig = new TencentConfig();
+            $tencentConfig->setAppId((string)Tool::getArrayVal($configs, 'tencent.app.id', '', true));
+            $tencentConfig->setPrivateKey((string)Tool::getArrayVal($configs, 'tencent.private.key', '', true));
+            $tencentConfig->setCommandSign((string)Tool::getArrayVal($configs, 'tencent.command.sign', '', true));
+            $tencentConfig->setAccountAdmin((string)Tool::getArrayVal($configs, 'tencent.account.admin', '', true));
+            $tencentConfig->setAccountType((string)Tool::getArrayVal($configs, 'tencent.account.type', '', true));
+            $this->tencentConfig = $tencentConfig;
+        }
+
         return $this->tencentConfig;
     }
 }
--- libs_frame/DesignPatterns/Singletons/WxConfigSingleton.php
+++ libs_frame/DesignPatterns/Singletons/WxConfigSingleton.php
@@ -55,37 +55,6 @@ class WxConfigSingleton
 
     private function __construct()
     {
-        $configs = Tool::getConfig('wx.' . SY_ENV . SY_PROJECT);
-
-        //初始化开放平台公共配置
-        $openCommonConfig = new WxConfigOpenCommon();
-        $openCommonConfig->setExpireComponentAccessToken((int)Tool::getArrayVal($configs, 'open.expire.component.accesstoken', 0, true));
-        $openCommonConfig->setExpireAuthorizerJsTicket((int)Tool::getArrayVal($configs, 'open.expire.authorizer.jsticket', 0, true));
-        $openCommonConfig->setExpireAuthorizerAccessToken((int)Tool::getArrayVal($configs, 'open.expire.authorizer.accesstoken', 0, true));
-        $openCommonConfig->setAppId((string)Tool::getArrayVal($configs, 'open.appid', '', true));
-        $openCommonConfig->setSecret((string)Tool::getArrayVal($configs, 'open.secret', '', true));
-        $openCommonConfig->setToken((string)Tool::getArrayVal($configs, 'open.token', '', true));
-        $openCommonConfig->setAesKeyBefore((string)Tool::getArrayVal($configs, 'open.aeskey.before', '', true));
-        $openCommonConfig->setAesKeyNow((string)Tool::getArrayVal($configs, 'open.aeskey.now', '', true));
-        $openCommonConfig->setUrlAuth((string)Tool::getArrayVal($configs, 'open.url.auth', '', true));
-        $openCommonConfig->setUrlAuthCallback((string)Tool::getArrayVal($configs, 'open.url.authcallback', '', true));
-        $openCommonConfig->setUrlMiniRebindAdmin((string)Tool::getArrayVal($configs, 'open.url.mini.rebindadmin', '', true));
-        $openCommonConfig->setUrlMiniFastRegister((string)Tool::getArrayVal($configs, 'open.url.mini.fastregister', '', true));
-        $openCommonConfig->setDomainMiniServers((array)Tool::getArrayVal($configs, 'open.domain.mini.server', [], true));
-        $openCommonConfig->setDomainMiniWebViews((array)Tool::getArrayVal($configs, 'open.domain.mini.webview', [], true));
-        $this->openCommonConfig = $openCommonConfig;
-
-        //初始化企业服务商公共配置
-        $corpProviderConfig = new WxConfigCorpProvider();
-        $corpProviderConfig->setCorpId((string)Tool::getArrayVal($configs, 'provider.corp.id', '', true));
-        $corpProviderConfig->setCorpSecret((string)Tool::getArrayVal($configs, 'provider.corp.secret', '', true));
-        $corpProviderConfig->setToken((string)Tool::getArrayVal($configs, 'provider.token', '', true));
-        $corpProviderConfig->setAesKey((string)Tool::getArrayVal($configs, 'provider.aeskey', '', true));
-        $corpProviderConfig->setSuiteId((string)Tool::getArrayVal($configs, 'provider.suite.id', '', true));
-        $corpProviderConfig->setSuiteSecret((string)Tool::getArrayVal($configs, 'provider.suite.secret', '', true));
-        $corpProviderConfig->setUrlAuthSuite((string)Tool::getArrayVal($configs, 'provider.url.auth.suite', '', true));
-        $corpProviderConfig->setUrlAuthLogin((string)Tool::getArrayVal($configs, 'provider.url.auth.login', '', true));
-        $this->corpProviderConfig = $corpProviderConfig;
     }
 
     private function __clone()
@@ -110,6 +79,26 @@ class WxConfigSingleton
      */
     public function getOpenCommonConfig()
     {
+        if (is_null($this->openCommonConfig)) {
+            $configs = Tool::getConfig('wx.' . SY_ENV . SY_PROJECT);
+            $openCommonConfig = new WxConfigOpenCommon();
+            $openCommonConfig->setExpireComponentAccessToken((int)Tool::getArrayVal($configs, 'open.expire.component.accesstoken', 0, true));
+            $openCommonConfig->setExpireAuthorizerJsTicket((int)Tool::getArrayVal($configs, 'open.expire.authorizer.jsticket', 0, true));
+            $openCommonConfig->setExpireAuthorizerAccessToken((int)Tool::getArrayVal($configs, 'open.expire.authorizer.accesstoken', 0, true));
+            $openCommonConfig->setAppId((string)Tool::getArrayVal($configs, 'open.appid', '', true));
+            $openCommonConfig->setSecret((string)Tool::getArrayVal($configs, 'open.secret', '', true));
+            $openCommonConfig->setToken((string)Tool::getArrayVal($configs, 'open.token', '', true));
+            $openCommonConfig->setAesKeyBefore((string)Tool::getArrayVal($configs, 'open.aeskey.before', '', true));
+            $openCommonConfig->setAesKeyNow((string)Tool::getArrayVal($configs, 'open.aeskey.now', '', true));
+            $openCommonConfig->setUrlAuth((string)Tool::getArrayVal($configs, 'open.url.auth', '', true));
+            $openCommonConfig->setUrlAuthCallback((string)Tool::getArrayVal($configs, 'open.url.authcallback', '', true));
+            $openCommonConfig->setUrlMiniRebindAdmin((string)Tool::getArrayVal($configs, 'open.url.mini.rebindadmin', '', true));
+            $openCommonConfig->setUrlMiniFastRegister((string)Tool::getArrayVal($configs, 'open.url.mini.fastregister', '', true));
+            $openCommonConfig->setDomainMiniServers((array)Tool::getArrayVal($configs, 'open.domain.mini.server', [], true));
+            $openCommonConfig->setDomainMiniWebViews((array)Tool::getArrayVal($configs, 'open.domain.mini.webview', [], true));
+            $this->openCommonConfig = $openCommonConfig;
+        }
+
         return $this->openCommonConfig;
     }
 
@@ -119,6 +108,20 @@ class WxConfigSingleton
      */
     public function getCorpProviderConfig()
     {
+        if (is_null($this->corpProviderConfig)) {
+            $configs = Tool::getConfig('wx.' . SY_ENV . SY_PROJECT);
+            $corpProviderConfig = new WxConfigCorpProvider();
+            $corpProviderConfig->setCorpId((string)Tool::getArrayVal($configs, 'provider.corp.id', '', true));
+            $corpProviderConfig->setCorpSecret((string)Tool::getArrayVal($configs, 'provider.corp.secret', '', true));
+            $corpProviderConfig->setToken((string)Tool::getArrayVal($configs, 'provider.token', '', true));
+            $corpProviderConfig->setAesKey((string)Tool::getArrayVal($configs, 'provider.aeskey', '', true));
+            $corpProviderConfig->setSuiteId((string)Tool::getArrayVal($configs, 'provider.suite.id', '', true));
+            $corpProviderConfig->setSuiteSecret((string)Tool::getArrayVal($configs, 'provider.suite.secret', '', true));
+            $corpProviderConfig->setUrlAuthSuite((string)Tool::getArrayVal($configs, 'provider.url.auth.suite', '', true));
+            $corpProviderConfig->setUrlAuthLogin((string)Tool::getArrayVal($configs, 'provider.url.auth.login', '', true));
+            $this->corpProviderConfig = $corpProviderConfig;
+        }
+
         return $this->corpProviderConfig;
     }
 
