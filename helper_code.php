9246fd037a9a9ad56e31ffa6b88fdb65fe5886a8
jiangwei <jiangwei07061625@163.com>
2019-07-09 14:52:50 +0800

    add helper_coder.php for code encrypt and decrypt

---
+++ helper_code.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * 代码处理
+ * code: 12e8df4f68a600f6c1b11638_164952005f9fff2a8efeea3aa6e3d3c6
+ * User: 姜伟
+ * Date: 2019/7/9 0009
+ * Time: 9:08
+ */
+
+require_once __DIR__ . '/helper_load.php';
+
+function syhelp_code()
+{
+    echo '显示帮助: /usr/local/php7/bin/php helper_code.php -h' . PHP_EOL;
+    echo '代码加密: /usr/local/php7/bin/php helper_code.php encrypt -file xxx.php -key xxx' . PHP_EOL;
+    echo '    -file:必填 待加密文件或者文件目录,从根目录/开始' . PHP_EOL;
+    echo '    -key:必填 加密密钥' . PHP_EOL;
+    echo '代码解密: /usr/local/php7/bin/php helper_code.php decrypt -file xxx.php -key xxx' . PHP_EOL;
+    echo '    -file:必填 待加密文件或者文件目录,从根目录/开始' . PHP_EOL;
+    echo '    -key:必填 加密密钥' . PHP_EOL;
+}
+
+$option = \Tool\Tool::getClientOption(1, true);
+switch ($option) {
+    case 'encrypt':
+        $file = trim(\Tool\Tool::getClientOption('-file', false, ''));
+        $key = trim(\Tool\Tool::getClientOption('-key', false, ''));
+        \Helper\CodeTool::generator($file, $key, 'encrypt');
+        break;
+    case 'decrypt':
+        $file = trim(\Tool\Tool::getClientOption('-file', false, ''));
+        $key = trim(\Tool\Tool::getClientOption('-key', false, ''));
+        \Helper\CodeTool::generator($file, $key, 'decrypt');
+        break;
+    default:
+        syhelp_code();
+        break;
+}
