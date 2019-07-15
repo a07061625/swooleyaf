581743437f9a91c368e2ed16920384eaac563506
jiangwei <jiangwei07061625@163.com>
2019-07-15 12:42:36 +0800

    syiot add aliyun

---
+++ libs_frame/DesignPatterns/Singletons/IotConfigSingleton.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 姜伟
+ * Date: 2019/6/18 0018
+ * Time: 16:42
+ */
+namespace DesignPatterns\Singletons;
+
+use SyIot\ConfigAliYun;
+use Tool\Tool;
+use Traits\SingletonTrait;
+
+class IotConfigSingleton
+{
+    use SingletonTrait;
+    /**
+     * @var \SyIot\ConfigAliYun
+     */
+    private $aliYunConfig = null;
+
+    private function __construct()
+    {
+    }
+
+    /**
+     * @return \DesignPatterns\Singletons\IotConfigSingleton
+     */
+    public static function getInstance()
+    {
+        if (is_null(self::$instance)) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+
+    /**
+     * @return \SyIot\ConfigAliYun
+     */
+    public function getAliYunConfig()
+    {
+        if (is_null($this->aliYunConfig)) {
+            $configs = Tool::getConfig('iot.' . SY_ENV . SY_PROJECT);
+            $aliYunConfig = new ConfigAliYun();
+            $aliYunConfig->setRegionId((string)Tool::getArrayVal($configs, 'aliyun.region.id', '', true));
+            $aliYunConfig->setAccessKey((string)Tool::getArrayVal($configs, 'aliyun.access.key', '', true));
+            $aliYunConfig->setAccessSecret((string)Tool::getArrayVal($configs, 'aliyun.access.secret', '', true));
+            $this->aliYunConfig = $aliYunConfig;
+        }
+
+        return $this->aliYunConfig;
+    }
+}
---
+++ libs_frame/SyIot/ConfigAliYun.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 姜伟
+ * Date: 2019/7/15 0015
+ * Time: 11:30
+ */
+namespace SyIot;
+
+use Constant\ErrorCode;
+use Exception\Iot\AliYunIotException;
+
+class ConfigAliYun
+{
+    /**
+     * 区域ID
+     * @var string
+     */
+    private $regionId = '';
+    /**
+     * 访问ID
+     * @var string
+     */
+    private $accessKey = '';
+    /**
+     * 访问密钥
+     * @var string
+     */
+    private $accessSecret = '';
+
+    public function __construct()
+    {
+    }
+
+    private function __clone()
+    {
+    }
+
+    /**
+     * @return string
+     */
+    public function getRegionId() : string
+    {
+        return $this->regionId;
+    }
+
+    /**
+     * @param string $regionId
+     * @throws \Exception\Iot\AliYunIotException
+     */
+    public function setRegionId(string $regionId)
+    {
+        if (strlen($regionId) > 0) {
+            $this->regionId = $regionId;
+        } else {
+            throw new AliYunIotException('区域ID不合法', ErrorCode::IOT_PARAM_ERROR);
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getAccessKey() : string
+    {
+        return $this->accessKey;
+    }
+
+    /**
+     * @param string $accessKey
+     * @throws \Exception\Iot\AliYunIotException
+     */
+    public function setAccessKey(string $accessKey)
+    {
+        if (ctype_alnum($accessKey)) {
+            $this->accessKey = $accessKey;
+        } else {
+            throw new AliYunIotException('访问ID不合法', ErrorCode::IOT_PARAM_ERROR);
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getAccessSecret() : string
+    {
+        return $this->accessSecret;
+    }
+
+    /**
+     * @param string $accessSecret
+     * @throws \Exception\Iot\AliYunIotException
+     */
+    public function setAccessSecret(string $accessSecret)
+    {
+        if (ctype_alnum($accessSecret)) {
+            $this->accessSecret = $accessSecret;
+        } else {
+            throw new AliYunIotException('访问密钥不合法', ErrorCode::IOT_PARAM_ERROR);
+        }
+    }
+}
