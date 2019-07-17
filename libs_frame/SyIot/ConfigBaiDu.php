f30a686592c206e9f992327e36fba7f4976f75a1
jiangwei <jiangwei07061625@163.com>
2019-07-17 14:39:26 +0800

    iot add baidu

---
+++ libs_frame/SyIot/ConfigBaiDu.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 姜伟
+ * Date: 2019/7/16 0016
+ * Time: 19:21
+ */
+namespace SyIot;
+
+use Constant\ErrorCode;
+use SyException\Iot\BaiDuIotException;
+
+class ConfigBaiDu
+{
+    /**
+     * 访问ID
+     * @var string
+     */
+    private $accessKey = '';
+    /**
+     * 访问密钥
+     * @var string
+     */
+    private $accessSecret = '';
+
+    public function __construct()
+    {
+    }
+
+    private function __clone()
+    {
+    }
+
+    /**
+     * @return string
+     */
+    public function getAccessKey() : string
+    {
+        return $this->accessKey;
+    }
+
+    /**
+     * @param string $accessKey
+     * @throws \SyException\Iot\BaiDuIotException
+     */
+    public function setAccessKey(string $accessKey)
+    {
+        if (ctype_alnum($accessKey)) {
+            $this->accessKey = $accessKey;
+        } else {
+            throw new BaiDuIotException('访问ID不合法', ErrorCode::IOT_PARAM_ERROR);
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getAccessSecret() : string
+    {
+        return $this->accessSecret;
+    }
+
+    /**
+     * @param string $accessSecret
+     * @throws \SyException\Iot\BaiDuIotException
+     */
+    public function setAccessSecret(string $accessSecret)
+    {
+        if (ctype_alnum($accessSecret)) {
+            $this->accessSecret = $accessSecret;
+        } else {
+            throw new BaiDuIotException('访问密钥不合法', ErrorCode::IOT_PARAM_ERROR);
+        }
+    }
+}
---
+++ libs_frame/SyIot/IotBase.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 姜伟
+ * Date: 2019/7/17 0017
+ * Time: 14:09
+ */
+namespace SyIot;
+
+abstract class IotBase
+{
+    /**
+     * 请求数据
+     * @var array
+     */
+    protected $reqData = [];
+    /**
+     * curl配置数组
+     * @var array
+     */
+    protected $curlConfigs = [];
+
+    public function __construct()
+    {
+    }
+
+    private function __clone()
+    {
+    }
+
+    abstract public function getDetail() : array;
+    abstract protected function getContent() : array;
+}
