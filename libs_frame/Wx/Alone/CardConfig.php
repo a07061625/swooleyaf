18e015693cd53773c21ad1fe3caceb23d9b32d57
jiangwei <jiangwei07061625@163.com>
2019-08-01 10:57:48 +0800

    wxalone add cardconfig and cardextconfig

--- libs_frame/Wx/Alone/CardConfig.php
+++ libs_frame/Wx/Alone/CardConfig.php
@@ -1,49 +1,175 @@
 <?php
 /**
- * Created by PhpStorm.
+ * 拉取卡券配置
  * User: 姜伟
  * Date: 2019/7/30 0030
  * Time: 19:13
  */
 namespace Wx\Alone;
 
+use Constant\ErrorCode;
+use DesignPatterns\Singletons\WxConfigSingleton;
+use SyException\Wx\WxException;
+use Tool\Tool;
+use Wx\Shop\Pay\JsConfig;
 use Wx\WxBaseAlone;
+use Wx\WxUtilBase;
+use Wx\WxUtilOpenBase;
+use Wx\WxUtilShop;
 
-/**
- * wx.chooseCard({
-shopId: '', // 门店Id
-cardType: '', // 卡券类型
-cardId: '', // 卡券Id
-timestamp: 0, // 卡券签名时间戳
-nonceStr: '', // 卡券签名随机串
-signType: '', // 签名方式，默认'SHA1'
-cardSign: '', // 卡券签名
-success: function (res) {
-var cardList= res.cardList; // 用户选中的卡券列表信息
-}
-});
-参数名	必填	类型	示例值	描述
-shopId	否	string(24)	1234	门店ID。shopID用于筛选出拉起带有指定location_list(shopID)的卡券列表，非必填。
-cardType	否	string(24)	GROUPON	卡券类型，用于拉起指定卡券类型的卡券列表。当cardType为空时，默认拉起所有卡券的列表，非必填。
-cardId	否	string(32)	p1Pj9jr90_SQRaVqYI239Ka1erk	卡券ID，用于拉起指定cardId的卡券列表，当cardId为空时，默认拉起所有卡券的列表，非必填。
-timestamp	是	string(32)	14300000000	时间戳。
-nonceStr	是	string(32)	sduhi123	随机字符串。
-signType	是	string(32)	SHA1	签名方式，目前仅支持SHA1
-cardSign	是	string(64)	abcsdijcous123	签名。
- */
 class CardConfig extends WxBaseAlone
 {
-    public function __construct()
+    /**
+     * 应用ID
+     * @var string
+     */
+    private $appid = '';
+    /**
+     * 时间戳
+     * @var int
+     */
+    private $timestamp = 0;
+    /**
+     * 随机字符串
+     * @var string
+     */
+    private $nonceStr = '';
+    /**
+     * 签名方式
+     * @var string
+     */
+    private $signType = '';
+    /**
+     * 门店ID
+     * @var string
+     */
+    private $shopId = '';
+    /**
+     * 卡券ID
+     * @var string
+     */
+    private $cardId = '';
+    /**
+     * 卡券类型
+     * @var string
+     */
+    private $cardType = '';
+    /**
+     * 平台类型 shop：公众号 openshop：第三方平台代理公众号
+     * @var string
+     */
+    private $platType = '';
+    /**
+     * JS签名标识,true:需要JS签名 false:不需要JS签名
+     * @var bool
+     */
+    private $needJs = false;
+
+    public function __construct(string $appId)
     {
         parent::__construct();
+        $shopConfig = WxConfigSingleton::getInstance()->getShopConfig($appId);
+        $this->appid = $shopConfig->getAppId();
+        $this->reqData['timestamp'] = Tool::getNowTime();
+        $this->reqData['nonceStr'] = Tool::createNonceStr(32, 'numlower');
+        $this->reqData['signType'] = 'SHA1';
+        $this->reqData['shopId'] = '';
+        $this->reqData['cardType'] = '';
+        $this->reqData['cardId'] = '';
+        $this->platType = WxUtilBase::PLAT_TYPE_SHOP;
     }
 
     private function __clone()
     {
     }
 
+    /**
+     * @param string $shopId
+     * @throws \SyException\Wx\WxException
+     */
+    public function setShopId(string $shopId)
+    {
+        if (strlen($shopId) <= 24) {
+            $this->reqData['shopId'] = $shopId;
+        } else {
+            throw new WxException('门店ID不合法', ErrorCode::WX_PARAM_ERROR);
+        }
+    }
+
+    /**
+     * @param string $cardId
+     * @throws \SyException\Wx\WxException
+     */
+    public function setCardId(string $cardId)
+    {
+        if (strlen($cardId) <= 32) {
+            $this->reqData['cardId'] = $cardId;
+        } else {
+            throw new WxException('卡券ID不合法', ErrorCode::WX_PARAM_ERROR);
+        }
+    }
+
+    /**
+     * @param string $cardType
+     * @throws \SyException\Wx\WxException
+     */
+    public function setCardType(string $cardType)
+    {
+        if (strlen($cardType) <= 24) {
+            $this->reqData['cardType'] = $cardType;
+        } else {
+            throw new WxException('卡券类型不合法', ErrorCode::WX_PARAM_ERROR);
+        }
+    }
+
+    /**
+     * @param string $platType 平台类型 shop：公众号 openshop：第三方平台代理公众号
+     * @throws \SyException\Wx\WxException
+     */
+    public function setPlatType(string $platType)
+    {
+        if (in_array($platType, [WxUtilBase::PLAT_TYPE_SHOP, WxUtilBase::PLAT_TYPE_OPEN_SHOP], true)) {
+            $this->platType = $platType;
+        } else {
+            throw new WxException('平台类型不支持', ErrorCode::WX_PARAM_ERROR);
+        }
+    }
+
+    /**
+     * @param bool $needJs
+     */
+    public function setNeedJs(bool $needJs)
+    {
+        $this->needJs = $needJs;
+    }
+
     public function getDetail() : array
     {
-        return [];
+        if ($this->platType == WxUtilBase::PLAT_TYPE_SHOP) { //公众号获取card_ticket
+            $ticket = WxUtilShop::getCardTicket($this->appid);
+        } else { //第三方平台获取card_ticket
+            $ticket = WxUtilOpenBase::getAuthorizerCardTicket($this->appid);
+        }
+        $signData = [
+            'api_ticket' => $ticket,
+            'appid' => $this->appid,
+            'location_id' => $this->reqData['shopId'],
+            'timestamp' => (string)$this->reqData['timestamp'],
+            'nonce_str' => $this->reqData['nonceStr'],
+            'card_id' => $this->reqData['card_id'],
+            'card_type' => $this->reqData['cardType'],
+        ];
+        sort($signData);
+        $this->reqData['cardSign'] = sha1(implode('', $signData));
+
+        if ($this->needJs) {
+            $jsConfig = new JsConfig($this->appid);
+            $jsConfig->setPlatType($this->platType);
+            $jsConfig->setTimestamp($this->reqData['timestamp']);
+            $jsConfig->setNonceStr($this->reqData['nonceStr']);
+            $this->reqData['jsConfig'] = $jsConfig->getDetail();
+        }
+
+        return $this->reqData;
     }
 }
