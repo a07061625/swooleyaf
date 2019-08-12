da2244a13eeff2dc2e91de9a81b090eadf20144f
jiangwei <jiangwei07061625@163.com>
2019-08-12 17:45:00 +0800

    test mqtt server

--- libs_frame/Tool/MqttTool.php
+++ libs_frame/Tool/MqttTool.php
@@ -12,4 +12,25 @@ use Traits\SimpleTrait;
 final class MqttTool
 {
     use SimpleTrait;
+
+    /**
+     * 获取消息长度
+     * @param string $data
+     * @return int
+     */
+    public static function getMsgLength(string $data)
+    {
+        $index = 1;
+        $multiplier = 1;
+        $length = 0;
+
+        do {
+            $digit = ord($data{$index});
+            $length += ($digit & 127) * $multiplier;
+            $multiplier *= 128;
+            $index++;
+        } while (($digit & 128) != 0);
+
+        return $length;
+    }
 }
