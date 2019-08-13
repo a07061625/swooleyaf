6d9e71f4cb43213a130f8ab18412202d35fb4027
jiangwei <jiangwei07061625@163.com>
2019-08-13 10:36:40 +0800

    test mqtt server

--- libs_frame/SyServer/MqttServer.php
+++ libs_frame/SyServer/MqttServer.php
@@ -57,57 +57,40 @@ class MqttServer
 
     public function onReceive(\swoole_server $server, int $fd, int $reactor_id, string $data)
     {
+        $byte = ord($data[0]);
+        $_POST = [];
         $_SERVER = [
-            'SY_MSG_TYPE' => (ord($data[0]) & 0xF0) >> 4,
+            'SY_MSG_TYPE' => ($byte & 0xF0) >> 4,
             'SY_MSG_LENGTH' => MqttTool::getMsgLength($data),
         ];
 
         switch ($_SERVER['SY_MSG_TYPE']) {
             case 1: //CONNECT
-                $resp = chr(32) . chr(2) . chr(0) . chr(0);//转换为二进制返回应该使用chr
-                $client_info = $this->get_connect_info(substr($data, 2));
-                $client_id = $client_info['clientId'];
-                $serv->send($fd, $resp);
-                $this->debug("Send CONNACK");
+                $rspContent = chr(32) . chr(2) . chr(0) . chr(0);
+                $server->send($fd, $rspContent);
                 break;
             case 3: //PUBLISH
-                $fix_header['dup'] = ($byte & 0x08) >> 3;
-                $fix_header['qos'] = ($byte & 0x06) >> 1;
-                $fix_header['retain'] = $byte & 0x01;
+                $_SERVER['SY_DUP'] = ($byte & 0x08) >> 3;
+                $_SERVER['SY_QOS'] = ($byte & 0x06) >> 1;
+                $_SERVER['SY_RETAIN'] = $byte & 0x01;
                 $offset = 2;
-                $topic = $this->decodeString(substr($data, $offset));
-                $offset += strlen($topic) + 2;
-                $msg = substr($data, $offset);
-                echo "client msg: $topic\n---------------------------------\n$msg\n---------------------------------\n";
-                $client_id = $this->redis_get("client_" . $fd);
+                $_POST['msg_topic'] = MqttTool::decodeStr(substr($data, $offset));
+                $offset += strlen($_POST['msg_topic']) + 2;
+                $_POST['msg_content'] = substr($data, $offset);
                 break;
             case 8: //SUBSCRIBE
-                //id有可能是两个字节的,这个需要更多的测试
-                //                $msg_id = ord($data[2]);
-                $msg_id = ord($data[3]);
-                $fix_header['sign'] = ($byte & 0x02) >> 1;
-                $qos = ord($data[$fix_header['data_len'] + 1]);
-                if ($fix_header['sign'] == 1) {
-                    echo "this is subscribe message!!!!\n";
-                    $this->debug($msg_id, "msg id");
-                    $this->debug($qos, "QOS");
-                    //这里没有从协议中读取topic的长度,按照固定的写做6
-                    $topic = substr($data, 6, $fix_header['data_len'] - 1);
-                    $this->debug($topic, "topic");
-                }
+                $_SERVER['SY_SIGN'] = ($byte & 0x02) >> 1;
+                $_POST['msg_id'] = ord($data[3]);
+                $_POST['msg_topic'] = $_SERVER['SY_SIGN'] == 1 ? substr($data, 6, ($_SERVER['SY_MSG_LENGTH'] - 1)) : '';
                 //订阅后返回
-                $resp = chr(0x90) . chr(3) . chr(0) . chr($msg_id) . chr(0);
-                $this->printstr($resp);
-                $serv->send($fd, $resp);
-                $this->debug("send SUBACK");
+                $rspContent = chr(0x90) . chr(3) . chr(0) . chr($_POST['msg_id']) . chr(0);
+                $server->send($fd, $rspContent);
                 break;
             case 10: //UNSUBSCRIBE
                 break;
             case 12: //PINGREQ
-                $resp = chr(208) . chr(0);//转换为二进制返回应该使用chr
-                //保存最后ping的时间
-                $serv->send($fd, $resp);
-                $this->debug("Send PINGRESP");
+                $rspContent = chr(208) . chr(0);
+                $server->send($fd, $rspContent);
                 break;
             case 14: //DISCONNECT
                 break;
--- libs_frame/Tool/MqttTool.php
+++ libs_frame/Tool/MqttTool.php
@@ -33,4 +33,71 @@ final class MqttTool
 
         return $length;
     }
+
+    /**
+     * 解码字符串长度
+     * @param string $data
+     * @return int
+     */
+    public static function decodeStrLength(string $data)
+    {
+        return 256 * ord($data[0]) + ord($data[1]);
+    }
+
+    /**
+     * 解码字符串
+     * @param string $data
+     * @return string
+     */
+    public static function decodeStr(string $data)
+    {
+        $length = self::decodeStrLength($data);
+        return substr($data, 2, $length);
+    }
+
+    /**
+     * 获取连接信息
+     * @param string $data
+     * @return array
+     */
+    public static function getConnectInfo(string $data)
+    {
+        $connectInfo = [
+            'protocolName' => self::decodeStr($data),
+        ];
+        $offset = strlen($connectInfo['protocolName']) + 2;
+        $connectInfo['version'] = ord(substr($data, $offset, 1));
+        $offset += 1;
+        $byte = ord($data[$offset]);
+        $connectInfo['willRetain'] = ($byte & 0x20 == 0x20);
+        $connectInfo['willQos'] = ($byte & 0x18 >> 3);
+        $connectInfo['willFlag'] = ($byte & 0x04 == 0x04);
+        $connectInfo['cleanStart'] = ($byte & 0x02 == 0x02);
+        $offset += 1;
+        $connectInfo['keepAlive'] = self::decodeStrLength(substr($data, $offset, 2));
+        $offset += 2;
+        $connectInfo['clientId'] = self::decodeStr(substr($data, $offset));
+        return $connectInfo;
+    }
+
+    /**
+     * 获取推送数据
+     * @param array $data
+     * @return string
+     */
+    public static function getPublishStr(array $data)
+    {
+        $needStr = chr(0x30);
+        $length = strlen($data['topic']) + strlen($data['content']) + 2;
+        do {
+            $digit = $length % 128;
+            $length = $length >> 7;
+            if ($length > 0) {
+                $digit = ($digit | 0x80);
+            }
+            $needStr .= chr($digit);
+        } while ($length > 0);
+        $needStr .= chr(0) . chr(0x06) . $data['topic'] . $data['content'];
+        return $needStr;
+    }
 }
