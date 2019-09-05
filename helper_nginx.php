5f150d51667ea703d38739b00dba9efbfc41ad6b
jiangwei <jiangwei07061625@163.com>
2019-09-05 16:46:17 +0800

    rename helper_projects.php to config_projects.php

--- helper_nginx.php
+++ helper_nginx.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/helper_load.php';
 $type = \Tool\Tool::getClientOption('-ct', false, '');
 switch ($type) {
     case 'streams':
-        $projects = include __DIR__ . '/helper_projects.php';
+        $projects = include __DIR__ . '/config_projects.php';
         \Helper\NginxTool::createStreamFile($projects);
         break;
     default:
