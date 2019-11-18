b73448247f9a6056a1d418218b79cec82cfa0cdf
jiangwei <jiangwei07061625@163.com>
2019-11-18 18:04:59 +0800

    log support handle by project

---
+++ libs_project/SyTrait/LogTrait.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 姜伟
+ * Date: 2019/11/18 0018
+ * Time: 17:52
+ */
+namespace SyTrait;
+
+trait LogTrait
+{
+    /**
+     * 处理日志
+     * @param string $logContent 日志内容
+     * @param string $logLevel 日志级别
+     */
+    public static function handleLog(string $logContent, string $logLevel)
+    {
+    }
+}
