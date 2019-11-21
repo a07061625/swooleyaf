bf69315a38cde2716051709bdb581cefa36d1055
jiangwei <jiangwei07061625@163.com>
2019-11-21 23:25:03 +0800

    qiniu add kodo

--- libs_frame/QiNiu/QiNiuUtilKodo.php
+++ libs_frame/QiNiu/QiNiuUtilKodo.php
@@ -7,9 +7,42 @@
  */
 namespace QiNiu;
 
+use SyConstant\ErrorCode;
 use SyTrait\SimpleTrait;
+use Tool\Tool;
 
 final class QiNiuUtilKodo extends QiNiuUtilBase
 {
     use SimpleTrait;
+
+    /**
+     * 发送服务请求
+     * @param \QiNiu\QiNiuBaseKodo $baseService
+     * @return array
+     */
+    public static function sendServiceRequest(QiNiuBaseKodo $baseService)
+    {
+        $resArr = [
+            'code' => 0,
+        ];
+
+        $sendRes = self::sendCurl($baseService->getDetail());
+        if ($sendRes === false) {
+            $resArr['code'] = ErrorCode::QINIU_KODO_PARAM_ERROR;
+            $resArr['message'] = '发送请求出错';
+        }
+
+        $sendData = Tool::jsonDecode($sendRes);
+        if (isset($sendData['code']) && ($sendData['code'] > 200)) {
+            $resArr['code'] = ErrorCode::QINIU_KODO_PARAM_ERROR;
+            $resArr['message'] = $sendData['error'];
+        } elseif (is_array($sendData)) {
+            $resArr['data'] = $sendData;
+        } else {
+            $resArr['code'] = ErrorCode::QINIU_KODO_PARAM_ERROR;
+            $resArr['message'] = '解析响应数据出错';
+        }
+
+        return $resArr;
+    }
 }
