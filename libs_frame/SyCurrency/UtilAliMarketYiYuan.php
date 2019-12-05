1c205a02d4b4eb1b57cf3bf6c0128967c923349a
jiangwei <jiangwei07061625@163.com>
2019-12-05 10:55:56 +0800

    sycurrency add alimarket yiyuan

---
+++ libs_frame/SyCurrency/UtilAliMarketYiYuan.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 姜伟
+ * Date: 2019/6/18 0018
+ * Time: 16:36
+ */
+namespace SyCurrency;
+
+use SyConstant\ErrorCode;
+use Tool\Tool;
+use SyTrait\SimpleTrait;
+
+abstract class UtilAliMarketYiYuan extends UtilCommon
+{
+    use SimpleTrait;
+
+    /**
+     * 发送服务请求
+     * @param \SyCurrency\BaseAliMarketYiYuan $aliMarketYiYuan
+     * @return array
+     */
+    public static function sendServiceRequest(BaseAliMarketYiYuan $aliMarketYiYuan)
+    {
+        $resArr = [
+            'code' => 0
+        ];
+        
+        $curlConfigs = $aliMarketYiYuan->getDetail();
+        $sendRes = Tool::sendCurlReq($curlConfigs);
+        if ($sendRes['res_no'] > 0) {
+            $resArr['code'] = ErrorCode::CURRENCY_REQ_ALIMARKET_YIYUAN_ERROR;
+            $resArr['msg'] = $sendRes['res_msg'];
+            return $resArr;
+        }
+        $rspData = Tool::jsonDecode($sendRes['res_content']);
+        if (isset($rspData['showapi_res_code']) && ($rspData['showapi_res_code'] == 0)) {
+            if (isset($rspData['showapi_res_body']['ret_code']) && ($rspData['showapi_res_body']['ret_code'] > 0)) {
+                $resArr['code'] = ErrorCode::CURRENCY_REQ_ALIMARKET_YIYUAN_ERROR;
+                $resArr['msg'] = $rspData['showapi_res_body']['msg'];
+            } else {
+                $resArr['data'] = $rspData['showapi_res_body'];
+            }
+        } elseif (isset($rspData['showapi_res_error'])) {
+            $resArr['code'] = ErrorCode::CURRENCY_REQ_ALIMARKET_YIYUAN_ERROR;
+            $resArr['msg'] = $rspData['showapi_res_error'];
+        } else {
+            $resArr['code'] = ErrorCode::CURRENCY_REQ_ALIMARKET_YIYUAN_ERROR;
+            $resArr['msg'] = '解析响应数据出错';
+        }
+        return $resArr;
+    }
+}
