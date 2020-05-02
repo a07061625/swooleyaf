fd3ee7a3492fc1fb35d9cd698952c8772e949df7
jiangwei <jiangwei07061625@163.com>
2020-05-02 16:07:29 +0800

    namespace Wx\Shop rename to Wx\Account

--- libs_frame/Wx/Account/Media/UploadNews.php
+++ libs_frame/Wx/Account/Media/UploadNews.php
@@ -5,16 +5,16 @@
  * Date: 2018/12/13 0013
  * Time: 9:37
  */
-namespace Wx\Shop\Media;
+namespace Wx\Account\Media;
 
 use SyConstant\ErrorCode;
 use SyException\Wx\WxException;
 use SyTool\Tool;
-use Wx\WxBaseShop;
+use Wx\WxBaseAccount;
+use Wx\WxUtilAccount;
 use Wx\WxUtilBase;
-use Wx\WxUtilShop;
 
-class UploadNews extends WxBaseShop
+class UploadNews extends WxBaseAccount
 {
     /**
      * 公众号ID
@@ -88,7 +88,7 @@ class UploadNews extends WxBaseShop
             'code' => 0,
         ];
 
-        $this->curlConfigs[CURLOPT_URL] = $this->serviceUrl . WxUtilShop::getAccessToken($this->appid);
+        $this->curlConfigs[CURLOPT_URL] = $this->serviceUrl . WxUtilAccount::getAccessToken($this->appid);
         $this->curlConfigs[CURLOPT_POSTFIELDS] = Tool::jsonEncode($this->reqData, JSON_UNESCAPED_UNICODE);
         $sendRes = WxUtilBase::sendPostReq($this->curlConfigs);
         $sendData = Tool::jsonDecode($sendRes);
