20c483ff0b5dd6bf0b0eacb1d5cdf6603fb5445c
jiangwei <jiangwei07061625@163.com>
2020-05-03 23:57:01 +0800

    namespace Wx\Shop rename to Wx\Account

--- libs_frame/Wx/WxBaseAccount.php
+++ libs_frame/Wx/WxBaseAccount.php
@@ -7,10 +7,6 @@
  */
 namespace Wx;
 
-use SyConstant\ErrorCode;
-use DesignPatterns\Singletons\WxConfigSingleton;
-use SyException\Wx\WxException;
-
 abstract class WxBaseAccount extends WxBase
 {
     const MATERIAL_TYPE_IMAGE = 'image';
@@ -24,8 +20,6 @@ abstract class WxBaseAccount extends WxBase
     const MESSAGE_TYPE_IMAGE = 'image';
     const MESSAGE_TYPE_VIDEO = 'video';
     const MESSAGE_TYPE_WXCARD = 'wxcard';
-    const MERCHANT_TYPE_SELF = 'self'; //商户类型-自身
-    const MERCHANT_TYPE_SUB = 'sub'; //商户类型-子商户,属于服务商下
 
     protected static $totalMaterialType = [
         self::MATERIAL_TYPE_IMAGE => '图片',
@@ -42,42 +36,9 @@ abstract class WxBaseAccount extends WxBase
         self::MESSAGE_TYPE_VIDEO => '视频',
         self::MESSAGE_TYPE_WXCARD => '卡券',
     ];
-    protected static $totalMerchantType = [
-        self::MERCHANT_TYPE_SELF => '自身',
-        self::MERCHANT_TYPE_SUB => '子商户',
-    ];
-
-    /**
-     * 商户类型
-     * @var string
-     */
-    protected $merchantType = '';
 
     public function __construct()
     {
         parent::__construct();
-        $this->merchantType = self::MERCHANT_TYPE_SELF;
-    }
-
-    /**
-     * @param \Wx\WxConfigAccount $configAccount
-     * @throws \SyException\Wx\WxException
-     */
-    protected function setAppIdAndMchId(WxConfigAccount $configAccount)
-    {
-        if ($this->merchantType == self::MERCHANT_TYPE_SELF) {
-            $this->reqData['appid'] = $configAccount->getAppId();
-            $this->reqData['mch_id'] = $configAccount->getPayMchId();
-        } else {
-            $merchantAppId = $configAccount->getMerchantAppId();
-            if (strlen($merchantAppId) == 0) {
-                throw new WxException('服务商微信号不能为空', ErrorCode::WX_PARAM_ERROR);
-            }
-            $merchantConfig = WxConfigSingleton::getInstance()->getShopConfig($merchantAppId);
-            $this->reqData['appid'] = $merchantConfig->getAppId();
-            $this->reqData['mch_id'] = $merchantConfig->getPayMchId();
-            $this->reqData['sub_appid'] = $configAccount->getAppId();
-            $this->reqData['sub_mch_id'] = $configAccount->getPayMchId();
-        }
     }
 }
--- libs_frame/Wx/WxBasePayment.php
+++ libs_frame/Wx/WxBasePayment.php
@@ -13,35 +13,9 @@ use SyException\Wx\WxException;
 
 abstract class WxBasePayment extends WxBase
 {
-    const MATERIAL_TYPE_IMAGE = 'image';
-    const MATERIAL_TYPE_VOICE = 'voice';
-    const MATERIAL_TYPE_VIDEO = 'video';
-    const MATERIAL_TYPE_THUMB = 'thumb';
-    const MESSAGE_TYPE_MPNEWS = 'mpnews';
-    const MESSAGE_TYPE_TEXT = 'text';
-    const MESSAGE_TYPE_VOICE = 'voice';
-    const MESSAGE_TYPE_MUSIC = 'music';
-    const MESSAGE_TYPE_IMAGE = 'image';
-    const MESSAGE_TYPE_VIDEO = 'video';
-    const MESSAGE_TYPE_WXCARD = 'wxcard';
     const MERCHANT_TYPE_SELF = 'self'; //商户类型-自身
     const MERCHANT_TYPE_SUB = 'sub'; //商户类型-子商户,属于服务商下
 
-    protected static $totalMaterialType = [
-        self::MATERIAL_TYPE_IMAGE => '图片',
-        self::MATERIAL_TYPE_VOICE => '语音',
-        self::MATERIAL_TYPE_VIDEO => '视频',
-        self::MATERIAL_TYPE_THUMB => '缩略图',
-    ];
-    protected static $totalMessageType = [
-        self::MESSAGE_TYPE_MPNEWS => '图文',
-        self::MESSAGE_TYPE_TEXT => '文本',
-        self::MESSAGE_TYPE_VOICE => '语音',
-        self::MESSAGE_TYPE_MUSIC => '音乐',
-        self::MESSAGE_TYPE_IMAGE => '图片',
-        self::MESSAGE_TYPE_VIDEO => '视频',
-        self::MESSAGE_TYPE_WXCARD => '卡券',
-    ];
     protected static $totalMerchantType = [
         self::MERCHANT_TYPE_SELF => '自身',
         self::MERCHANT_TYPE_SUB => '子商户',
