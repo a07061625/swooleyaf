5fe1dfbbacbb7a80c717220cfcc5dd5c96107c3e
jiangwei <jiangwei07061625@163.com>
2020-05-04 09:58:20 +0800

    fixed error for phpexcel of phpoffice

--- libs_frame/PhpOffice/PHPExcel.php
+++ libs_frame/PhpOffice/PHPExcel.php
@@ -1,10 +1,4 @@
 <?php
-/** PHPExcel root directory */
-if (!defined('PHPEXCEL_ROOT')) {
-    define('PHPEXCEL_ROOT', dirname(__FILE__) . '/');
-    require PHPEXCEL_ROOT . 'PHPExcel/Autoloader.php';
-}
-
 /**
  * PHPExcel
  *
