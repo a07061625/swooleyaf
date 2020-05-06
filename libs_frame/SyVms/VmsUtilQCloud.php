d917472b561b372ff33cc354d0a98259ec7b8c8a
jiangwei <jiangwei07061625@163.com>
2020-05-06 22:50:20 +0800

    syvms add qcloud

--- libs_frame/SyVms/VmsUtilQCloud.php
+++ libs_frame/SyVms/VmsUtilQCloud.php
@@ -8,6 +8,7 @@
 namespace SyVms;
 
 use DesignPatterns\Singletons\VmsConfigSingleton;
+use SyConstant\ErrorCode;
 use SyTool\Tool;
 use SyTrait\SimpleTrait;
 
@@ -37,5 +38,37 @@ abstract class VmsUtilQCloud extends VmsUtilBase
             'app_id' => $config->getAppId(),
             'sign' => hash('sha256', $signStr),
         ];
+    }
+
+    /**
+     * 发送服务请求
+     * @param \SyVms\VmsBaseQCloud $service
+     * @return array
+     */
+    public static function sendServiceRequest(VmsBaseQCloud $service) : array
+    {
+        $resArr = [
+            'code' => 0
+        ];
+
+        $curlConfigs = $service->getDetail();
+        $sendRes = Tool::sendCurlReq($curlConfigs);
+        if ($sendRes['res_no'] > 0) {
+            $resArr['code'] = ErrorCode::VMS_REQ_QCLOUD_ERROR;
+            $resArr['msg'] = $sendRes['res_msg'];
+            return $resArr;
+        }
+        $rspData = Tool::jsonDecode($sendRes['res_content']);
+        if (isset($rspData['result']) && ($rspData['result'] == 0)) {
+            $resArr['data'] = $rspData;
+        } else if (isset($rspData['result'])) {
+            $resArr['code'] = ErrorCode::VMS_REQ_QCLOUD_ERROR;
+            $resArr['msg'] = $rspData['errmsg'];
+        } else {
+            $resArr['code'] = ErrorCode::VMS_REQ_QCLOUD_ERROR;
+            $resArr['msg'] = '解析响应数据出错';
+        }
+
+        return $resArr;
     }
 }
