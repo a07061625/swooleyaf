e53a6a37198d41c7f9b2024d0e2f2877e766feaf
jiangwei <jiangwei07061625@163.com>
2020-07-23 13:47:31 +0800

    add helper_load_example.php

--- helper_service_manager.php
+++ helper_service_manager.php
@@ -1,14 +1,15 @@
 <?php
 require_once __DIR__ . '/helper_load.php';
 
-$type = SyTool\Tool::getClientOption('-st', false, 'server');
+$phpInfo = include __DIR__ . '/helper_php.php';
+$type = \SyTool\Tool::getClientOption('-st', false, 'server');
 if ($type == 'server') {
     $projects = include __DIR__ . '/config_projects.php';
-    $commandPrefix = 'sudo /usr/local/php7/bin/php ' . __DIR__ . '/helper_service.php';
+    $commandPrefix = $phpInfo['dir_bin'] . ' ' . __DIR__ . '/helper_service.php';
     \Helper\ServiceManager::handleAllService($commandPrefix, $projects);
 } elseif ($type == 'processpool') {
     $pools = include __DIR__ . '/config_pools.php';
-    $commandPrefix = 'sudo /usr/local/php7/bin/php ' . __DIR__ . '/helper_service.php';
+    $commandPrefix = $phpInfo['dir_bin'] . ' ' . __DIR__ . '/helper_service.php';
     \Helper\ServiceManager::handleAllProcessPool($commandPrefix, $pools);
 } else {
     exit('服务类型不支持' . PHP_EOL);
