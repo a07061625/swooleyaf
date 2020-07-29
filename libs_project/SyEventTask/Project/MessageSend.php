e5b73bbc60e92238b07eabc195125199470b1e49
jiangwei <jiangwei07061625@163.com>
2020-07-29 08:29:24 +0800

    event task add demo for message send

---
+++ libs_project/SyEventTask/Project/MessageSend.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 姜伟
+ * Date: 2020/7/28 0028
+ * Time: 21:52
+ */
+namespace SyEventTask\Project;
+
+use DesignPatterns\Singletons\MessageHandlerSingleton;
+use SyConstant\Project;
+use SyEventTask\TaskBase;
+use SyLog\Log;
+
+/**
+ * Class MessageSend
+ * @package SyEventTask\Project
+ */
+class MessageSend extends TaskBase
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->intervalTime = 3000;
+    }
+
+    public function __clone()
+    {
+    }
+
+    public function getData() : array
+    {
+        return [];
+    }
+
+    public function handle(array $data)
+    {
+        $msgNum = 1;
+        while ($msgNum <= 200) {
+            $msgNum++;
+            try {
+                $msgData = MessageHandlerSingleton::getInstance()->getMsgData(Project::MESSAGE_QUEUE_TYPE_RABBIT);
+                if (empty($msgData)) {
+                    break;
+                }
+
+                $res = MessageHandlerSingleton::getInstance()->invokeMsg($msgData);
+                //保存处理结果到数据库等
+            } catch (\Exception $e) {
+                Log::error($e->getMessage(), $e->getCode(), $e->getTraceAsString());
+            }
+        }
+    }
+}
