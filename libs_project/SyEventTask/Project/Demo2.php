1debabd37569198dc21aebe042438182e9f1e6ec
jiangwei <jiangwei07061625@163.com>
2020-07-29 09:10:27 +0800

    event task add demo for message send

---
+++ libs_project/SyEventTask/Project/Demo2.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 姜伟
+ * Date: 2020/7/29 0029
+ * Time: 9:10
+ */
+namespace SyEventTask\Project;
+
+/**
+ * Class Demo2
+ * @package SyEventTask\Project
+ */
+class Demo2
+{
+}
