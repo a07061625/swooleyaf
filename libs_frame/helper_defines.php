e3416d914c009c2a4bc8b821f89dc593c63e7b80
jiangwei <jiangwei07061625@163.com>
2021-01-04 23:22:51 +0800

    a000-feat(config): add config file prefix

--- libs_frame/helper_defines.php
+++ libs_frame/helper_defines.php
@@ -89,3 +89,8 @@ if (!defined('SY_HTTP_RSP_CODE_ERROR')) {
 if (!defined('SY_PAY_PAYPAL_ENV')) {
     define('SY_PAY_PAYPAL_ENV', 'product');
 }
+
+//配置文件前缀
+if (!defined('SY_CONFIG_PREFIX')) {
+    define('SY_CONFIG_PREFIX', '');
+}
