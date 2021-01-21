d0934e5e5a1e187ca2a9271593b455b71de2936d
jiangwei <jiangwei07061625@163.com>
2021-01-21 11:08:53 +0800

    a000-feat(tool): add bloom filter

---
+++ libs_project/SyTrait/BloomTrait.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 姜伟
+ * Date: 2021/1/21 0021
+ * Time: 10:16
+ */
+
+namespace SyTrait;
+
+use SyTool\BloomFilter;
+
+/**
+ * Trait BloomTrait
+ *
+ * @package SyTrait
+ */
+trait BloomTrait
+{
+    /**
+     * 初始化过滤器
+     *
+     * @throws \SyException\Common\ErrorException
+     */
+    private function initFilters()
+    {
+        $this->filters['a01'] = new BloomFilter(100000);
+    }
+}
