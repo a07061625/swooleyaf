958ff2e87f0112ba07778aace760c6b93879d3b4
jiangwei <jiangwei07061625@163.com>
2021-01-21 20:01:39 +0800

    a000-fix(constant): update doc for constant

--- libs_frame/SyConstant/SyInner.php
+++ libs_frame/SyConstant/SyInner.php
@@ -13,88 +13,260 @@ final class SyInner
 {
     use SimpleTrait;
 
-    //服务常量
-    const SERVER_TYPE_API_GATE = 'api'; //服务端类型-api入口
-    const SERVER_TYPE_API_MODULE = 'rpc'; //服务端类型-api模块
-    const SERVER_TYPE_FRONT_GATE = 'frontgate'; //服务端类型-前端入口
-    const SERVER_HTTP_TAG_REQUEST_HEADER = 'swoole-yaf'; //服务端http标识-请求头名称
-    const SERVER_DATA_KEY_TASK = '_sytask'; //服务端内部数据键名-task
-    const SERVER_DATA_KEY_TIMESTAMP = 'SYREQ_TIME'; //服务端内部数据键名-请求时间戳
-    const SERVER_DATA_KEY_HTTP_RSP_CODE_ERROR = 'Syresp-Status'; //服务端内部数据键名-http响应错误状态码
+    /**
+     * 服务-类型-api入口
+     */
+    const SERVER_TYPE_API_GATE = 'api';
+    /**
+     * 服务-类型-api模块
+     */
+    const SERVER_TYPE_API_MODULE = 'rpc';
+    /**
+     * 服务-类型-前端入口
+     */
+    const SERVER_TYPE_FRONT_GATE = 'frontgate';
+    /**
+     * 服务-http标识-请求头名称
+     */
+    const SERVER_HTTP_TAG_REQUEST_HEADER = 'swoole-yaf';
+    /**
+     * 服务-内部数据键名-task
+     */
+    const SERVER_DATA_KEY_TASK = '_sytask';
+    /**
+     * 服务-内部数据键名-请求时间戳
+     */
+    const SERVER_DATA_KEY_TIMESTAMP = 'SYREQ_TIME';
+    /**
+     * 服务-内部数据键名-http响应错误状态码
+     */
+    const SERVER_DATA_KEY_HTTP_RSP_CODE_ERROR = 'Syresp-Status';
 
-    //服务注册常量
-    const SERVER_REGISTER_TYPE_NGINX = 'nginx'; //类型-nginx
+    /**
+     * 服务注册-类型-nginx
+     */
+    const SERVER_REGISTER_TYPE_NGINX = 'nginx';
 
-    //进程常量
-    const PROCESS_TYPE_TASK = 'Task'; //类型-task
-    const PROCESS_TYPE_WORKER = 'Worker'; //类型-worker
-    const PROCESS_TYPE_MANAGER = 'Manager'; //类型-manager
-    const PROCESS_TYPE_MAIN = 'Main'; //类型-main
+    /**
+     * 进程-类型-task
+     */
+    const PROCESS_TYPE_TASK = 'Task';
+    /**
+     * 进程-类型-worker
+     */
+    const PROCESS_TYPE_WORKER = 'Worker';
+    /**
+     * 进程-类型-manager
+     */
+    const PROCESS_TYPE_MANAGER = 'Manager';
+    /**
+     * 进程-类型-main
+     */
+    const PROCESS_TYPE_MAIN = 'Main';
 
-    //环境常量
-    const ENV_PROJECT_DEV = 'dev'; //项目环境-测试
-    const ENV_PROJECT_PRODUCT = 'product'; //项目环境-生产
-    const ENV_SYSTEM_BSD = 'FreeBSD'; //系统环境-bsd
-    const ENV_SYSTEM_MAC = 'Darwin'; //系统环境-mac
-    const ENV_SYSTEM_LINUX = 'Linux'; //系统环境-linux
-    const ENV_PORT_MIN = 1024; //端口-最小
-    const ENV_PORT_MAX = 65535; //端口-最大
-    const SYSTEM_USER_ROOT = 'root'; //系统用户-root
+    /**
+     * 环境-项目-测试
+     */
+    const ENV_PROJECT_DEV = 'dev';
+    /**
+     * 环境-项目-生产
+     */
+    const ENV_PROJECT_PRODUCT = 'product';
+    /**
+     * 环境-系统-bsd
+     */
+    const ENV_SYSTEM_BSD = 'FreeBSD';
+    /**
+     * 环境-系统-mac
+     */
+    const ENV_SYSTEM_MAC = 'Darwin';
+    /**
+     * 环境-系统-linux
+     */
+    const ENV_SYSTEM_LINUX = 'Linux';
+    /**
+     * 环境-端口-最小
+     */
+    const ENV_PORT_MIN = 1024;
+    /**
+     * 环境-端口-最大
+     */
+    const ENV_PORT_MAX = 65535;
+    /**
+     * 环境-系统用户-root
+     */
+    const SYSTEM_USER_ROOT = 'root';
 
-    //版本常量
-    const VERSION_MIN_PHP = '7.2.0'; //最低版本-php
-    const VERSION_MIN_SWOOLE = '4.5.2'; //最低版本-swoole,测试发现4.5.3-4.5.5在启动服务时候会发生诡异的问题,没有日志信息,但是只能启动api模块
-    const VERSION_MIN_SEASLOG = '2.1.0'; //最低版本-seaslog
-    const VERSION_MIN_YAC = '2.2.0'; //最低版本-yac
-    const VERSION_MIN_YAF = '3.2.1'; //最低版本-yaf
-    const VERSION_MIN_RUNKIT = '3.0.0'; //最低版本-runkit
-    const VERSION_MIN_REDIS = '5.0.0'; //最低版本-redis
+    /**
+     * 版本-最低-php
+     */
+    const VERSION_MIN_PHP = '7.2.0';
+    /**
+     * 版本-最低-swoole,测试发现4.5.3-4.5.5在启动服务时候会发生诡异的问题,没有日志信息,但是只能启动api模块
+     */
+    const VERSION_MIN_SWOOLE = '4.5.2';
+    /**
+     * 版本-最低-seaslog
+     */
+    const VERSION_MIN_SEASLOG = '2.1.0';
+    /**
+     * 版本-最低-yac
+     */
+    const VERSION_MIN_YAC = '2.2.0';
+    /**
+     * 版本-最低-yaf
+     */
+    const VERSION_MIN_YAF = '3.2.1';
+    /**
+     * 版本-最低-runkit
+     */
+    const VERSION_MIN_RUNKIT = '3.0.0';
+    /**
+     * 版本-最低-redis
+     */
+    const VERSION_MIN_REDIS = '5.0.0';
 
-    //熔断器常量
-    const FUSE_STATE_OPEN = 'open'; //状态-开启
-    const FUSE_STATE_CLOSED = 'closed'; //状态-关闭
-    const FUSE_STATE_HALF_OPEN = 'half_open'; //状态-半开
-    const FUSE_TIME_ERROR_STAT = 15; //错误统计间隔时间,单位为秒
-    const FUSE_TIME_OPEN_KEEP = 10; //开启状态保持时间,单位为秒
-    const FUSE_NUM_REQUEST_ERROR = 20; //请求出错次数
-    const FUSE_NUM_HALF_REQUEST_SUCCESS = 10; //半开状态请求成功次数
-    const FUSE_MSG_REQUEST_ERROR = '{"code":10001,"data":[],"msg":"服务繁忙,请稍后重试"}'; //请求出错提示消息
+    /**
+     * 熔断器-状态-开启
+     */
+    const FUSE_STATE_OPEN = 'open';
+    /**
+     * 熔断器-状态-关闭
+     */
+    const FUSE_STATE_CLOSED = 'closed';
+    /**
+     * 熔断器-状态-半开
+     */
+    const FUSE_STATE_HALF_OPEN = 'half_open';
+    /**
+     * 熔断器-错误统计间隔时间,单位为秒
+     */
+    const FUSE_TIME_ERROR_STAT = 15;
+    /**
+     * 熔断器-开启状态保持时间,单位为秒
+     */
+    const FUSE_TIME_OPEN_KEEP = 10;
+    /**
+     * 熔断器-请求出错次数
+     */
+    const FUSE_NUM_REQUEST_ERROR = 20;
+    /**
+     * 熔断器-半开状态请求成功次数
+     */
+    const FUSE_NUM_HALF_REQUEST_SUCCESS = 10;
+    /**
+     * 熔断器-请求出错提示消息
+     */
+    const FUSE_MSG_REQUEST_ERROR = '{"code":10001,"data":[],"msg":"服务繁忙,请稍后重试"}';
 
-    //路由常量
-    const ROUTE_TYPE_SIMPLE = 'simple'; //类型-简单路由
+    /**
+     * 路由-类型-简单路由
+     */
+    const ROUTE_TYPE_SIMPLE = 'simple';
 
-    //注册常量
-    const REGISTRY_NAME_SERVICE_ERROR = 'SERVICE_ERROR'; //名称-服务错误
-    const REGISTRY_NAME_REQUEST_HEADER = 'REQUEST_HEADER'; //名称-请求头
-    const REGISTRY_NAME_REQUEST_SERVER = 'REQUEST_SERVER'; //名称-服务器信息
-    const REGISTRY_NAME_RESPONSE_HEADER = 'RESPONSE_HEADER'; //名称-响应头
-    const REGISTRY_NAME_RESPONSE_COOKIE = 'RESPONSE_COOKIE'; //名称-响应cookie
-    const REGISTRY_NAME_RESPONSE_JWT_SESSION = 'RESPONSE_JWT_SESSION'; //名称-响应jwt会话
-    const REGISTRY_NAME_RESPONSE_JWT_DATA = 'RESPONSE_JWT_DATA'; //名称-响应jwt数据
-    const REGISTRY_NAME_PREFIX_VALIDATOR = 'VALIDATOR_'; //名称前缀-校验器
-    const REGISTRY_NAME_PREFIX_CONTROLLER = 'CONTROLLER_'; //名称前缀-控制器
-    const REGISTRY_NAME_PREFIX_ASPECT_BEFORE = 'ASPECTBEFORE_'; //名称前缀-前置切面
-    const REGISTRY_NAME_PREFIX_ASPECT_AFTER = 'ASPECTAFTER_'; //名称前缀-后置切面
+    /**
+     * 注册-名称-服务错误
+     */
+    const REGISTRY_NAME_SERVICE_ERROR = 'SERVICE_ERROR';
+    /**
+     * 注册-名称-请求头
+     */
+    const REGISTRY_NAME_REQUEST_HEADER = 'REQUEST_HEADER';
+    /**
+     * 注册-名称-服务器信息
+     */
+    const REGISTRY_NAME_REQUEST_SERVER = 'REQUEST_SERVER';
+    /**
+     * 注册-名称-响应头
+     */
+    const REGISTRY_NAME_RESPONSE_HEADER = 'RESPONSE_HEADER';
+    /**
+     * 注册-名称-响应cookie
+     */
+    const REGISTRY_NAME_RESPONSE_COOKIE = 'RESPONSE_COOKIE';
+    /**
+     * 注册-名称-响应jwt会话
+     */
+    const REGISTRY_NAME_RESPONSE_JWT_SESSION = 'RESPONSE_JWT_SESSION';
+    /**
+     * 注册-名称-响应jwt数据
+     */
+    const REGISTRY_NAME_RESPONSE_JWT_DATA = 'RESPONSE_JWT_DATA';
+    /**
+     * 注册-名称前缀-校验器
+     */
+    const REGISTRY_NAME_PREFIX_VALIDATOR = 'VALIDATOR_';
+    /**
+     * 注册-名称前缀-控制器
+     */
+    const REGISTRY_NAME_PREFIX_CONTROLLER = 'CONTROLLER_';
+    /**
+     * 注册-名称前缀-前置切面
+     */
+    const REGISTRY_NAME_PREFIX_ASPECT_BEFORE = 'ASPECTBEFORE_';
+    /**
+     * 注册-名称前缀-后置切面
+     */
+    const REGISTRY_NAME_PREFIX_ASPECT_AFTER = 'ASPECTAFTER_';
 
-    //图片常量
-    const IMAGE_MIME_TYPE_PNG = 'image/png'; //MIME类型-PNG
-    const IMAGE_MIME_TYPE_JPEG = 'image/jpeg'; //MIME类型-JPEG
-    const IMAGE_MIME_TYPE_GIF = 'image/gif'; //MIME类型-GIF
-    const IMAGE_FILTER_DITHER_DIFFUSION = 'diffusion'; //噪点滤镜类型-扩散
-    const IMAGE_FILTER_DITHER_ORDERED = 'ordered'; //噪点滤镜类型-规整
-    const IMAGE_DATA_TYPE_BINARY = 'binary'; //数据类型-binary
-    const IMAGE_DATA_TYPE_BASE64 = 'base64'; //数据类型-base64
+    /**
+     * 图片-MIME类型-PNG
+     */
+    const IMAGE_MIME_TYPE_PNG = 'image/png';
+    /**
+     * 图片-MIME类型-JPEG
+     */
+    const IMAGE_MIME_TYPE_JPEG = 'image/jpeg';
+    /**
+     * 图片-MIME类型-GIF
+     */
+    const IMAGE_MIME_TYPE_GIF = 'image/gif';
+    /**
+     * 图片-噪点滤镜类型-扩散
+     */
+    const IMAGE_FILTER_DITHER_DIFFUSION = 'diffusion';
+    /**
+     * 图片-噪点滤镜类型-规整
+     */
+    const IMAGE_FILTER_DITHER_ORDERED = 'ordered';
+    /**
+     * 图片-数据类型-binary
+     */
+    const IMAGE_DATA_TYPE_BINARY = 'binary';
+    /**
+     * 图片-数据类型-base64
+     */
+    const IMAGE_DATA_TYPE_BASE64 = 'base64';
 
-    //注解常量
-    const ANNOTATION_NAME_FILTER = 'SyFilter'; //名称-数据校验器
-    const ANNOTATION_NAME_ASPECT = 'SyAspect'; //名称-环绕切面
-    const ANNOTATION_NAME_ASPECT_BEFORE = 'SyAspectBefore'; //名称-前置切面
-    const ANNOTATION_NAME_ASPECT_AFTER = 'SyAspectAfter'; //名称-后置切面
-    const ANNOTATION_TAG_SY_MANAGER = '__symanager'; //标识-管理控制
+    /**
+     * 注解-名称-数据校验器
+     */
+    const ANNOTATION_NAME_FILTER = 'SyFilter';
+    /**
+     * 注解-名称-环绕切面
+     */
+    const ANNOTATION_NAME_ASPECT = 'SyAspect';
+    /**
+     * 注解-名称-前置切面
+     */
+    const ANNOTATION_NAME_ASPECT_BEFORE = 'SyAspectBefore';
+    /**
+     * 注解-名称-后置切面
+     */
+    const ANNOTATION_NAME_ASPECT_AFTER = 'SyAspectAfter';
+    /**
+     * 注解-标识-管理控制
+     */
+    const ANNOTATION_TAG_SY_MANAGER = '__symanager';
 
-    //支付常量
-    const PAY_PAYPAL_ENV_PRODUCT = 'product'; //贝宝支付环境-正式
-    const PAY_PAYPAL_ENV_SANDBOX = 'sandbox'; //贝宝支付环境-沙箱
+    /**
+     * 支付-贝宝支付环境-正式
+     */
+    const PAY_PAYPAL_ENV_PRODUCT = 'product';
+    /**
+     * 支付-贝宝支付环境-沙箱
+     */
+    const PAY_PAYPAL_ENV_SANDBOX = 'sandbox';
 
     public static $totalServerType = [
         self::SERVER_TYPE_API_GATE => 'api入口',
--- libs_project/SyConstant/Project.php
+++ libs_project/SyConstant/Project.php
@@ -18,28 +18,68 @@ final class Project extends ProjectBase
     const MODULE_NAME_API = SY_PROJECT . self::MODULE_BASE_API;
 
     //REDIS常量 后五位字母+数字的前缀为项目前缀
-    const REDIS_PREFIX_TIMER_QUEUE = 'sy' . SY_PROJECT . 'a0000_'; //前缀-定时器队列
-    const REDIS_PREFIX_TIMER_CONTENT = 'sy' . SY_PROJECT . 'a0001_'; //前缀-定时器内容
+    /**
+     * redis-前缀-定时器队列
+     */
+    const REDIS_PREFIX_TIMER_QUEUE = 'sy' . SY_PROJECT . 'a0000_';
+    /**
+     * redis-前缀-定时器内容
+     */
+    const REDIS_PREFIX_TIMER_CONTENT = 'sy' . SY_PROJECT . 'a0001_';
 
-    //任务常量
-    const TASK_STATUS_DELETE = -1; //任务状态-已删除
-    const TASK_STATUS_INVALID = 0; //任务状态-无效
-    const TASK_STATUS_VALID = 1; //任务状态-有效
-    const TASK_PERSIST_TYPE_SINGLE = 1; //持久化类型-单次
-    const TASK_PERSIST_TYPE_INTERVAL = 2; //持久化类型-间隔时间
-    const TASK_CACHE_EXPIRE_TIME = 300; //缓存过期时间-五分钟
+    /**
+     * 任务-状态-已删除
+     */
+    const TASK_STATUS_DELETE = -1;
+    /**
+     * 任务-状态-无效
+     */
+    const TASK_STATUS_INVALID = 0;
+    /**
+     * 任务-状态-有效
+     */
+    const TASK_STATUS_VALID = 1;
+    /**
+     * 任务-持久化类型-单次
+     */
+    const TASK_PERSIST_TYPE_SINGLE = 1;
+    /**
+     * 任务-持久化类型-间隔时间
+     */
+    const TASK_PERSIST_TYPE_INTERVAL = 2;
+    /**
+     * 任务-缓存过期时间-五分钟
+     */
+    const TASK_CACHE_EXPIRE_TIME = 300;
 
-    //消息队列常量
-    const MESSAGE_QUEUE_TOPIC_ADD_LOG = 'a000'; //主题-添加日志
-    const MESSAGE_QUEUE_TOPIC_REQ_HEALTH_CHECK = 'a001'; //主题-请求健康检查
-    const MESSAGE_QUEUE_TOPIC_TEST = SY_ENV . SY_PROJECT . '0000'; //主题-测试
+    /**
+     * 消息队列-主题-添加日志
+     */
+    const MESSAGE_QUEUE_TOPIC_ADD_LOG = 'a000';
+    /**
+     * 消息队列-主题-请求健康检查
+     */
+    const MESSAGE_QUEUE_TOPIC_REQ_HEALTH_CHECK = 'a001';
+    /**
+     * 消息队列-主题-测试
+     */
+    const MESSAGE_QUEUE_TOPIC_TEST = SY_ENV . SY_PROJECT . '0000';
 
     //服务预处理常量,标识长度为5位,第一位固定为/,后四位代表不同预处理操作,其中后四位全为数字的为框架内部预留标识
-    const PRE_PROCESS_TAG_HTTP_PROJECT_REFRESH_TOKEN_EXPIRE = '/a000'; //HTTP服务项目标识-更新令牌过期时间
-    const PRE_PROCESS_TAG_RPC_PROJECT_TEST = '/a000'; //RPC服务项目标识-测试
+    /**
+     * 服务预处理-HTTP服务项目标识-更新令牌过期时间
+     */
+    const PRE_PROCESS_TAG_HTTP_PROJECT_REFRESH_TOKEN_EXPIRE = '/a000';
+    /**
+     * 服务预处理-RPC服务项目标识-测试
+     */
+    const PRE_PROCESS_TAG_RPC_PROJECT_TEST = '/a000';
 
     //进程池服务标识常量,4位字符串,数字和字母组成,纯数字的为框架内部服务,其他为自定义服务
-    const POOL_PROCESS_SERVICE_TAG_TEST = 'a000'; //服务标识-测试
+    /**
+     * 进程池-服务标识-测试
+     */
+    const POOL_PROCESS_SERVICE_TAG_TEST = 'a000';
 
     public static $totalModuleName = [
         self::MODULE_NAME_API,
