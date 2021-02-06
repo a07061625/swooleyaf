7b9681155f04b885d4ae2826ad8c193dbdd673a3
jiangwei <jiangwei07061625@163.com>
2021-02-06 12:40:13 +0800

    a000-fix(defines): fixed bug for define constant

--- libs_frame/Helper/ServiceManager.php
+++ libs_frame/Helper/ServiceManager.php
@@ -5,6 +5,7 @@
  * Date: 2018/7/21 0021
  * Time: 9:27
  */
+
 namespace Helper;
 
 use SyTool\Tool;
@@ -14,13 +15,149 @@ class ServiceManager
 {
     use SimpleTrait;
 
-    private static function createCommandStr(string $commandPrefix, array $commandParams) : string
+    public static function handleAllService(string $commandPrefix, array $projects)
+    {
+        $commandList = [];
+        $action = Tool::getClientOption('-s', false, '');
+        switch ($action) {
+            case 'start-all':
+                foreach ($projects as $eProject) {
+                    foreach ($eProject['listens'] as $eListen) {
+                        $params1 = [
+                            '-s' => 'start',
+                        ];
+                        self::getServiceParamsCommon($eProject, $eListen, $params1);
+                        $commandList[] = self::createCommandStr($commandPrefix, $params1);
+
+                        $params2 = [
+                            '-s' => 'startstatus',
+                        ];
+                        self::getServiceParamsCommon($eProject, $eListen, $params2);
+                        self::getServiceParamsRegister($eListen, $params2);
+                        $commandList[] = self::createCommandStr($commandPrefix, $params2);
+                    }
+                }
+
+                break;
+            case 'stop-all':
+                foreach ($projects as $eProject) {
+                    foreach ($eProject['listens'] as $eListen) {
+                        $params = [
+                            '-s' => 'stop',
+                        ];
+                        self::getServiceParamsCommon($eProject, $eListen, $params);
+                        self::getServiceParamsRegister($eListen, $params);
+                        $commandList[] = self::createCommandStr($commandPrefix, $params);
+                    }
+                }
+
+                break;
+            case 'restart-all':
+                foreach ($projects as $eProject) {
+                    foreach ($eProject['listens'] as $eListen) {
+                        $params1 = [
+                            '-s' => 'restart',
+                        ];
+                        self::getServiceParamsCommon($eProject, $eListen, $params1);
+                        self::getServiceParamsRegister($eListen, $params1);
+                        $commandList[] = self::createCommandStr($commandPrefix, $params1);
+
+                        $params2 = [
+                            '-s' => 'startstatus',
+                        ];
+                        self::getServiceParamsCommon($eProject, $eListen, $params2);
+                        self::getServiceParamsRegister($eListen, $params2);
+                        $commandList[] = self::createCommandStr($commandPrefix, $params2);
+                    }
+                }
+
+                break;
+            case 'kz-all':
+                foreach ($projects as $eProject) {
+                    foreach ($eProject['listens'] as $eListen) {
+                        $params = [
+                            '-s' => 'kz',
+                        ];
+                        self::getServiceParamsCommon($eProject, $eListen, $params);
+                        $commandList[] = self::createCommandStr($commandPrefix, $params);
+                    }
+                }
+
+                break;
+            default:
+                $commandList[] = 'echo -e "\e[1;31m command not exist \e[0m"';
+        }
+        $totalCommand = implode(' && ', $commandList);
+        system($totalCommand);
+    }
+
+    public static function handleAllProcessPool(string $commandPrefix, array $pools)
+    {
+        $commandList = [];
+        $action = Tool::getClientOption('-s', false, '');
+        switch ($action) {
+            case 'start-all':
+                foreach ($pools as $ePool) {
+                    foreach ($ePool['listens'] as $eListen) {
+                        $params1 = [
+                            '-s' => 'start',
+                        ];
+                        self::getProcessPoolParamsCommon($ePool, $eListen, $params1);
+                        $commandList[] = self::createCommandStr($commandPrefix, $params1);
+
+                        $params2 = [
+                            '-s' => 'startstatus',
+                        ];
+                        self::getProcessPoolParamsCommon($ePool, $eListen, $params2);
+                        $commandList[] = self::createCommandStr($commandPrefix, $params2);
+                    }
+                }
+
+                break;
+            case 'stop-all':
+                foreach ($pools as $ePool) {
+                    foreach ($ePool['listens'] as $eListen) {
+                        $params = [
+                            '-s' => 'stop',
+                        ];
+                        self::getProcessPoolParamsCommon($ePool, $eListen, $params);
+                        $commandList[] = self::createCommandStr($commandPrefix, $params);
+                    }
+                }
+
+                break;
+            case 'restart-all':
+                foreach ($pools as $ePool) {
+                    foreach ($ePool['listens'] as $eListen) {
+                        $params1 = [
+                            '-s' => 'restart',
+                        ];
+                        self::getProcessPoolParamsCommon($ePool, $eListen, $params1);
+                        $commandList[] = self::createCommandStr($commandPrefix, $params1);
+
+                        $params2 = [
+                            '-s' => 'startstatus',
+                        ];
+                        self::getProcessPoolParamsCommon($ePool, $eListen, $params2);
+                        $commandList[] = self::createCommandStr($commandPrefix, $params2);
+                    }
+                }
+
+                break;
+            default:
+                $commandList[] = 'echo -e "\e[1;31m command not exist \e[0m"';
+        }
+        $totalCommand = implode(' && ', $commandList);
+        system($totalCommand);
+    }
+
+    private static function createCommandStr(string $commandPrefix, array $commandParams): string
     {
         $commandStr = $commandPrefix;
         foreach ($commandParams as $paramKey => $paramVal) {
             $commandStr .= ' ' . trim($paramKey);
             $trueVal = trim($paramVal);
-            if (strlen($trueVal) > 0) {
+            if (\strlen($trueVal) > 0) {
                 $commandStr .= ' ' . $trueVal;
             }
         }
@@ -45,135 +182,5 @@ class ServiceManager
         $commandParams['-st'] = 'processpool';
         $commandParams['-module'] = $poolInfo['module_name'];
         $commandParams['-port'] = $listenInfo['port'];
-    }
-
-    public static function handleAllService(string $commandPrefix, array $projects)
-    {
-        $action = Tool::getClientOption('-s', false, '');
-        switch ($action) {
-            case 'start-all':
-                foreach ($projects as $eProject) {
-                    foreach ($eProject['listens'] as $eListen) {
-                        $params1 = [
-                            '-s' => 'start',
-                        ];
-                        self::getServiceParamsCommon($eProject, $eListen, $params1);
-                        $command1 = self::createCommandStr($commandPrefix, $params1);
-
-                        $params2 = [
-                            '-s' => 'startstatus',
-                        ];
-                        self::getServiceParamsCommon($eProject, $eListen, $params2);
-                        self::getServiceParamsRegister($eListen, $params2);
-                        $command2 = self::createCommandStr($commandPrefix, $params2);
-                        system($command1 . ' && ' . $command2);
-                    }
-                }
-                break;
-            case 'stop-all':
-                foreach ($projects as $eProject) {
-                    foreach ($eProject['listens'] as $eListen) {
-                        $params = [
-                            '-s' => 'stop',
-                        ];
-                        self::getServiceParamsCommon($eProject, $eListen, $params);
-                        self::getServiceParamsRegister($eListen, $params);
-                        $command = self::createCommandStr($commandPrefix, $params);
-                        system($command);
-                    }
-                }
-                break;
-            case 'restart-all':
-                foreach ($projects as $eProject) {
-                    foreach ($eProject['listens'] as $eListen) {
-                        $params1 = [
-                            '-s' => 'restart',
-                        ];
-                        self::getServiceParamsCommon($eProject, $eListen, $params1);
-                        self::getServiceParamsRegister($eListen, $params1);
-                        $command1 = self::createCommandStr($commandPrefix, $params1);
-
-                        $params2 = [
-                            '-s' => 'startstatus',
-                        ];
-                        self::getServiceParamsCommon($eProject, $eListen, $params2);
-                        self::getServiceParamsRegister($eListen, $params2);
-                        $command2 = self::createCommandStr($commandPrefix, $params2);
-                        system($command1 . ' && ' . $command2);
-                    }
-                }
-                break;
-            case 'kz-all':
-                foreach ($projects as $eProject) {
-                    foreach ($eProject['listens'] as $eListen) {
-                        $params = [
-                            '-s' => 'kz',
-                        ];
-                        self::getServiceParamsCommon($eProject, $eListen, $params);
-                        $command = self::createCommandStr($commandPrefix, $params);
-                        system($command);
-                    }
-                }
-                break;
-            default:
-                system('echo -e "\e[1;31m command not exist \e[0m"');
-        }
-    }
-
-    public static function handleAllProcessPool(string $commandPrefix, array $pools)
-    {
-        $action = Tool::getClientOption('-s', false, '');
-        switch ($action) {
-            case 'start-all':
-                foreach ($pools as $ePool) {
-                    foreach ($ePool['listens'] as $eListen) {
-                        $params1 = [
-                            '-s' => 'start',
-                        ];
-                        self::getProcessPoolParamsCommon($ePool, $eListen, $params1);
-                        $command1 = self::createCommandStr($commandPrefix, $params1);
-
-                        $params2 = [
-                            '-s' => 'startstatus',
-                        ];
-                        self::getProcessPoolParamsCommon($ePool, $eListen, $params2);
-                        $command2 = self::createCommandStr($commandPrefix, $params2);
-                        system($command1 . ' && ' . $command2);
-                    }
-                }
-                break;
-            case 'stop-all':
-                foreach ($pools as $ePool) {
-                    foreach ($ePool['listens'] as $eListen) {
-                        $params = [
-                            '-s' => 'stop',
-                        ];
-                        self::getProcessPoolParamsCommon($ePool, $eListen, $params);
-                        $command = self::createCommandStr($commandPrefix, $params);
-                        system($command);
-                    }
-                }
-                break;
-            case 'restart-all':
-                foreach ($pools as $ePool) {
-                    foreach ($ePool['listens'] as $eListen) {
-                        $params1 = [
-                            '-s' => 'restart',
-                        ];
-                        self::getProcessPoolParamsCommon($ePool, $eListen, $params1);
-                        $command1 = self::createCommandStr($commandPrefix, $params1);
-
-                        $params2 = [
-                            '-s' => 'startstatus',
-                        ];
-                        self::getProcessPoolParamsCommon($ePool, $eListen, $params2);
-                        $command2 = self::createCommandStr($commandPrefix, $params2);
-                        system($command1 . ' && ' . $command2);
-                    }
-                }
-                break;
-            default:
-                system('echo -e "\e[1;31m command not exist \e[0m"');
-        }
     }
 }
