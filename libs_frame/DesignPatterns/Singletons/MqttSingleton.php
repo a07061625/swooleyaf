575a3315fae7cd07b15695efcfa5fb6917061e4b
jiangwei <jiangwei07061625@163.com>
2021-03-16 11:51:08 +0800

    a000-feat(mq): add mqtt client for mq

---
+++ libs_frame/DesignPatterns/Singletons/MqttSingleton.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 姜伟
+ * Date: 2021/3/11 0011
+ * Time: 17:04
+ */
+namespace DesignPatterns\Singletons;
+
+use SyMessageQueue\Mqtt\Connection;
+use SyTrait\SingletonTrait;
+
+/**
+ * Class MqttSingleton
+ * @package DesignPatterns\Singletons
+ */
+class MqttSingleton
+{
+    use SingletonTrait;
+
+    /**
+     * @var null|\SyMessageQueue\Mqtt\Connection
+     */
+    private $conn = null;
+
+    private function __construct(array $will)
+    {
+        $this->conn = new Connection($will);
+    }
+
+    /**
+     * @param array $will 延迟配置
+     * @return \DesignPatterns\Singletons\MqttSingleton
+     */
+    public static function getInstance(array $will = []) : self
+    {
+        if (null === self::$instance) {
+            self::$instance = new self($will);
+        }
+
+        return self::$instance;
+    }
+
+    /**
+     * @return \SyMessageQueue\Mqtt\Connection
+     */
+    public function getConn() : Connection
+    {
+        return $this->conn;
+    }
+}
