b4b78b9ee711d808501448a5e18356e098458a0b
jiangwei <jiangwei07061625@163.com>
2021-04-15 18:11:43 +0800

    a000-feat(api): add douyin api

--- libs_frame/SyDouYin/Base.php
+++ libs_frame/SyDouYin/Base.php
@@ -16,6 +16,12 @@ namespace SyDouYin;
 abstract class Base
 {
     /**
+     * 应用标识
+     * @var string
+     */
+    protected $clientKey = '';
+
+    /**
      * 服务域名
      *
      * @var string
@@ -43,21 +49,22 @@ abstract class Base
      */
     protected $curlConfigs = [];
 
-    public function __construct()
+    public function __construct(string $clientKey)
     {
+        $this->clientKey = $clientKey;
+    }
+
+    protected function getContent()
+    {
+        $this->curlConfigs[CURLOPT_URL] = $this->serviceHost . $this->serviceUri;
+        $this->curlConfigs[CURLOPT_SSL_VERIFYPEER] = false;
+        $this->curlConfigs[CURLOPT_SSL_VERIFYHOST] = false;
+        $this->curlConfigs[CURLOPT_HEADER] = false;
+        $this->curlConfigs[CURLOPT_RETURNTRANSFER] = true;
+        if (!isset($this->curlConfigs[CURLOPT_TIMEOUT_MS])) {
+            $this->curlConfigs[CURLOPT_TIMEOUT_MS] = 2000;
+        }
     }
 
     abstract public function getDetail(): array;
-
-    /**
-     * 获取服务地址
-     *
-     * @return string 服务地址
-     */
-    protected function getServiceUrl(): string
-    {
-        return $this->serviceHost . $this->serviceUri;
-    }
-
-    abstract protected function getContent(): array;
 }
