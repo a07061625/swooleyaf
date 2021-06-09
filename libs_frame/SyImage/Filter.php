9acfdd9b6476e3a7b8eb29e7f308dfe11d1c89e0
jiangwei <jiangwei07061625@163.com>
2021-06-09 18:30:35 +0800

    a000-feat(image): update filter

--- libs_frame/SyImage/Filter.php
+++ libs_frame/SyImage/Filter.php
@@ -7,6 +7,9 @@
  */
 namespace SyImage;
 
+use Grafika\Color;
+use Grafika\EditorInterface;
+use Grafika\ImageInterface;
 use SyConstant\ErrorCode;
 use SyConstant\SyInner;
 use SyException\Image\ImageException;
@@ -37,6 +40,10 @@ class Filter
      * @var \Grafika\ImageInterface|null
      */
     private $image = null;
+    /**
+     * @var string
+     */
+    private $mimeType = '';
 
     /**
      * @param string $srcFile 来源全路径文件名
@@ -49,6 +56,14 @@ class Filter
             throw new ImageException('解析图片失败', ErrorCode::IMAGE_UPLOAD_PARAM_ERROR);
         } elseif (!in_array($imageInfo[2], [1, 2, 3], true)) {
             throw new ImageException('图片类型不支持', ErrorCode::IMAGE_UPLOAD_PARAM_ERROR);
+        }
+
+        if ($imageInfo[2] == 1) {
+            $this->mimeType = SyInner::IMAGE_MIME_TYPE_GIF;
+        } elseif ($imageInfo[2] == 2) {
+            $this->mimeType = SyInner::IMAGE_MIME_TYPE_JPEG;
+        } else {
+            $this->mimeType = SyInner::IMAGE_MIME_TYPE_PNG;
         }
 
         $this->editor = Grafika::createEditor();
@@ -114,6 +129,7 @@ class Filter
      * @param int $blur 模糊度,0到100,数值越大越模糊
      * @return $this
      * @throws \SyException\Image\ImageException
+     * @throws \Exception
      */
     public function handleBlur(int $blur)
     {
@@ -132,6 +148,7 @@ class Filter
      * @param int $brightness 亮度,-100到100,<0变暗 >0变亮
      * @return $this
      * @throws \SyException\Image\ImageException
+     * @throws \Exception
      */
     public function handleBrightness(int $brightness)
     {
@@ -152,6 +169,7 @@ class Filter
      * @param int $blue 蓝色色值,-100到100,<0色值减少 >0色值增加
      * @return $this
      * @throws \SyException\Image\ImageException
+     * @throws \Exception
      */
     public function handleColorize(int $red, int $green, int $blue)
     {
@@ -176,6 +194,7 @@ class Filter
      * @param int $contrast 对比度,-100到100,<0对比度减少 >0对比度增加
      * @return $this
      * @throws \SyException\Image\ImageException
+     * @throws \Exception
      */
     public function handleContrast(int $contrast)
     {
@@ -194,6 +213,7 @@ class Filter
      * @param string $ditherType 噪点类型,diffusion:扩散 ordered:规整
      * @return $this
      * @throws \SyException\Image\ImageException
+     * @throws \Exception
      */
     public function handleDither(string $ditherType)
     {
@@ -212,6 +232,7 @@ class Filter
      * @param float $gamma 色阶
      * @return $this
      * @throws \SyException\Image\ImageException
+     * @throws \Exception
      */
     public function handleGamma($gamma)
     {
@@ -228,6 +249,7 @@ class Filter
     /**
      * 图片灰度,只保留黑白两种颜色
      * @return $this
+     * @throws \Exception
      */
     public function handleGrayscale()
     {
@@ -240,6 +262,7 @@ class Filter
     /**
      * 图像反色处理
      * @return $this
+     * @throws \Exception
      */
     public function handleInvert()
     {
@@ -288,11 +311,236 @@ class Filter
     /**
      * 图像查找边缘,检测出图像的边缘
      * @return $this
+     * @throws \Exception
      */
     public function handleSobel()
     {
         $filter = Grafika::createFilter('Sobel');
         $this->editor->apply($this->image, $filter);
+
+        return $this;
+    }
+
+    /**
+     * 等比例缩放,缩放后不填充背景
+     * @param int $width 宽度
+     * @param int $height 高度
+     * @return $this
+     */
+    public function handleResizeFit(int $width, int $height)
+    {
+        $this->editor->resizeFit($this->image, $width, $height);
+
+        return $this;
+    }
+
+    /**
+     * 固定尺寸缩放,可能导致图片变形
+     * @param int $width 宽度
+     * @param int $height 高度
+     * @return $this
+     */
+    public function handleResizeExact(int $width, int $height)
+    {
+        $this->editor->resizeExact($this->image, $width, $height);
+
+        return $this;
+    }
+
+    /**
+     * 居中剪裁,长边的大于指定值的部分居中剪裁掉,图片不会变形
+     * @param int $width 宽度
+     * @param int $height 高度
+     * @return $this
+     */
+    public function handleResizeFill(int $width, int $height)
+    {
+        $this->editor->resizeFill($this->image, $width, $height);
+
+        return $this;
+    }
+
+    /**
+     * 等宽缩放,等比缩放,高度不管
+     * @param int $width 宽度
+     * @return $this
+     */
+    public function handleResizeExactWidth(int $width)
+    {
+        $this->editor->resizeExactWidth($this->image, $width);
+
+        return $this;
+    }
+
+    /**
+     * 等高缩放,等比缩放,宽度不管
+     * @param int $height 高度
+     * @return $this
+     */
+    public function handleResizeExactHeight(int $height)
+    {
+        $this->editor->resizeExactHeight($this->image, $height);
+
+        return $this;
+    }
+
+    /**
+     * 移除GIF动画效果
+     * @return $this
+     */
+    public function handleFlatten()
+    {
+        if ($this->mimeType == SyInner::IMAGE_MIME_TYPE_GIF) {
+            $this->editor->flatten($this->image);
+        }
+
+        return $this;
+    }
+
+    /**
+     * 图片合并
+     * @param string $blendImage 待合并图片
+     * @param array $configs 配置<pre>
+     *   mode_type: string 选填 模式类型,默认为normal,可选值: normal multiply overlay screen
+     *   position_type: string 选填 位置类型,默认为center,可选值: top-left top-center top-right center-left center center-right bottom-left bottom-center bottom-right smart
+     *   opacity: float 选填 透明度,默认为1.0,完全不透明,最小为0.0,最大为1.0
+     *   offset_x: int 选填 待合并图片到原图左边的距离,默认为0,最小为0
+     *   offset_y: int 选填 待合并图片到原图上边的距离,默认为0,最小为0</pre>
+     * @return $this
+     * @throws \SyException\Image\ImageException
+     * @throws \Exception
+     */
+    public function handleBlend(string $blendImage, array $configs = [])
+    {
+        if (!is_file($blendImage)) {
+            throw new ImageException('待合并图片不合法', ErrorCode::IMAGE_UPLOAD_PARAM_ERROR);
+        }
+
+        $modeType = $configs['mode_type'] ?? 'normal';
+        if (!in_array($modeType, [
+            'normal',
+            'multiply',
+            'overlay',
+            'screen',
+        ])) {
+            throw new ImageException('模式类型不支持', ErrorCode::IMAGE_UPLOAD_PARAM_ERROR);
+        }
+
+        $opacity = $configs['opacity'] ?? 1.0;
+        if (($opacity < 0) || ($opacity > 1)) {
+            throw new ImageException('透明度不合法', ErrorCode::IMAGE_UPLOAD_PARAM_ERROR);
+        }
+
+        $positionType = $configs['position_type'] ?? 'center';
+        if (!in_array($positionType, [
+            'top-left',
+            'top-center',
+            'top-right',
+            'center-left',
+            'center',
+            'center-right',
+            'bottom-left',
+            'bottom-center',
+            'bottom-right',
+            'smart',
+        ])) {
+            throw new ImageException('位置类型不支持', ErrorCode::IMAGE_UPLOAD_PARAM_ERROR);
+        }
+
+        $offsetX = $configs['offset_x'] ?? 0;
+        if ($offsetX < 0) {
+            throw new ImageException('左边距不合法', ErrorCode::IMAGE_UPLOAD_PARAM_ERROR);
+        }
+
+        $offsetY = $configs['offset_y'] ?? 0;
+        if ($offsetY < 0) {
+            throw new ImageException('上边距不合法', ErrorCode::IMAGE_UPLOAD_PARAM_ERROR);
+        }
+
+        $this->editor->open($image2, $blendImage);
+        $this->editor->blend($this->image, $image2, $modeType, $opacity, $positionType, $offsetX, $offsetY);
+        unset($image2);
+
+        return $this;
+    }
+
+    /**
+     * 图像旋转
+     * @param int $angle 角度
+     * @param string $color 背景色,16进制rgb
+     * @return $this
+     * @throws \SyException\Image\ImageException
+     * @throws \Exception
+     */
+    public function handleRotate(int $angle, string $color = '')
+    {
+        if (($angle < 0) || ($angle > 359)) {
+            throw new ImageException('角度不合法', ErrorCode::IMAGE_UPLOAD_PARAM_ERROR);
+        }
+        if ((strlen($color) > 0) && !ctype_alpha($color)) {
+            throw new ImageException('背景色不合法', ErrorCode::IMAGE_UPLOAD_PARAM_ERROR);
+        }
+
+        if (strlen($color) == 0) {
+            $this->editor->rotate($this->image, $angle);
+        } else {
+            $this->editor->rotate($this->image, $angle, new Color('#' . $color));
+        }
+
+        return $this;
+    }
+
+    /**
+     * 水印文字
+     * @param string $text 文字内容
+     * @param array $configs 配置<pre>
+     *   size: int 选填 字体大小,默认为12,最小为1
+     *   offset_x: int 选填 文字内容到原图左边的距离,默认为0,最小为0
+     *   offset_y: int 选填 文字内容到原图上边的距离,默认为12,最小为0
+     *   color: string 选填 背景色,16进制rgb,默认为黑色
+     *   font: string 选填 字体路径,默认为Sans font
+     *   angle: int 选填 旋转角度,默认为0,取值范围为0-359</pre>
+     * @return $this
+     * @throws \SyException\Image\ImageException
+     * @throws \Exception
+     */
+    public function handleText(string $text, array $configs = [])
+    {
+        if (strlen($text) == 0) {
+            throw new ImageException('文字内容不能为空', ErrorCode::IMAGE_UPLOAD_PARAM_ERROR);
+        }
+
+        $size = $configs['size'] ?? 12;
+        if ($size <= 0) {
+            throw new ImageException('字体大小不合法', ErrorCode::IMAGE_UPLOAD_PARAM_ERROR);
+        }
+
+        $offsetX = $configs['offset_x'] ?? 0;
+        if ($offsetX < 0) {
+            throw new ImageException('左边距不合法', ErrorCode::IMAGE_UPLOAD_PARAM_ERROR);
+        }
+
+        $offsetY = $configs['offset_y'] ?? $size;
+        if ($offsetY < 0) {
+            throw new ImageException('上边距不合法', ErrorCode::IMAGE_UPLOAD_PARAM_ERROR);
+        }
+
+        $color = $configs['color'] ?? '000000';
+        if ((strlen($color) > 0) && !ctype_alpha($color)) {
+            throw new ImageException('背景色不合法', ErrorCode::IMAGE_UPLOAD_PARAM_ERROR);
+        }
+
+        $font = $configs['font'] ?? '';
+        if ((strlen($font) > 0) && !is_file($font)) {
+            throw new ImageException('字体路径不合法', ErrorCode::IMAGE_UPLOAD_PARAM_ERROR);
+        }
+
+        $angle = $configs['angle'] ?? 0;
+        if (($angle < 0) || ($angle > 359)) {
+            throw new ImageException('角度不合法', ErrorCode::IMAGE_UPLOAD_PARAM_ERROR);
+        }
+
+        $this->editor->text($this->image, $text, $size, $offsetX, $offsetY, new Color('#' . $color), $font, $angle);
 
         return $this;
     }
