38a535ea0f283933038e4a5abb4faab70a82ad7c
jiangwei <jiangwei07061625@163.com>
2021-06-09 18:28:54 +0800

    a000-feat(image): update filter

--- libs_frame/SyImage/Filter.php
+++ libs_frame/SyImage/Filter.php
@@ -5,39 +5,43 @@
  * Date: 2018/5/8 0008
  * Time: 10:10
  */
+
 namespace SyImage;
 
 use Grafika\Color;
+use Grafika\Grafika;
 use SyConstant\ErrorCode;
 use SyConstant\SyInner;
 use SyException\Image\ImageException;
-use Grafika\Grafika;
 
 /**
  * 图片滤镜处理类
  * Class Filter
+ *
  * @package SyImage
  */
 class Filter
 {
     /**
      * 来源文件全路径
+     *
      * @var string
      */
     private $srcFile = '';
     /**
      * 目的文件全路径
+     *
      * @var string
      */
     private $dstFile = '';
     /**
-     * @var \Grafika\EditorInterface|null
+     * @var null|\Grafika\EditorInterface
      */
-    private $editor = null;
+    private $editor;
     /**
-     * @var \Grafika\ImageInterface|null
+     * @var null|\Grafika\ImageInterface
      */
-    private $image = null;
+    private $image;
     /**
      * @var string
      */
@@ -45,20 +49,22 @@ class Filter
 
     /**
      * @param string $srcFile 来源全路径文件名
+     *
      * @throws \SyException\Image\ImageException
      */
     public function __construct(string $srcFile)
     {
         $imageInfo = getimagesize($srcFile);
-        if ($imageInfo === false) {
+        if (false === $imageInfo) {
             throw new ImageException('解析图片失败', ErrorCode::IMAGE_UPLOAD_PARAM_ERROR);
-        } elseif (!in_array($imageInfo[2], [1, 2, 3], true)) {
+        }
+        if (!\in_array($imageInfo[2], [1, 2, 3], true)) {
             throw new ImageException('图片类型不支持', ErrorCode::IMAGE_UPLOAD_PARAM_ERROR);
         }
 
-        if ($imageInfo[2] == 1) {
+        if (1 == $imageInfo[2]) {
             $this->mimeType = SyInner::IMAGE_MIME_TYPE_GIF;
-        } elseif ($imageInfo[2] == 2) {
+        } elseif (2 == $imageInfo[2]) {
             $this->mimeType = SyInner::IMAGE_MIME_TYPE_JPEG;
         } else {
             $this->mimeType = SyInner::IMAGE_MIME_TYPE_PNG;
@@ -72,11 +78,11 @@ class Filter
 
     public function __destruct()
     {
-        if (!is_null($this->image)) {
-            unset($this->image);
+        if (null !== $this->image) {
+            $this->image = null;
         }
-        if (!is_null($this->editor)) {
-            unset($this->editor);
+        if (null !== $this->editor) {
+            $this->editor = null;
         }
     }
 
@@ -84,24 +90,17 @@ class Filter
     {
     }
 
-    /**
-     * @return string
-     */
-    public function getSrcFile() : string
+    public function getSrcFile(): string
     {
         return $this->srcFile;
     }
 
-    /**
-     * @return string
-     */
-    public function getDstFile() : string
+    public function getDstFile(): string
     {
         return $this->dstFile;
     }
 
     /**
-     * @param string $dstFile
      * @throws \SyException\Image\ImageException
      */
     public function setDstFile(string $dstFile)
@@ -115,7 +114,7 @@ class Filter
             '/',
             '/',
         ], $dstFile);
-        if (strlen($trueFile) > 0) {
+        if (\strlen($trueFile) > 0) {
             $this->dstFile = $trueFile;
         } else {
             throw new ImageException('目标文件不合法', ErrorCode::IMAGE_UPLOAD_PARAM_ERROR);
@@ -124,8 +123,11 @@ class Filter
 
     /**
      * 图片模糊
+     *
      * @param int $blur 模糊度,0到100,数值越大越模糊
+     *
      * @return $this
+     *
      * @throws \SyException\Image\ImageException
      * @throws \Exception
      */
@@ -143,8 +145,11 @@ class Filter
 
     /**
      * 图片亮度调整
+     *
      * @param int $brightness 亮度,-100到100,<0变暗 >0变亮
+     *
      * @return $this
+     *
      * @throws \SyException\Image\ImageException
      * @throws \Exception
      */
@@ -162,10 +167,13 @@ class Filter
 
     /**
      * 改变图片颜色
-     * @param int $red 红色色值,-100到100,<0色值减少 >0色值增加
+     *
+     * @param int $red   红色色值,-100到100,<0色值减少 >0色值增加
      * @param int $green 绿色色值,-100到100,<0色值减少 >0色值增加
-     * @param int $blue 蓝色色值,-100到100,<0色值减少 >0色值增加
+     * @param int $blue  蓝色色值,-100到100,<0色值减少 >0色值增加
+     *
      * @return $this
+     *
      * @throws \SyException\Image\ImageException
      * @throws \Exception
      */
@@ -189,8 +197,11 @@ class Filter
 
     /**
      * 改变图片对比度
+     *
      * @param int $contrast 对比度,-100到100,<0对比度减少 >0对比度增加
+     *
      * @return $this
+     *
      * @throws \SyException\Image\ImageException
      * @throws \Exception
      */
@@ -208,14 +219,17 @@ class Filter
 
     /**
      * 图像噪点
+     *
      * @param string $ditherType 噪点类型,diffusion:扩散 ordered:规整
+     *
      * @return $this
+     *
      * @throws \SyException\Image\ImageException
      * @throws \Exception
      */
     public function handleDither(string $ditherType)
     {
-        if (in_array($ditherType, SyInner::$totalImageFilterDither, true)) {
+        if (\in_array($ditherType, SyInner::$totalImageFilterDither, true)) {
             $filter = Grafika::createFilter('Dither', $ditherType);
             $this->editor->apply($this->image, $filter);
         } else {
@@ -227,8 +241,11 @@ class Filter
 
     /**
      * 图像色阶调整,使图像看起来颜色更加正确
+     *
      * @param float $gamma 色阶
+     *
      * @return $this
+     *
      * @throws \SyException\Image\ImageException
      * @throws \Exception
      */
@@ -246,7 +263,9 @@ class Filter
 
     /**
      * 图片灰度,只保留黑白两种颜色
+     *
      * @return $this
+     *
      * @throws \Exception
      */
     public function handleGrayscale()
@@ -259,7 +278,9 @@ class Filter
 
     /**
      * 图像反色处理
+     *
      * @return $this
+     *
      * @throws \Exception
      */
     public function handleInvert()
@@ -272,8 +293,11 @@ class Filter
 
     /**
      * 图片像素化、栅格化
+     *
      * @param int $pixel 像素
+     *
      * @return $this
+     *
      * @throws \SyException\Image\ImageException
      */
     public function handlePixelate(int $pixel)
@@ -290,8 +314,11 @@ class Filter
 
     /**
      * 图片锐化,补偿图像的轮廓,增强图像的边缘及灰度跳变的部分,使图像变得清晰
+     *
      * @param int $sharpen 锐化值,1到100
+     *
      * @return $this
+     *
      * @throws \SyException\Image\ImageException
      */
     public function handleSharpen(int $sharpen)
@@ -308,7 +335,9 @@ class Filter
 
     /**
      * 图像查找边缘,检测出图像的边缘
+     *
      * @return $this
+     *
      * @throws \Exception
      */
     public function handleSobel()
@@ -321,8 +350,10 @@ class Filter
 
     /**
      * 等比例缩放,缩放后不填充背景
-     * @param int $width 宽度
+     *
+     * @param int $width  宽度
      * @param int $height 高度
+     *
      * @return $this
      */
     public function handleResizeFit(int $width, int $height)
@@ -334,8 +365,10 @@ class Filter
 
     /**
      * 固定尺寸缩放,可能导致图片变形
-     * @param int $width 宽度
+     *
+     * @param int $width  宽度
      * @param int $height 高度
+     *
      * @return $this
      */
     public function handleResizeExact(int $width, int $height)
@@ -347,8 +380,10 @@ class Filter
 
     /**
      * 居中剪裁,长边的大于指定值的部分居中剪裁掉,图片不会变形
-     * @param int $width 宽度
+     *
+     * @param int $width  宽度
      * @param int $height 高度
+     *
      * @return $this
      */
     public function handleResizeFill(int $width, int $height)
@@ -360,7 +395,9 @@ class Filter
 
     /**
      * 等宽缩放,等比缩放,高度不管
+     *
      * @param int $width 宽度
+     *
      * @return $this
      */
     public function handleResizeExactWidth(int $width)
@@ -372,7 +409,9 @@ class Filter
 
     /**
      * 等高缩放,等比缩放,宽度不管
+     *
      * @param int $height 高度
+     *
      * @return $this
      */
     public function handleResizeExactHeight(int $height)
@@ -384,11 +423,12 @@ class Filter
 
     /**
      * 移除GIF动画效果
+     *
      * @return $this
      */
     public function handleFlatten()
     {
-        if ($this->mimeType == SyInner::IMAGE_MIME_TYPE_GIF) {
+        if (SyInner::IMAGE_MIME_TYPE_GIF == $this->mimeType) {
             $this->editor->flatten($this->image);
         }
 
@@ -397,14 +437,17 @@ class Filter
 
     /**
      * 图片合并
+     *
      * @param string $blendImage 待合并图片
-     * @param array $configs 配置<pre>
-     *   mode_type: string 选填 模式类型,默认为normal,可选值: normal multiply overlay screen
-     *   position_type: string 选填 位置类型,默认为center,可选值: top-left top-center top-right center-left center center-right bottom-left bottom-center bottom-right smart
-     *   opacity: float 选填 透明度,默认为1.0,完全不透明,最小为0.0,最大为1.0
-     *   offset_x: int 选填 待合并图片到原图左边的距离,默认为0,最小为0
-     *   offset_y: int 选填 待合并图片到原图上边的距离,默认为0,最小为0</pre>
+     * @param array  $configs    配置<pre>
+     *                           mode_type: string 选填 模式类型,默认为normal,可选值: normal multiply overlay screen
+     *                           position_type: string 选填 位置类型,默认为center,可选值: top-left top-center top-right center-left center center-right bottom-left bottom-center bottom-right smart
+     *                           opacity: float 选填 透明度,默认为1.0,完全不透明,最小为0.0,最大为1.0
+     *                           offset_x: int 选填 待合并图片到原图左边的距离,默认为0,最小为0
+     *                           offset_y: int 选填 待合并图片到原图上边的距离,默认为0,最小为0</pre>
+     *
      * @return $this
+     *
      * @throws \SyException\Image\ImageException
      * @throws \Exception
      */
@@ -415,7 +458,7 @@ class Filter
         }
 
         $modeType = $configs['mode_type'] ?? 'normal';
-        if (!in_array($modeType, [
+        if (!\in_array($modeType, [
             'normal',
             'multiply',
             'overlay',
@@ -430,7 +473,7 @@ class Filter
         }
 
         $positionType = $configs['position_type'] ?? 'center';
-        if (!in_array($positionType, [
+        if (!\in_array($positionType, [
             'top-left',
             'top-center',
             'top-right',
@@ -464,9 +507,12 @@ class Filter
 
     /**
      * 图像旋转
-     * @param int $angle 角度
+     *
+     * @param int    $angle 角度
      * @param string $color 背景色,16进制rgb
+     *
      * @return $this
+     *
      * @throws \SyException\Image\ImageException
      * @throws \Exception
      */
@@ -475,11 +521,11 @@ class Filter
         if (($angle < 0) || ($angle > 359)) {
             throw new ImageException('角度不合法', ErrorCode::IMAGE_UPLOAD_PARAM_ERROR);
         }
-        if ((strlen($color) > 0) && !ctype_alpha($color)) {
+        if ((\strlen($color) > 0) && !ctype_alpha($color)) {
             throw new ImageException('背景色不合法', ErrorCode::IMAGE_UPLOAD_PARAM_ERROR);
         }
 
-        if (strlen($color) == 0) {
+        if (0 == \strlen($color)) {
             $this->editor->rotate($this->image, $angle);
         } else {
             $this->editor->rotate($this->image, $angle, new Color('#' . $color));
@@ -490,21 +536,24 @@ class Filter
 
     /**
      * 水印文字
-     * @param string $text 文字内容
-     * @param array $configs 配置<pre>
-     *   size: int 选填 字体大小,默认为12,最小为1
-     *   offset_x: int 选填 文字内容到原图左边的距离,默认为0,最小为0
-     *   offset_y: int 选填 文字内容到原图上边的距离,默认为12,最小为0
-     *   color: string 选填 背景色,16进制rgb,默认为黑色
-     *   font: string 选填 字体路径,默认为Sans font
-     *   angle: int 选填 旋转角度,默认为0,取值范围为0-359</pre>
+     *
+     * @param string $text    文字内容
+     * @param array  $configs 配置<pre>
+     *                        size: int 选填 字体大小,默认为12,最小为1
+     *                        offset_x: int 选填 文字内容到原图左边的距离,默认为0,最小为0
+     *                        offset_y: int 选填 文字内容到原图上边的距离,默认为12,最小为0
+     *                        color: string 选填 背景色,16进制rgb,默认为黑色
+     *                        font: string 选填 字体路径,默认为Sans font
+     *                        angle: int 选填 旋转角度,默认为0,取值范围为0-359</pre>
+     *
      * @return $this
+     *
      * @throws \SyException\Image\ImageException
      * @throws \Exception
      */
     public function handleText(string $text, array $configs = [])
     {
-        if (strlen($text) == 0) {
+        if (0 == \strlen($text)) {
             throw new ImageException('文字内容不能为空', ErrorCode::IMAGE_UPLOAD_PARAM_ERROR);
         }
 
@@ -524,12 +573,12 @@ class Filter
         }
 
         $color = $configs['color'] ?? '000000';
-        if ((strlen($color) > 0) && !ctype_alpha($color)) {
+        if ((\strlen($color) > 0) && !ctype_alpha($color)) {
             throw new ImageException('背景色不合法', ErrorCode::IMAGE_UPLOAD_PARAM_ERROR);
         }
 
         $font = $configs['font'] ?? '';
-        if ((strlen($font) > 0) && !is_file($font)) {
+        if ((\strlen($font) > 0) && !is_file($font)) {
             throw new ImageException('字体路径不合法', ErrorCode::IMAGE_UPLOAD_PARAM_ERROR);
         }
 
