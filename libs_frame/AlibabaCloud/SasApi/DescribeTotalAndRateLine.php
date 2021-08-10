4940e99adcf51960615774a69258fd190aead072
jiangwei <jiangwei07061625@163.com>
2021-08-11 01:43:11 +0800

    a000-feat(libs): update alibabacloud libs

---
+++ libs_frame/AlibabaCloud/SasApi/DescribeTotalAndRateLine.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AlibabaCloud\SasApi;
+
+/**
+ * @method string getSourceIp()
+ * @method $this withSourceIp($value)
+ * @method string getApiType()
+ * @method $this withApiType($value)
+ */
+class DescribeTotalAndRateLine extends Rpc
+{
+}
