8250ee2eb21a795232d84c9aea9e0b08be057c1d
jiangwei <jiangwei07061625@163.com>
2021-08-11 07:04:15 +0800

    a000-feat(libs): update alibabacloud libs

---
+++ libs_frame/AlibabaCloud/Dypnsapi/GetMobile.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace AlibabaCloud\Dypnsapi;
+
+/**
+ * @method string getResourceOwnerId()
+ * @method $this withResourceOwnerId($value)
+ * @method string getResourceOwnerAccount()
+ * @method $this withResourceOwnerAccount($value)
+ * @method string getAccessToken()
+ * @method $this withAccessToken($value)
+ * @method string getOwnerId()
+ * @method $this withOwnerId($value)
+ * @method string getOutId()
+ * @method $this withOutId($value)
+ */
+class GetMobile extends Rpc
+{
+    /** @var string */
+    public $scheme = 'https';
+}
---
+++ libs_frame/AlibabaCloud/Dypnsapi/TwiceTelVerify.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace AlibabaCloud\Dypnsapi;
+
+/**
+ * @method string getResourceOwnerId()
+ * @method $this withResourceOwnerId($value)
+ * @method string getResourceOwnerAccount()
+ * @method $this withResourceOwnerAccount($value)
+ * @method string getPhoneNumber()
+ * @method $this withPhoneNumber($value)
+ * @method string getOwnerId()
+ * @method $this withOwnerId($value)
+ * @method string getSince()
+ * @method $this withSince($value)
+ */
+class TwiceTelVerify extends Rpc
+{
+}
