99336875d86242e30f7a1fc0371470ecca7bf978
jiangwei <jiangwei07061625@163.com>
2021-08-11 07:11:10 +0800

    a000-feat(libs): update alibabacloud libs

--- libs_frame/AlibabaCloud/MoguanSdk/RegisterDevice.php
+++ libs_frame/AlibabaCloud/MoguanSdk/RegisterDevice.php
@@ -1,27 +1,6 @@
 <?php
 
-namespace AlibabaCloud\MoguanSdk\V20210415;
-
-use AlibabaCloud\Client\Resolver\ApiResolver;
-
-/**
- * @method RegisterDevice registerDevice(array $options = [])
- */
-class MoguanSdkApiResolver extends ApiResolver
-{
-}
-
-class Rpc extends \AlibabaCloud\Client\Resolver\Rpc
-{
-    /** @var string */
-    public $product = 'moguan-sdk';
-
-    /** @var string */
-    public $version = '2021-04-15';
-
-    /** @var string */
-    public $method = 'POST';
-}
+namespace AlibabaCloud\MoguanSdk;
 
 /**
  * @method string getUserDeviceId()
