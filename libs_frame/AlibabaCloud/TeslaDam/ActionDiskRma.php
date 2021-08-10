1f05d46ec588b242ed879924b6057d2b8e133164
jiangwei <jiangwei07061625@163.com>
2021-08-11 07:35:26 +0800

    a000-feat(libs): update alibabacloud libs

---
+++ libs_frame/AlibabaCloud/TeslaDam/ActionDiskRma.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace AlibabaCloud\TeslaDam;
+
+/**
+ * @method string getDiskName()
+ * @method $this withDiskName($value)
+ * @method string getExecutionId()
+ * @method $this withExecutionId($value)
+ * @method string getDiskSlot()
+ * @method $this withDiskSlot($value)
+ * @method string getHostname()
+ * @method $this withHostname($value)
+ * @method string getDiskMount()
+ * @method $this withDiskMount($value)
+ * @method string getDiskReason()
+ * @method $this withDiskReason($value)
+ * @method string getDiskSn()
+ * @method $this withDiskSn($value)
+ */
+class ActionDiskRma extends Rpc
+{
+}
---
+++ libs_frame/AlibabaCloud/TeslaDam/HostGets.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace AlibabaCloud\TeslaDam;
+
+/**
+ * @method string getQuery()
+ * @method $this withQuery($value)
+ * @method string getEndTime()
+ * @method $this withEndTime($value)
+ * @method string getStartTime()
+ * @method $this withStartTime($value)
+ * @method string getQueryType()
+ * @method $this withQueryType($value)
+ */
+class HostGets extends Rpc
+{
+}
