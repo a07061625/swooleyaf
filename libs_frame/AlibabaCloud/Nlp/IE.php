8443e4287d960f64dac238a809ec365e0e45d90f
jiangwei <jiangwei07061625@163.com>
2021-08-11 07:33:52 +0800

    a000-feat(libs): update alibabacloud libs

---
+++ libs_frame/AlibabaCloud/Nlp/IE.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AlibabaCloud\Nlp;
+
+/**
+ * @method string getDomain()
+ * @method $this withDomain($value)
+ */
+class IE extends Roa
+{
+    /** @var string */
+    public $pathPattern = '/nlp/api/ie/[Domain]';
+}
---
+++ libs_frame/AlibabaCloud/Nlp/TextStructure.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AlibabaCloud\Nlp;
+
+/**
+ * @method string getDomain()
+ * @method $this withDomain($value)
+ */
+class TextStructure extends Roa
+{
+    /** @var string */
+    public $pathPattern = '/nlp/api/textstructure/[Domain]';
+}
