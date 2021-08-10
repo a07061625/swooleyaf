ee366af39558685a71d132d57e4114f455a23915
jiangwei <jiangwei07061625@163.com>
2021-08-11 07:18:39 +0800

    a000-feat(libs): update alibabacloud libs

---
+++ libs_frame/AlibabaCloud/SmsIntl/SendMessageToGlobe.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace AlibabaCloud\SmsIntl;
+
+/**
+ * @method string getResourceOwnerId()
+ * @method $this withResourceOwnerId($value)
+ * @method string getResourceOwnerAccount()
+ * @method $this withResourceOwnerAccount($value)
+ * @method string getFrom()
+ * @method $this withFrom($value)
+ * @method string getTo()
+ * @method $this withTo($value)
+ * @method string getOwnerId()
+ * @method $this withOwnerId($value)
+ * @method string getMessage()
+ * @method $this withMessage($value)
+ * @method string getType()
+ * @method $this withType($value)
+ */
+class SendMessageToGlobe extends Rpc
+{
+}
