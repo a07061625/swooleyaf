c1d3d7c5c9130db5d8626bce5a3009526fcaa9ed
jiangwei <jiangwei07061625@163.com>
2021-08-11 01:37:42 +0800

    a000-feat(libs): update alibabacloud libs

---
+++ libs_frame/AlibabaCloud/Jaq/AfsAppCheck.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AlibabaCloud\Jaq;
+
+/**
+ * @method string getSession()
+ * @method $this withSession($value)
+ * @method string getCallerName()
+ * @method $this withCallerName($value)
+ */
+class AfsAppCheck extends Rpc
+{
+}
