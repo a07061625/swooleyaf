6eae40df6417a993b35c8cf80b2dc2af27270632
jiangwei <jiangwei07061625@163.com>
2021-08-11 06:55:58 +0800

    a000-feat(libs): update alibabacloud libs

---
+++ libs_frame/AlibabaCloud/Dybaseapi/QueryTokenForMnsQueue.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace AlibabaCloud\Dybaseapi;
+
+/**
+ * @method string getQueueName()
+ * @method $this withQueueName($value)
+ * @method string getResourceOwnerId()
+ * @method $this withResourceOwnerId($value)
+ * @method string getResourceOwnerAccount()
+ * @method $this withResourceOwnerAccount($value)
+ * @method string getMessageType()
+ * @method $this withMessageType($value)
+ * @method string getOwnerId()
+ * @method $this withOwnerId($value)
+ */
+class QueryTokenForMnsQueue extends Rpc
+{
+}
