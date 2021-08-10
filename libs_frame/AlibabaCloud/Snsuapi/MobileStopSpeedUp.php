2ef478a35a27ed0994be2238ee78f7e42b86f5f1
jiangwei <jiangwei07061625@163.com>
2021-08-11 07:22:18 +0800

    a000-feat(libs): update alibabacloud libs

---
+++ libs_frame/AlibabaCloud/Snsuapi/MobileStopSpeedUp.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace AlibabaCloud\Snsuapi;
+
+/**
+ * @method string getResourceOwnerId()
+ * @method $this withResourceOwnerId($value)
+ * @method string getResourceOwnerAccount()
+ * @method $this withResourceOwnerAccount($value)
+ * @method string getCorrelationId()
+ * @method $this withCorrelationId($value)
+ * @method string getOwnerId()
+ * @method $this withOwnerId($value)
+ */
+class MobileStopSpeedUp extends Rpc
+{
+}
