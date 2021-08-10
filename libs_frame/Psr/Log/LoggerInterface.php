c48d647eb6d196e7d3d239cd6e738b3c5e823af8
jiangwei <jiangwei07061625@163.com>
2021-08-10 12:28:20 +0800

    a000-feat(libs): update aliopen libs

--- libs_frame/Psr/Log/LoggerInterface.php
+++ libs_frame/Psr/Log/LoggerInterface.php
@@ -7,7 +7,7 @@ namespace Psr\Log;
  * The message MAY contain placeholders in the form: {foo} where foo
  * will be replaced by the context data in key "foo".
  * The context array can contain arbitrary data, the only assumption that
- * can be made by implementors is that if an SyException instance is given
+ * can be made by implementors is that if an Exception instance is given
  * to produce a stack trace, it MUST be in a key named "exception".
  * See https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-3-logger-interface.md
  * for the full interface specification.
