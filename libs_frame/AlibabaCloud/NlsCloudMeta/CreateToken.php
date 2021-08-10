f349e67d7386fdd7534bbea25170f9d02bfe7969
jiangwei <jiangwei07061625@163.com>
2021-08-11 07:39:04 +0800

    a000-feat(libs): update alibabacloud libs

---
+++ libs_frame/AlibabaCloud/NlsCloudMeta/CreateToken.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace AlibabaCloud\NlsCloudMeta;
+
+class CreateToken extends Roa
+{
+    /** @var string */
+    public $pathPattern = '/pop/2018-05-18/tokens';
+}
---
+++ libs_frame/AlibabaCloud/NlsCloudMeta/Roa.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace AlibabaCloud\NlsCloudMeta;
+
+class Roa extends \AlibabaCloud\Client\Resolver\Roa
+{
+    /** @var string */
+    public $product = 'nls-cloud-meta';
+
+    /** @var string */
+    public $version = '2018-05-18';
+
+    /** @var string */
+    public $method = 'POST';
+}
