71a7f92ba7037e94d46b27730112598809497e15
jiangwei <jiangwei07061625@163.com>
2021-08-11 07:39:09 +0800

    a000-feat(libs): update alibabacloud libs

---
+++ libs_frame/AlibabaCloud/TeslaMaxCompute/GetInstancesStatusCount.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace AlibabaCloud\TeslaMaxCompute;
+
+/**
+ * @method string getCluster()
+ * @method $this withCluster($value)
+ * @method string getQuotaId()
+ * @method $this withQuotaId($value)
+ * @method string getRegion()
+ * @method $this withRegion($value)
+ * @method string getQuotaName()
+ * @method $this withQuotaName($value)
+ */
+class GetInstancesStatusCount extends Rpc
+{
+}
