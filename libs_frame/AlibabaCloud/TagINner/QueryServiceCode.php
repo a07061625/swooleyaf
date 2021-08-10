1ac2d9f42755337f764d23fb30d475475e69b1cb
jiangwei <jiangwei07061625@163.com>
2021-08-11 07:30:13 +0800

    a000-feat(libs): update alibabacloud libs

---
+++ libs_frame/AlibabaCloud/TagINner/QueryServiceCode.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace AlibabaCloud\TagINner;
+
+/**
+ * @method string getResourceOwnerId()
+ * @method $this withResourceOwnerId($value)
+ * @method string getTagOwnerUid()
+ * @method $this withTagOwnerUid($value)
+ * @method string getResourceOwnerAccount()
+ * @method $this withResourceOwnerAccount($value)
+ * @method string getOwnerAccount()
+ * @method $this withOwnerAccount($value)
+ * @method string getOwnerId()
+ * @method $this withOwnerId($value)
+ */
+class QueryServiceCode extends Rpc
+{
+}
