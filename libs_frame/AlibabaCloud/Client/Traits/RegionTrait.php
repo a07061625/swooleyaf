4b021ef6383c571f70333ddc2d163b21b1db41fa
jiangwei <jiangwei07061625@163.com>
2021-08-10 15:26:03 +0800

    a000-feat(libs): update alibabacloud libs

---
+++ libs_frame/AlibabaCloud/Client/Traits/RegionTrait.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace AlibabaCloud\Client\Traits;
+
+use AlibabaCloud\Client\Filter\ClientFilter;
+use AlibabaCloud\Client\Exception\ClientException;
+
+/**
+ * Trait RegionTrait
+ * @package AlibabaCloud\Client\Traits
+ */
+trait RegionTrait
+{
+    /**
+     * @var string|null
+     */
+    public $regionId;
+
+    /**
+     * @param string $regionId
+     * @return $this
+     * @throws ClientException
+     */
+    public function regionId($regionId)
+    {
+        $this->regionId = ClientFilter::regionId($regionId);
+
+        return $this;
+    }
+}
