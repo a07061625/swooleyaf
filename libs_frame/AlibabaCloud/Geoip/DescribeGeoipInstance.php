f9bbdec2e16979271d3ca793dedc3daeb8f92b30
jiangwei <jiangwei07061625@163.com>
2021-08-11 08:47:46 +0800

    a000-feat(libs): update alibabacloud libs

---
+++ libs_frame/AlibabaCloud/Geoip/DescribeGeoipInstance.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace AlibabaCloud\Geoip;
+
+/**
+ * @method string getInstanceId()
+ * @method $this withInstanceId($value)
+ * @method string getUserClientIp()
+ * @method $this withUserClientIp($value)
+ * @method string getLang()
+ * @method $this withLang($value)
+ */
+class DescribeGeoipInstance extends Rpc
+{
+}
