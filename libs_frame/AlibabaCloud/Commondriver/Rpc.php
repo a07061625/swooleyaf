6cac28353f06d59482fe7dcc51cb3404168a2e6b
jiangwei <jiangwei07061625@163.com>
2021-08-11 09:17:52 +0800

    a000-feat(libs): update alibabacloud libs

---
+++ libs_frame/AlibabaCloud/Commondriver/Rpc.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace AlibabaCloud\Commondriver;
+
+class Rpc extends \AlibabaCloud\Client\Resolver\Rpc
+{
+    /** @var string */
+    public $product = 'Commondriver';
+
+    /** @var string */
+    public $version = '2015-12-29';
+
+    /** @var string */
+    public $method = 'POST';
+}
