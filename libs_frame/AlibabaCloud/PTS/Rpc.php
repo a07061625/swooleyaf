b484cdbb89a3c74a9144e7eedb8a6f53e0c1ddd8
jiangwei <jiangwei07061625@163.com>
2021-08-11 09:22:49 +0800

    a000-feat(libs): update alibabacloud libs

---
+++ libs_frame/AlibabaCloud/PTS/Rpc.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace AlibabaCloud\PTS;
+
+class Rpc extends \AlibabaCloud\Client\Resolver\Rpc
+{
+    /** @var string */
+    public $product = 'PTS';
+
+    /** @var string */
+    public $version = '2018-11-11';
+}
