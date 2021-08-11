4d4ab23a09b0df565222f36ecf2c73d10b0eabe0
jiangwei <jiangwei07061625@163.com>
2021-08-11 08:49:41 +0800

    a000-feat(libs): update alibabacloud libs

---
+++ libs_frame/AlibabaCloud/Videorecog/DetectVideoShot.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AlibabaCloud\Videorecog;
+
+/**
+ * @method string getAsync()
+ * @method string getVideoUrl()
+ */
+class DetectVideoShot extends Rpc
+{
+    /**
+     * @param string $value
+     *
+     * @return $this
+     */
+    public function withAsync($value)
+    {
+        $this->data['Async'] = $value;
+        $this->options['form_params']['Async'] = $value;
+
+        return $this;
+    }
+
+    /**
+     * @param string $value
+     *
+     * @return $this
+     */
+    public function withVideoUrl($value)
+    {
+        $this->data['VideoUrl'] = $value;
+        $this->options['form_params']['VideoUrl'] = $value;
+
+        return $this;
+    }
+}
