3b361869b0d6910fc9d386cf7832ace0928bab05
jiangwei <jiangwei07061625@163.com>
2021-08-11 09:21:46 +0800

    a000-feat(libs): update alibabacloud libs

---
+++ libs_frame/AlibabaCloud/ProductCatalog/Roa.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace AlibabaCloud\ProductCatalog;
+
+class Roa extends \AlibabaCloud\Client\Resolver\Roa
+{
+    /** @var string */
+    public $product = 'ProductCatalog';
+
+    /** @var string */
+    public $version = '2018-09-18';
+}
