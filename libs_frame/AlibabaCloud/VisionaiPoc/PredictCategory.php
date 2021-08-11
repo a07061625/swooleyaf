9fe64e0121328b14443349ee81106613d69ec71d
jiangwei <jiangwei07061625@163.com>
2021-08-11 08:56:19 +0800

    a000-feat(libs): update alibabacloud libs

---
+++ libs_frame/AlibabaCloud/VisionaiPoc/PredictCategory.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace AlibabaCloud\VisionaiPoc;
+
+/**
+ * @method string getMethod()
+ * @method string getImageUrl()
+ * @method string getTitle()
+ * @method string getUrl()
+ */
+class PredictCategory extends Rpc
+{
+    /**
+     * @param string $value
+     *
+     * @return $this
+     */
+    public function withMethod($value)
+    {
+        $this->data['Method'] = $value;
+        $this->options['form_params']['Method'] = $value;
+
+        return $this;
+    }
+
+    /**
+     * @param string $value
+     *
+     * @return $this
+     */
+    public function withImageUrl($value)
+    {
+        $this->data['ImageUrl'] = $value;
+        $this->options['form_params']['ImageUrl'] = $value;
+
+        return $this;
+    }
+
+    /**
+     * @param string $value
+     *
+     * @return $this
+     */
+    public function withTitle($value)
+    {
+        $this->data['Title'] = $value;
+        $this->options['form_params']['Title'] = $value;
+
+        return $this;
+    }
+
+    /**
+     * @param string $value
+     *
+     * @return $this
+     */
+    public function withUrl($value)
+    {
+        $this->data['Url'] = $value;
+        $this->options['form_params']['Url'] = $value;
+
+        return $this;
+    }
+}
