73780de6677bdfae60f14e642a0336dbc323245c
jiangwei <jiangwei07061625@163.com>
2021-08-12 16:47:23 +0800

    a000-feat(constant): constant define type change from define to const

--- helper_load_example.php
+++ helper_load_example.php
@@ -1,14 +1,14 @@
 <?php
-define('SY_ROOT', __DIR__);
-define('SY_ENV', 'dev');
-define('SY_PROJECT', 'z01');
-define('SY_DATABASE', true);
-define('SY_LC_WX_ACCOUNT', false);
-define('SY_LC_WXOPEN_AUTHORIZER', false);
-define('SY_PROJECT_LIBS_ROOT', __DIR__ . '/libs_project/');
+const SY_ROOT = __DIR__;
+const SY_ENV = 'dev';
+const SY_PROJECT = 'z01';
+const SY_DATABASE = true;
+const SY_LC_WX_ACCOUNT = false;
+const SY_LC_WXOPEN_AUTHORIZER = false;
+const SY_PROJECT_LIBS_ROOT = __DIR__ . '/libs_project/';
 //前两位为数字的内部自用,放出去的都是前两位非数字的
-define('SY_TOKEN', '245dcbf2');
-define('SY_TOKEN_SECRET', 'eyJpdiI6ImY2OGE2MDBmNmMxYjExNmMiLCJ2YWx1ZSI6IkdESm5vcFBHSlltU0FCc3R3bm1zZDBCb0poVWtkQ0R2bU1lOFVUdGdBekE9In0=');
+const SY_TOKEN = '245dcbf2';
+const SY_TOKEN_SECRET = 'eyJpdiI6ImY2OGE2MDBmNmMxYjExNmMiLCJ2YWx1ZSI6IkdESm5vcFBHSlltU0FCc3R3bm1zZDBCb0poVWtkQ0R2bU1lOFVUdGdBekE9In0=';
 
 $frameLibsDir = \Yaconf::get('project.' . SY_ENV . SY_PROJECT . '.dir.libs.frame');
 if (substr($frameLibsDir, -1) == '/') {
