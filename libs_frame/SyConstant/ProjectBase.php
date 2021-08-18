ca57a429d27bc089d2f8ed9d74216a3a5568252d
jiangwei <jiangwei07061625@163.com>
2021-08-18 15:13:50 +0800

    a000-fix(server): fixed error for the key of request id in

--- libs_frame/SyConstant/ProjectBase.php
+++ libs_frame/SyConstant/ProjectBase.php
@@ -65,7 +65,7 @@ class ProjectBase
     /**
      * 数据-键名-$_SERVER中请求ID字段
      */
-    const DATA_KEY_REQUEST_ID_SERVER = 'SyReq-Id';
+    const DATA_KEY_REQUEST_ID_SERVER = 'SY-REQ-ID';
 
     /**
      * 公共-默认页数
