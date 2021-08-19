3effa2127b17ac56a252df06182068311c9d9f55
jiangwei <jiangwei07061625@163.com>
2021-08-19 13:57:27 +0800

    a000-feat(validator): move sign and rate data store in nginx

--- libs_frame/SyTrait/Server/FrameHttpTrait.php
+++ libs_frame/SyTrait/Server/FrameHttpTrait.php
@@ -8,28 +8,8 @@
 
 namespace SyTrait\Server;
 
-use DesignPatterns\Factories\CacheSimpleFactory;
-use SyConstant\Project;
-
 trait FrameHttpTrait
 {
-    /**
-     * 添加签名缓存
-     *
-     * @param string $sign 签名信息
-     */
-    public static function addApiSign(string $sign): bool
-    {
-        $signKey = Project::YAC_PREFIX_API_SIGN . md5($sign);
-        $cacheData = CacheSimpleFactory::getYacInstance()->get($signKey);
-        if (\is_string($cacheData)) {
-            return false;
-        }
-        CacheSimpleFactory::getYacInstance()->set($signKey, '1', Project::TIME_EXPIRE_LOCAL_API_SIGN_CACHE);
-
-        return true;
-    }
-
     private function checkServerHttp()
     {
         $this->checkServerBase();
