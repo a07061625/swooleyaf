f70fd162a1105eb071a805487662e20d401ed587
jiangwei <jiangwei07061625@163.com>
2021-09-06 14:52:16 +0800

    a000-feat(validator): add apilimit for string

--- libs_project/SyTrait/Validators/ApiLimitTrait.php
+++ libs_project/SyTrait/Validators/ApiLimitTrait.php
@@ -5,17 +5,19 @@
  * Date: 2021/9/6
  * Time: 14:03
  */
+
 namespace SyTrait\Validators;
 
 use SyTool\Tool;
 
 /**
  * Trait ApiLimitTrait
+ *
  * @package SyTrait\Validators
  */
 trait ApiLimitTrait
 {
-    private function getApiExpireTime() : int
+    private function getApiExpireTime(): int
     {
         return Tool::getNowTime() + 100;
     }
