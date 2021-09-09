59b4ca916f87495c28482953e5f5503f3c5ba929
jiangwei <jiangwei07061625@163.com>
2021-09-09 11:22:08 +0800

    a000-feat(frame): add simplecache for psr

--- libs_frame/Psr/SimpleCache/CacheInterface.php
+++ libs_frame/Psr/SimpleCache/CacheInterface.php
@@ -5,10 +5,12 @@
  * Date: 2021/9/9
  * Time: 0:16
  */
+
 namespace Psr\SimpleCache;
 
 /**
  * Interface CacheInterface
+ *
  * @package Psr\SimpleCache
  */
 interface CacheInterface
@@ -22,21 +24,21 @@ interface CacheInterface
      * @return mixed 从缓存中返回的值，或者是不存在时的默认值
      *
      * @throws \Psr\SimpleCache\InvalidArgumentException
-     *   如果给定的key不是一个合法的字符串时，抛出该异常
+     *                                                   如果给定的key不是一个合法的字符串时，抛出该异常
      */
     public function get($key, $default = null);
 
     /**
      * 存储值在cache中，唯一关键到一个key及一个可选的存在时间
      *
-     * @param string                 $key   存储项目的key.
+     * @param string                 $key   存储项目的key
      * @param mixed                  $value 存储的值，必须可以被序列化的
-     * @param null|int|\DateInterval $ttl   可选项.项目的存在时间，如果该值没有设置，且驱动支持生存时间时，将设置一个默认值，或者驱自行处理。
+     * @param null|\DateInterval|int $ttl   可选项.项目的存在时间，如果该值没有设置，且驱动支持生存时间时，将设置一个默认值，或者驱自行处理。
      *
      * @return bool true 存储成功  false 存储失败
      *
      * @throws \Psr\SimpleCache\InvalidArgumentException
-     *  如果给定的key不是一个合法的字符串时，抛出该异常。
+     *                                                   如果给定的key不是一个合法的字符串时，抛出该异常
      */
     public function set($key, $value, $ttl = null);
 
@@ -48,7 +50,7 @@ interface CacheInterface
      * @return bool 成功删除时返回ture，有其它错误时时返回false
      *
      * @throws \Psr\SimpleCache\InvalidArgumentException
-     *   如果给定的key不是一个合法的字符串时，抛出该异常。
+     *                                                   如果给定的key不是一个合法的字符串时，抛出该异常
      */
     public function delete($key);
 
@@ -62,13 +64,13 @@ interface CacheInterface
     /**
      * 根据指定的缓存键值列表获取得多个缓存项目
      *
-     * @param iterable $keys   在单次操作中可被获取的键值项
+     * @param iterable $keys    在单次操作中可被获取的键值项
      * @param mixed    $default 如果key不存在时，返回的默认值
      *
-     * @return iterable  返回键值对（key=>value形式）列表。如果key不存在，或者已经过期时，返回默认值。
+     * @return iterable 返回键值对（key=>value形式）列表。如果key不存在，或者已经过期时，返回默认值。
      *
      * @throws \Psr\SimpleCache\InvalidArgumentException
-     *  如果给定的keys既不是合法的数组，也不可以被转成数组，或者给得的任何一个key不是一个合法的值时，拖出该异常。
+     *                                                   如果给定的keys既不是合法的数组，也不可以被转成数组，或者给得的任何一个key不是一个合法的值时，拖出该异常
      */
     public function getMultiple($keys, $default = null);
 
@@ -76,12 +78,12 @@ interface CacheInterface
      * 存储一个键值对形式的集合到缓存中。
      *
      * @param iterable               $values 一系列操作的键值对列表
-     * @param null|int|\DateInterval $ttl     可选项.项目的存在时间，如果该值没有设置，且驱动支持生存时间时，将设置一个默认值，或者驱自行处理。
+     * @param null|\DateInterval|int $ttl    可选项.项目的存在时间，如果该值没有设置，且驱动支持生存时间时，将设置一个默认值，或者驱自行处理。
      *
      * @return bool 成功返回True.失败返回False.
      *
      * @throws \Psr\SimpleCache\InvalidArgumentException
-     *   如果给定的keys既不是合法的数组，也不可以被转成数组，或者给得的任何一个key不是一个合法的值时，拖出该异常.
+     *                                                   如果给定的keys既不是合法的数组，也不可以被转成数组，或者给得的任何一个key不是一个合法的值时，拖出该异常
      */
     public function setMultiple($values, $ttl = null);
 
@@ -93,7 +95,7 @@ interface CacheInterface
      * @return bool True 所有项目都成功被删除时回true,有任何错误时返回false
      *
      * @throws \Psr\SimpleCache\InvalidArgumentException
-     *   如果给定的keys既不是合法的数组，也不可以被转成数组，或者给得的任何一个key不是一个合法的值时，拖出该异常.
+     *                                                   如果给定的keys既不是合法的数组，也不可以被转成数组，或者给得的任何一个key不是一个合法的值时，拖出该异常
      */
     public function deleteMultiple($keys);
 
@@ -101,12 +103,13 @@ interface CacheInterface
      * 判断一个项目在缓存中是否存在
      *
      * 注意: has()方法仅仅在缓存预热的场景被推荐使用且不允许的活跃     * 的应用中场景中对get/set方法使用, 因为方法受竞态条件的限制，当     * 你调用has()方法时会立即返回true。另一个脚本可以删除它，使应     * 用状态过期。
+     *
      * @param string $key 缓存键值
      *
      * @return bool
      *
      * @throws \Psr\SimpleCache\InvalidArgumentException
-     *    如果给定的key不是一个合法的字符串时，抛出该异常.
+     *                                                   如果给定的key不是一个合法的字符串时，抛出该异常
      */
     public function has($key);
 }
