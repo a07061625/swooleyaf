daaaf43ff9d5f3ea11c8437387675b0d90348eb8
jiangwei <jiangwei07061625@163.com>
2021-09-09 11:22:01 +0800

    a000-feat(frame): add simplecache for psr

---
+++ libs_frame/Psr/SimpleCache/CacheException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 姜伟
+ * Date: 2021/9/9
+ * Time: 0:17
+ */
+namespace Psr\SimpleCache;
+
+/**
+ * 库抛出异常的接口，用于所有类型异常。
+ */
+interface CacheException
+{
+}
