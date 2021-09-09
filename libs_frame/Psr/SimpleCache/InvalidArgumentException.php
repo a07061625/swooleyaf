31ed1acf640533cf9fdfde051a45b257ae4542d7
jiangwei <jiangwei07061625@163.com>
2021-09-09 11:19:10 +0800

    a000-feat(frame): add simplecache for psr

---
+++ libs_frame/Psr/SimpleCache/InvalidArgumentException.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 姜伟
+ * Date: 2021/9/9
+ * Time: 0:17
+ */
+namespace Psr\SimpleCache;
+
+/**
+ * 无效缓存参数异常的接口。
+ *
+ * 当传递一个无效参数时，必须抛出一个实现了此接口的异常。
+ */
+interface InvalidArgumentException extends CacheException
+{
+}
