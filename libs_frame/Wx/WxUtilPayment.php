005512ea5fff1251725f135d80feee2db2e2a049
jiangwei <jiangwei07061625@163.com>
2022-12-17 15:14:32 +0800

    a000-feat(frame): update dingtalk api

--- libs_frame/Wx/WxUtilPayment.php
+++ libs_frame/Wx/WxUtilPayment.php
@@ -5,6 +5,7 @@
  * Date: 2020/5/4 0004
  * Time: 13:59
  */
+
 namespace Wx;
 
 use DesignPatterns\Singletons\WxConfigSingleton;
@@ -15,6 +16,7 @@ use SyTrait\SimpleTrait;
 
 /**
  * Class WxUtilPayment
+ *
  * @package Wx
  */
 abstract class WxUtilPayment extends WxUtilBase
@@ -23,25 +25,25 @@ abstract class WxUtilPayment extends WxUtilBase
 
     /**
      * 处理退款通知消息
-     * @param array $data
-     * @return array
      */
-    public static function handleRefundNotify(array $data) : array
+    public static function handleRefundNotify(array $data): array
     {
         $resArr = [
-            'code' => 0
+            'code' => 0,
         ];
 
-        if ($data['return_code'] == 'FAIL') {
+        if ('FAIL' == $data['return_code']) {
             $resArr['code'] = ErrorCode::WX_POST_ERROR;
             $resArr['message'] = $data['return_msg'];
+
             return $resArr;
         }
 
         $decryptData = base64_decode($data['req_info'], true);
-        if (is_bool($decryptData)) {
+        if (\is_bool($decryptData)) {
             $resArr['code'] = ErrorCode::WX_POST_ERROR;
             $resArr['message'] = '解密数据失败';
+
             return $resArr;
         }
 
@@ -62,26 +64,24 @@ abstract class WxUtilPayment extends WxUtilBase
 
     /**
      * 企业银行付款rsa加密
-     * @param string $appId
-     * @param array $data
-     * @return array
+     *
      * @throws \SyException\Wx\WxException
      */
-    public static function encryptRsaCompanyBank(string $appId, array $data) : array
+    public static function encryptRsaCompanyBank(string $appId, array $data): array
     {
         if (empty($data)) {
             throw new WxException('加密数据不能为空', ErrorCode::WX_POST_ERROR);
         }
 
         $keyContent = WxConfigSingleton::getInstance()->getAccountConfig($appId)->getSslCompanyBank();
-        if (strlen($keyContent) == 0) {
+        if (0 == \strlen($keyContent)) {
             throw new WxException('银行卡公钥不能为空', ErrorCode::WX_POST_ERROR);
         }
 
         $publicKey = openssl_pkey_get_public($keyContent);
         $encryptData = [];
         foreach ($data as $key => $val) {
-            if (is_string($val) && (strlen($val) > 0)) {
+            if (\is_string($val) && (\strlen($val) > 0)) {
                 $encryptStr = '';
                 openssl_public_encrypt($val, $encryptStr, $publicKey, OPENSSL_PKCS1_OAEP_PADDING);
                 $encryptData[$key] = $encryptStr;
@@ -94,10 +94,8 @@ abstract class WxUtilPayment extends WxUtilBase
 
     /**
      * 获取H5支付深度链接
-     * @param array $data
-     * @return string
      */
-    public static function getH5PayDeepLink(array $data) : string
+    public static function getH5PayDeepLink(array $data): string
     {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $data['mweb_url']);
@@ -116,26 +114,24 @@ abstract class WxUtilPayment extends WxUtilBase
 
         if (empty($matches)) {
             return '';
-        } else {
-            $deepLinkUrl = urldecode($matches[1]);
-            if (isset($data['redirect_url'])) {
-                $deepLinkUrl .= '&redirect_url=' . urlencode($data['redirect_url']);
-            }
-
-            return $deepLinkUrl;
         }
+        $deepLinkUrl = urldecode($matches[1]);
+        if (isset($data['redirect_url'])) {
+            $deepLinkUrl .= '&redirect_url=' . urlencode($data['redirect_url']);
+        }
+
+        return $deepLinkUrl;
     }
 
     /**
      * 生成V3请求头认证令牌
-     * @param array $data
-     * @return string
      */
-    public static function createV3Token(array $data) : string
+    public static function createV3Token(array $data): string
     {
         $message = $data['request_method'] . "\n" . $data['request_url'] . "\n" . $data['timestamp'] . "\n" . $data['nonce'] . "\n" . $data['body'] . "\n";
         openssl_sign($message, $rawSign, $data['mch_private_key'], 'sha256WithRSAEncryption');
         $sign = base64_encode($rawSign);
+
         return 'mchid="' . $data['merchant_id']
                . '",serial_no="' . $data['serial_no']
                . '",nonce_str="' . $data['nonce']
