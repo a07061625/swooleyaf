fbb15d466a1749edebb51aca638c114320df0029
jiangwei <jiangwei07061625@163.com>
2022-12-19 21:01:30 +0800

    a000-feat(wx): add merchant v3 api

---
+++ libs_frame/Wx/Merchant/V3/Certs/CertList.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * 获取平台证书列表
+ * User: 姜伟
+ * Date: 2020/5/4 0004
+ * Time: 15:09
+ */
+
+namespace Wx\Merchant\V3\Certs;
+
+use DesignPatterns\Singletons\WxConfigSingleton;
+use SyTool\Tool;
+use Wx\WxBaseMerchantV3;
+use Wx\WxUtilBase;
+
+/**
+ * Class CertList
+ *
+ * @package Wx\Payment\V3\Certs
+ */
+class CertList extends WxBaseMerchantV3
+{
+    public function __construct(string $appId)
+    {
+        parent::__construct($appId);
+        $this->serviceUrl = 'https://api.mch.weixin.qq.com/v3/certificates';
+        $this->reqMethod = self::REQUEST_METHOD_GET;
+        array_push($this->curlConfigs[CURLOPT_HTTPHEADER], 'Accept: application/json');
+    }
+
+    private function __clone()
+    {
+        //do nothing
+    }
+
+    /**
+     * @return array
+     * @throws \SyException\Wx\WxException
+     */
+    public function getDetail(): array
+    {
+        $this->curlConfigs[CURLOPT_URL] = $this->serviceUrl;
+        $this->setHeadAuth();
+
+        $accountConfig = WxConfigSingleton::getInstance()->getAccountConfig($this->appId);
+        array_push($this->curlConfigs[CURLOPT_HTTPHEADER], 'User-Agent: ' . $accountConfig->getPayMchId());
+        $sendRes = WxUtilBase::sendGetReq($this->curlConfigs);
+
+        return [
+            'code' => 0,
+            'data' => Tool::jsonDecode($sendRes),
+        ];
+    }
+}
