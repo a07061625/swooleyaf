1129054426274d5a49ceaba86d780d3c0ac6d4f7
jiangwei <jiangwei07061625@163.com>
2022-12-21 20:11:26 +0800

    a000-feat(wx): add merchant v3 api

---
+++ libs_frame/Wx/Merchant/V3/ProfitSharing/BillList.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * 分账账单列表
+ * User: 姜伟
+ * Date: 2022/12/21
+ * Time: 15:30
+ */
+namespace Wx\Merchant\V3\ProfitSharing;
+
+use SyConstant\ErrorCode;
+use SyException\Wx\WxException;
+use Wx\WxBaseMerchantV3;
+use Wx\WxUtilBase;
+
+/**
+ * Class BillList
+ * @package Wx\Merchant\V3\ProfitSharing
+ */
+class BillList extends WxBaseMerchantV3
+{
+    /**
+     * 账单日期,格式yyyy-MM-DD
+     * @var string
+     */
+    private $bill_date = '';
+
+    public function __construct(string $appId)
+    {
+        parent::__construct($appId);
+        $this->reqMethod = self::REQUEST_METHOD_GET;
+        $this->setHeadJson();
+    }
+
+    private function __clone()
+    {
+        //do nothing
+    }
+
+    /**
+     * @param string $billDate
+     * @throws \SyException\Wx\WxException
+     */
+    public function setBillDate(string $billDate)
+    {
+        if (\strlen($billDate) == 10) {
+            $this->reqData['bill_date'] = $billDate;
+        } else {
+            throw new WxException('账单日期不合法', ErrorCode::WX_PARAM_ERROR);
+        }
+    }
+
+    /**
+     * @return array
+     * @throws \SyException\Common\CheckException
+     * @throws \SyException\Wx\WxException
+     */
+    public function getDetail() : array
+    {
+        if (!isset($this->reqData['bill_date'])) {
+            throw new WxException('账单日期不能为空', ErrorCode::WX_PARAM_ERROR);
+        }
+        
+        $this->curlConfigs[CURLOPT_URL] = 'https://api.mch.weixin.qq.com/v3/profitsharing/bills?'
+                                         . http_build_query($this->reqData);
+        $this->setHeadAuth();
+        $sendRes = WxUtilBase::sendGetReq($this->curlConfigs, 2);
+        
+        return $this->handleRespJson($sendRes);
+    }
+}
---
+++ libs_frame/Wx/Merchant/V3/ProfitSharing/OrderApply.php
@@ -0,0 +1,141 @@
+<?php
+/**
+ * 请求分账
+ * User: 姜伟
+ * Date: 2022/12/21
+ * Time: 15:30
+ */
+namespace Wx\Merchant\V3\ProfitSharing;
+
+use SyConstant\ErrorCode;
+use SyException\Wx\WxException;
+use SyTool\Tool;
+use Wx\WxBaseMerchantV3;
+use Wx\WxUtilBase;
+
+/**
+ * Class OrderApply
+ * @package Wx\Merchant\V3\ProfitSharing
+ */
+class OrderApply extends WxBaseMerchantV3
+{
+    /**
+     * 微信订单号
+     * @var string
+     */
+    private $transaction_id = '';
+    /**
+     * 商户分账单号
+     * @var string
+     */
+    private $out_order_no = '';
+    /**
+     * 分账接收方列表
+     * @var array
+     */
+    private $receivers = [];
+    /**
+     * 解冻剩余未分资金状态
+     * @var bool
+     */
+    private $unfreeze_unsplit = false;
+
+    public function __construct(string $appId)
+    {
+        parent::__construct($appId);
+        $this->serviceUrl = 'https://api.mch.weixin.qq.com/v3/profitsharing/orders';
+        $this->reqMethod = self::REQUEST_METHOD_POST;
+        $this->setHeadJson();
+        $this->reqData['appid'] = $appId;
+        $this->reqData['receivers'] = [];
+    }
+
+    private function __clone()
+    {
+        //do nothing
+    }
+
+    /**
+     * @param string $transactionId
+     * @throws \SyException\Wx\WxException
+     */
+    public function setTransactionId(string $transactionId)
+    {
+        if (ctype_alnum($transactionId) && (\strlen($transactionId) <= 32)) {
+            $this->reqData['transaction_id'] = $transactionId;
+        } else {
+            throw new WxException('微信订单号不合法', ErrorCode::WX_PARAM_ERROR);
+        }
+    }
+
+    /**
+     * @param string $outOrderNo
+     * @throws \SyException\Wx\WxException
+     */
+    public function setOutOrderNo(string $outOrderNo)
+    {
+        if (ctype_alnum($outOrderNo) && (\strlen($outOrderNo) <= 64)) {
+            $this->reqData['out_order_no'] = $outOrderNo;
+        } else {
+            throw new WxException('商户分账单号不合法', ErrorCode::WX_PARAM_ERROR);
+        }
+    }
+
+    /**
+     * @param array $receivers
+     * @throws \SyException\Wx\WxException
+     */
+    public function setReceivers(array $receivers)
+    {
+        $this->receivers = $receivers;
+        $this->reqData['receivers'] = [];
+        foreach ($receivers as $receiverInfo) {
+            if (is_array($receiverInfo)&& is_string($receiverInfo['account'])) {
+                array_push($this->reqData['receivers'], $receiverInfo);
+            }
+        }
+
+        $receiversNum = count($this->reqData['receivers']);
+        if ($receiversNum == 0) {
+            throw new WxException('分账接收方不能为空', ErrorCode::WX_PARAM_ERROR);
+        } elseif ($receiversNum > 50) {
+            throw new WxException('分账接收方不能超过50个', ErrorCode::WX_PARAM_ERROR);
+        }
+    }
+
+    /**
+     * @param bool $unfreezeUnSplitStatus
+     */
+    public function setUnfreezeUnSplit(bool $unfreezeUnSplitStatus)
+    {
+        $this->reqData['unfreeze_unsplit'] = $unfreezeUnSplitStatus;
+    }
+
+    /**
+     * @return array
+     * @throws \SyException\Common\CheckException
+     * @throws \SyException\Wx\WxException
+     */
+    public function getDetail() : array
+    {
+        if (!isset($this->reqData['transaction_id'])) {
+            throw new WxException('微信订单号不能为空', ErrorCode::WX_PARAM_ERROR);
+        }
+        if (!isset($this->reqData['out_order_no'])) {
+            throw new WxException('商户分账单号不能为空', ErrorCode::WX_PARAM_ERROR);
+        }
+        if (count($this->reqData['receivers']) == 0) {
+            throw new WxException('分账接收方不能为空', ErrorCode::WX_PARAM_ERROR);
+        }
+        if (!isset($this->reqData['unfreeze_unsplit'])) {
+            throw new WxException('解冻剩余未分资金状态不能为空', ErrorCode::WX_PARAM_ERROR);
+        }
+
+        $this->curlConfigs[CURLOPT_URL] = $this->serviceUrl;
+        $this->setHeadAuth();
+        $this->curlConfigs[CURLOPT_POSTFIELDS] = Tool::jsonEncode($this->reqData, JSON_UNESCAPED_UNICODE);
+        $sendRes = WxUtilBase::sendPostReq($this->curlConfigs, 2);
+        
+        return $this->handleRespJson($sendRes);
+    }
+}
