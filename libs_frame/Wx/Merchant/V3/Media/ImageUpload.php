79998ad999518f26056ab87526eb4bf74b282c1e
jiangwei <jiangwei07061625@163.com>
2022-12-22 11:34:56 +0800

    a000-feat(wx): add merchant v3 api

--- libs_frame/Wx/Merchant/V3/Media/ImageUpload.php
+++ libs_frame/Wx/Merchant/V3/Media/ImageUpload.php
@@ -5,6 +5,7 @@
  * Date: 2022/12/22
  * Time: 11:04
  */
+
 namespace Wx\Merchant\V3\Media;
 
 use SyConstant\ErrorCode;
@@ -14,6 +15,7 @@ use Wx\WxUtilBase;
 
 /**
  * Class ImageUpload
+ *
  * @package Wx\Merchant\V3\Media
  */
 class ImageUpload extends WxBaseMerchantV3
@@ -24,7 +26,7 @@ class ImageUpload extends WxBaseMerchantV3
      * @var string
      */
     private $file_path = '';
-    
+
     public function __construct(string $appId)
     {
         parent::__construct($appId);
@@ -40,7 +42,6 @@ class ImageUpload extends WxBaseMerchantV3
     }
 
     /**
-     * @param string $filePath
      * @throws \SyException\Wx\WxException
      */
     public function setFilePath(string $filePath)
@@ -58,11 +59,10 @@ class ImageUpload extends WxBaseMerchantV3
     }
 
     /**
-     * @return array
      * @throws \SyException\Common\CheckException
      * @throws \SyException\Wx\WxException
      */
-    public function getDetail() : array
+    public function getDetail(): array
     {
         if (!isset($this->reqData['file'])) {
             throw new WxException('文件不能为空', ErrorCode::WX_PARAM_ERROR);
