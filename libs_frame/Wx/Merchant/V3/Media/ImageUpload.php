269feed4fb59312616420d41bccadea8c0451faa
jiangwei <jiangwei07061625@163.com>
2022-12-22 11:31:18 +0800

    a000-feat(wx): add merchant v3 api

---
+++ libs_frame/Wx/Merchant/V3/Media/ImageUpload.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * 图片上传
+ * User: 姜伟
+ * Date: 2022/12/22
+ * Time: 11:04
+ */
+namespace Wx\Merchant\V3\Media;
+
+use SyConstant\ErrorCode;
+use SyException\Wx\WxException;
+use Wx\WxBaseMerchantV3;
+use Wx\WxUtilBase;
+
+/**
+ * Class ImageUpload
+ * @package Wx\Merchant\V3\Media
+ */
+class ImageUpload extends WxBaseMerchantV3
+{
+    /**
+     * 文件全路径,包括文件名
+     *
+     * @var string
+     */
+    private $file_path = '';
+    
+    public function __construct(string $appId)
+    {
+        parent::__construct($appId);
+        $this->serviceUrl = 'https://api.mch.weixin.qq.com/v3/merchant/media/upload';
+        $this->reqMethod = self::REQUEST_METHOD_POST;
+        array_push($this->curlConfigs[CURLOPT_HTTPHEADER], 'Content-Type: multipart/form-data');
+        array_push($this->curlConfigs[CURLOPT_HTTPHEADER], 'Accept: application/json');
+    }
+
+    private function __clone()
+    {
+        //do nothing
+    }
+
+    /**
+     * @param string $filePath
+     * @throws \SyException\Wx\WxException
+     */
+    public function setFilePath(string $filePath)
+    {
+        if (file_exists($filePath) && is_readable($filePath)) {
+            $fileInfo = explode('/', $filePath);
+            $this->reqData['file'] = new \CURLFile($filePath);
+            $this->reqData['meta'] = [
+                'filename' => array_pop($fileInfo),
+                'sha256' => hash('sha256', file_get_contents($filePath)),
+            ];
+        } else {
+            throw new WxException('文件不合法', ErrorCode::WX_PARAM_ERROR);
+        }
+    }
+
+    /**
+     * @return array
+     * @throws \SyException\Common\CheckException
+     * @throws \SyException\Wx\WxException
+     */
+    public function getDetail() : array
+    {
+        if (!isset($this->reqData['file'])) {
+            throw new WxException('文件不能为空', ErrorCode::WX_PARAM_ERROR);
+        }
+
+        $this->curlConfigs[CURLOPT_URL] = $this->serviceUrl;
+        $this->curlConfigs[CURLOPT_POSTFIELDS] = $this->reqData;
+        $sendRes = WxUtilBase::sendPostReq($this->curlConfigs, 2);
+
+        return $this->handleRespJson($sendRes);
+    }
+}
